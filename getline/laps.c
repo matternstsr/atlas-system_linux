f3dea2610ddcd09b8b4a5e9f464aecca339d0a93
matternstsr <matternstsr@gmail.com>
2024-04-30 08:55:20 -0500

    cleaned up the function descriptions and the comment formatting laps.c:5:1: error: C++ style comments are not allowed in ISO C90 [-Werror] //Function to compare two cars by their IDs ^laps.c:5:1: error: (this will be reported only once per input file) [-Werror]laps.c: In function ‘race_state’:laps.c:25:5: error: ‘for’ loop initial declarations are only allowed in C99 mode     for (size_t i = 0; i < size; i++) {     ^laps.c:25:5: note: use option -std=c99 or -std=gnu99 to compile your codelaps.c:61:5: error: ISO C90 forbids mixed declarations and code [-Werror=pedantic]     int num_cars = 0;

--- getline/laps.c
+++ getline/laps.c
@@ -2,33 +2,45 @@
 
 Car *car_list = NULL;
 
-//Function to compare two cars by their IDs
+/**
+ * compare_cars - Function to compare two cars by their IDs
+ * @a: Pointer to the first car
+ * @b: Pointer to the second car
+ * 
+ * Returns: Difference in IDs
+ */
 int compare_cars(const void *a, const void *b) {
     const Car *car1 = *(const Car **)a;
     const Car *car2 = *(const Car **)b;
     return car1->id - car2->id;
 }
 
-void race_state(int *id, size_t size) {
-    if (size == 0) {
-        // Free all of the memory that was allocated
-        Car *current = car_list;
-        while (current != NULL) {
-            Car *temp = current;
-            current = current->next;
-            free(temp);
-        }
-        car_list = NULL;
-        return;
+/**
+ * free_memory - Function to free the list of cars
+ */
+void free_memory() {
+    Car *current = car_list;
+    while (current != NULL) {
+        Car *temp = current;
+        current = current->next;
+        free(temp);
     }
+    car_list = NULL;
+}
 
-    for (size_t i = 0; i < size; i++) {
+/**
+ * update_race_state - Function to update the race state based on the provided IDs
+ * @id: Array of car IDs
+ * @size: Size of the ID array
+ */
+void update_race_state(int *id, size_t size) {
+    size_t i;
+    for (i = 0; i < size; i++) {
         int current_id = id[i];
         Car *current_car = car_list;
         Car *prev_car = NULL;
         int found = 0;
 
-        // Checking if the current car exists in the list of the cars
         while (current_car != NULL) {
             if (current_car->id == current_id) {
                 found = 1;
@@ -39,14 +51,12 @@ void race_state(int *id, size_t size) {
             current_car = current_car->next;
         }
 
-        // Make a new car if not found
         if (!found) {
             Car *new_car = malloc(sizeof(Car));
             new_car->id = current_id;
             new_car->laps = 0;
             new_car->next = NULL;
 
-            // Adding a new car to the list
             if (prev_car == NULL) {
                 car_list = new_car;
             } else {
@@ -56,25 +66,60 @@ void race_state(int *id, size_t size) {
             printf("Car %d joined the race\n", current_id);
         }
     }
+}
 
-    // Sort the cars by their IDs
+/**
+ * sort_cars - Function to sort the cars by their IDs and display the race state
+ */
+void sort_cars() {
     int num_cars = 0;
     Car *current = car_list;
     while (current != NULL) {
         num_cars++;
         current = current->next;
     }
-    Car *car_array[num_cars];
+
+    Car **car_array = malloc(num_cars * sizeof(Car *));
+    if (car_array == NULL) {
+        // Memory allocation failed
+        return;
+    }
+
+    int i;
     current = car_list;
-    for (int i = 0; i < num_cars; i++) {
+    for (i = 0; i < num_cars; i++) {
         car_array[i] = current;
         current = current->next;
     }
+
     qsort(car_array, num_cars, sizeof(Car *), compare_cars);
 
-    // Show the state of the race
     printf("Race state:\n");
-    for (int i = 0; i < num_cars; i++) {
+    for (i = 0; i < num_cars; i++) {
         printf("Car %d [%d laps]\n", car_array[i]->id, car_array[i]->laps);
     }
+
+    free(car_array);
+}
+
+/**
+ * display_race_state - Function to display the race state
+ */
+void display_race_state() {
+    sort_cars();
+}
+
+/**
+ * race_state - Function to handle race state and memory management
+ * @id: Array of car IDs
+ * @size: Size of the ID array
+ */
+void race_state(int *id, size_t size) {
+    if (size == 0) {
+        free_memory();
+        return;
+    }
+
+    update_race_state(id, size);
+    display_race_state();
 }
