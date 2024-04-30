931134f8f933fa6d3f7433d6aca0ac3db776af4a
matternstsr <matternstsr@gmail.com>
2024-04-30 10:43:50 -0500

    desc for the stuct

--- getline/laps.h
+++ getline/laps.h
@@ -5,11 +5,18 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+/**
+ * struct Car - Represents a car participating in the race
+ * @id: The unique identifier of the car
+ * @laps: The number of laps completed by the car
+ * @next: Pointer to the next car in the linked list
+ */
 typedef struct Car {
     int id;
     int laps;
     struct Car *next;
 } Car;
+
 
 void race_state(int *id, size_t size);
 
