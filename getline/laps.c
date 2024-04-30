2869f8008fabadc5152cf71217dd5f9b2f476fc3
matternstsr <matternstsr@gmail.com>
2024-04-30 11:15:56 -0500

    ERROR: that open brace { should be on the next line

--- getline/laps.c
+++ getline/laps.c
@@ -8,19 +8,23 @@ Car *car_list = NULL;
  * @b: Pointer to second Car pointer
  * Return: Difference between IDs of the two cars
  */
-int compare_cars(const void *a, const void *b) {
+int compare_cars(const void *a, const void *b)
+{
 	const Car *car1 = *(const Car **)a;
 	const Car *car2 = *(const Car **)b;
 	return car1->id - car2->id;
 }
 
+
 /**
  * free_memory - Free memory allocated for car list
  */
-void free_memory(void) {
+void free_memory(void)
+{
 	Car *current = car_list;
 	Car *temp;
-	while (current != NULL) {
+	while (current != NULL)
+	{
 		temp = current;
 		current = current->next;
 		free(temp);
@@ -33,16 +37,20 @@ void free_memory(void) {
  * @id: Array of car IDs to update race state with
  * @size: Size of the id array
  */
-void update_race_state(int *id, size_t size) {
+void update_race_state(int *id, size_t size)
+{
 	size_t i;
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < size; i++)
+	{
 		int current_id = id[i];
 		Car *current_car = car_list;
 		Car *prev_car = NULL;
 		int found = 0;
 
-		while (current_car != NULL) {
-			if (current_car->id == current_id) {
+		while (current_car != NULL)
+		{
+			if (current_car->id == current_id)
+			{
 				found = 1;
 				current_car->laps++;
 				break;
@@ -51,13 +59,15 @@ void update_race_state(int *id, size_t size) {
 			current_car = current_car->next;
 		}
 
-		if (!found) {
+		if (!found)
+		{
 			Car *new_car = (Car *)malloc(sizeof(Car));
 			new_car->id = current_id;
 			new_car->laps = 0;
 			new_car->next = NULL;
 
-			if (prev_car == NULL) {
+			if (prev_car == NULL)
+			{
 				car_list = new_car;
 			} else {
 				prev_car->next = new_car;
@@ -71,21 +81,26 @@ void update_race_state(int *id, size_t size) {
 /**
  * sort_cars - Sort the cars in the race by ID
  */
-void sort_cars(void) {
+void sort_cars(void) 
+{
 	Car *current = car_list;
 	Car *sorted_list = NULL;
 
 	/* Traverse the original list */
-	while (current != NULL) {
+	while (current != NULL) 
+	{
 		Car *next = current->next;
 
 		/* Insert current node into sorted_list */
-		if (sorted_list == NULL || current->id < sorted_list->id) {
+		if (sorted_list == NULL || current->id < sorted_list->id)
+		{
 			current->next = sorted_list;
 			sorted_list = current;
-		} else {
+		} else
+		{
 			Car *search = sorted_list;
-			while (search->next != NULL && current->id > search->next->id) {
+			while (search->next != NULL && current->id > search->next->id)
+			{
 				search = search->next;
 			}
 			current->next = search->next;
@@ -103,13 +118,15 @@ void sort_cars(void) {
 /**
  * display_race_state - Display the current race state
  */
-void display_race_state(void) {
+void display_race_state(void) 
+{
 	Car *current;
 
 	sort_cars(); /* Call sort_cars() before displaying race state */
 	printf("Race state:\n");
 	current = car_list;
-	while (current != NULL) {
+	while (current != NULL) 
+	{
 		printf("Car %d [%d laps]\n", current->id, current->laps);
 		current = current->next;
 	}
@@ -120,8 +137,10 @@ void display_race_state(void) {
  * @id: Array of car IDs to update race state with
  * @size: Size of the id array
  */
-void race_state(int *id, size_t size) {
-	if (size == 0) {
+void race_state(int *id, size_t size)
+{
+	if (size == 0)
+	{
 		free_memory();
 		return;
 	}
