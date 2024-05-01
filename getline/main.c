2c8f44b3a4c8d52e03ff89866477f7565686036d
matternstsr <matternstsr@gmail.com>
2024-05-01 11:59:16 -0500

    how bad is this

--- getline/main.c
+++ getline/main.c
@@ -1,9 +1,41 @@
-// Test case 1: Redefine READ_SIZE to 1 and count the number of calls to read
-char *filename = "_getline.c";
-char *file_content = "Test content";
-int expected_read_calls = 4;
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-// Test case 2: Redefine READ_SIZE to 4 and count the number of calls to read
-char *filename = "_getline.c";
-char *file_content = "Test content";
-int expected_read_calls = 1;
+#include "_getline.h"
+
+/**
+ * main - entry point.
+ * @ac: Arguments counter
+ * @av: Arguments vector
+ *
+ * Return: always 0.
+ */
+int main(int ac, char *av[])
+{
+	char *file = __FILE__;
+	int fd;
+	char *line;
+	int i;
+
+	if (ac > 1)
+		file = av[1];
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		return (EXIT_FAILURE);
+	}
+	i = 0;
+	while ((line = _getline(fd)))
+	{
+		printf("%s\n", line);
+		fflush(stdout);
+		i++;
+		free(line);
+	}
+	printf("Total: %d lines\n", i);
+	close(fd);
+	return (EXIT_SUCCESS);
+}
