b557fd19dc34883114d6e00e9d72a6c64d4ebeee
matternstsr <matternstsr@gmail.com>
2024-05-02 09:47:55 -0500

    fixing betty

--- getline/_getline.c
+++ getline/_getline.c
@@ -20,7 +20,8 @@ static int bytes_remaining = 0;
 static int end_of_file_reached = 0;
 
 int fill_buffer(const int fd) {
-    int result = read(fd, buffer, READ_SIZE);
+    int result;
+    result = read(fd, buffer, READ_SIZE);
     if (result <= 0) {
         return result; /* Error or end of file */
     }
@@ -30,7 +31,8 @@ int fill_buffer(const int fd) {
 }
 
 char *find_newline(const char *start, int size) {
-    for (int i = 0; i < size; i++) {
+    int i;
+    for (i = 0; i < size; i++) {
         if (start[i] == '\n') {
             return (char *)(start + i);
         }
@@ -39,14 +41,17 @@ char *find_newline(const char *start, int size) {
 }
 
 char *read_line() {
+    int result;
     char *line = NULL;
     int line_size = 0;
     int newline_found = 0;
     char *newline_pos;
+    int line_length;
+    int remaining_size;
 
     while (!newline_found) {
         if (buf_pos - buffer >= bytes_remaining) {
-            int result = fill_buffer(0); /* Read from stdin */
+            result = fill_buffer(0); /* Read from stdin */
             if (result <= 0) {
                 if (line_size == 0) {
                     return NULL; /* No more lines and buffer is empty */
@@ -58,7 +63,7 @@ char *read_line() {
 
         newline_pos = find_newline(buf_pos, bytes_remaining);
         if (newline_pos != NULL) {
-            int line_length = newline_pos - buf_pos;
+            line_length = newline_pos - buf_pos;
             line = realloc(line, line_size + line_length + 1);
             if (!line) {
                 return NULL; /* Memory allocation failed */
@@ -69,7 +74,7 @@ char *read_line() {
             buf_pos = newline_pos + 1;
             newline_found = 1;
         } else {
-            int remaining_size = bytes_remaining - (buf_pos - buffer);
+            remaining_size = bytes_remaining - (buf_pos - buffer);
             line = realloc(line, line_size + remaining_size + 1);
             if (!line) {
                 return NULL; /* Memory allocation failed */
@@ -92,6 +97,7 @@ void reset_buffer() {
 
 char *_getline(const int fd) {
     char *line;
+    int result;
 
     if (fd == -1) {
         reset_buffer();
@@ -109,7 +115,7 @@ char *_getline(const int fd) {
 
     if (bytes_remaining == 0 || (buf_pos - buffer >= bytes_remaining)) {
         reset_buffer();
-        int result = fill_buffer(fd);
+        result = fill_buffer(fd);
         if (result <= 0) {
             end_of_file_reached = 1;
             return NULL;
