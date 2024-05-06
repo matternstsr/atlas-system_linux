c075871b8089e724df201498614fedf7ea792c18
matternstsr <matternstsr@gmail.com>
2024-05-06 11:49:27 -0500

    betty stil

--- getline/_getline.c
+++ getline/_getline.c
@@ -8,8 +8,10 @@
 /* Redefine malloc using a macro */
 #define malloc(size) my_malloc(size)
 
-void *my_malloc(size_t size) {
+void *my_malloc(size_t size)
+{
 	void *ptr = malloc(size);
+
 	if (ptr != NULL) {
 		/* Initialize allocated memory to 0xFF */
 		memset(ptr, MEMORY_FILL_VALUE, size);
@@ -22,8 +24,10 @@ static char *buf_pos = buffer;
 static int bytes_remaining;
 static int end_of_file_reached;
 
-int fill_buffer(const int fd) {
+int fill_buffer(const int fd)
+{
 	int result;
+
 	result = read(fd, buffer, READ_SIZE);
 	if (result <= 0) {
 		return result; /* Error or end of file */
@@ -33,7 +37,8 @@ int fill_buffer(const int fd) {
 	return result;
 }
 
-char *find_newline(const char *start, int size) {
+char *find_newline(const char *start, int size) 
+{
 	int i;
 
 	for (i = 0; i < size; i++) {
@@ -44,7 +49,8 @@ char *find_newline(const char *start, int size) {
 	return NULL;
 }
 
-char *read_line(const int fd) {
+char *read_line(const int fd)
+{
 	/* Function body remains the same */
 	int result;
 	char *line = NULL;
@@ -95,12 +101,14 @@ char *read_line(const int fd) {
 	return line;
 }
 
-void reset_buffer() {
+void reset_buffer() 
+{
 	buf_pos = buffer;
 	bytes_remaining = 0;
 }
 
-char *_getline(const int fd) {
+char *_getline(const int fd)
+{
 	char *line;
 	int result;
 
