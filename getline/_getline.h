f0d2497e783868a09e30d2155b8f068f48551b33
matternstsr <matternstsr@gmail.com>
2024-05-06 13:08:34 -0500

    I dosnt know why you want the desc in the h file but ok. here ill do that for all of them there too

--- getline/_getline.h
+++ getline/_getline.h
@@ -10,11 +10,36 @@
 #define MY_MALLOC(size) my_malloc(size) /* Renamed the macro */
 #define READ_SIZE 2048
 
+/**
+ * my_malloc - Allocates memory and initializes it to a fill value
+ * @size: Size of memory to allocate
+ * Return: Pointer to the allocated memory
+ */
 void *my_malloc(size_t size); /* Declaration moved before usage */
 
+/**
+ * _getline - Reads a line from a file descriptor
+ * @fd: File descriptor
+ * Return: Pointer to the read line or NULL if end of file
+ */
 char *_getline(const int fd);
+
+/**
+ * reset_buffer - Resets the buffer position and bytes remaining
+ */
 void reset_buffer(void);
+
+/**
+ * fill_buffer - Fills the buffer with data from a file descriptor
+ * @fd: File descriptor
+ * Return: Number of bytes read or error indicator
+ */
 int fill_buffer(const int fd);
+
+/**
+ * read_line - Reads a line from the buffer
+ * Return: Pointer to the read line
+ */
 char *read_line();
 
 extern int total_lines;
