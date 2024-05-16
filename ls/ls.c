0c8790fb0e38bdce6465a69f2265d45ca8ff5f1c
matternstsr <matternstsr@gmail.com>
2024-05-15 22:58:04 -0500

    fine ill use the string compare from shell

--- ls/ls.c
+++ ls/ls.c
@@ -8,12 +8,20 @@ int is_dot_or_dotdot(const char *name) {
 	return name[0] == '.' && (name[1] == '\0' || (name[1] == '.' && name[2] == '\0'));
 }
 
+int custom_strcmp(const char *s1, const char *s2) {
+	while (*s1 && *s2 && *s1 == *s2) {
+			s1++;
+			s2++;
+	}
+	return *s1 - *s2;
+}
+
 void bubble_sort(char **arr, int n) {
 	int i, j;
 	char *temp;
 	for (i = 0; i < n - 1; i++) {
 			for (j = 0; j < n - i - 1; j++) {
-					if (strcmp(arr[j], arr[j + 1]) > 0) {
+					if (custom_strcmp(arr[j], arr[j + 1]) > 0) {
 							temp = arr[j];
 							arr[j] = arr[j + 1];
 							arr[j + 1] = temp;
