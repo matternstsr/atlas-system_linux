4a7b010dce46fd5e864016228b7ffbd0d06c73a7
matternstsr <matternstsr@gmail.com>
2024-05-15 21:48:41 -0500

    [strcmp] is not allowed

--- ls/ls.c
+++ ls/ls.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
-#include <string.h>
+
+int is_dot_or_dotdot(const char *name) {
+	return name[0] == '.' && (name[1] == '\0' || (name[1] == '.' && name[2] == '\0'));
+}
 
 int main(void)
 {
-		DIR *dir;
-		struct dirent *entry;
+	DIR *dir;
+	struct dirent *entry;
 
-		dir = opendir(".");
-		if (dir == NULL) {
-				perror("opendir");
-				exit(EXIT_FAILURE);
-		}
+	dir = opendir(".");
+	if (dir == NULL) {
+			perror("opendir");
+			exit(EXIT_FAILURE);
+	}
 
-		while ((entry = readdir(dir)) != NULL) {
-				if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-						printf("%s\n", entry->d_name);
-				}
-		}
+	while ((entry = readdir(dir)) != NULL) {
+			if (!is_dot_or_dotdot(entry->d_name)) {
+					printf("%s\n", entry->d_name);
+			}
+	}
 
-		closedir(dir);
+	closedir(dir);
 
-		return 0;
+	return 0;
 }
