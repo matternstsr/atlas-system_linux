3363f6abc629f728f586bf495f84d7e51a3d06ea
matternstsr <matternstsr@gmail.com>
2024-05-15 22:24:57 -0500

    hmmm

--- ls/ls.c
+++ ls/ls.c
@@ -3,18 +3,21 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <string.h>
 
 int is_dot_or_dotdot(const char *name) {
-	return name[0] == '.' && (name[1] == '\0' || (name[1] == '.' && name[2] == '\0'));
+	return strcmp(name, ".") == 0 || strcmp(name, "..") == 0;
 }
 
-int main(void)
-{
+int compare(const void *a, const void *b) {
+	return strcmp(*(const char **)a, *(const char **)b);
+}
+
+int main(void) {
 	DIR *dir;
-	DIR *subdir;
 	struct dirent *entry;
-	struct dirent *subentry; /*Declaration moved here*/
 	struct stat statbuf;
+	char *files[1000];
 
 	dir = opendir(".");
 	if (dir == NULL) {
@@ -22,32 +25,22 @@ int main(void)
 			exit(EXIT_FAILURE);
 	}
 
+	int num_files = 0;
+
 	while ((entry = readdir(dir)) != NULL) {
 			if (!is_dot_or_dotdot(entry->d_name)) {
-					if (lstat(entry->d_name, &statbuf) == -1) {
-							perror("lstat");
-							exit(EXIT_FAILURE);
-					}
-					if (S_ISDIR(statbuf.st_mode)) {
-							printf("%s:\n", entry->d_name);
-							subdir = opendir(entry->d_name);
-							if (subdir == NULL) {
-									perror("opendir");
-									exit(EXIT_FAILURE);
-							}
-							while ((subentry = readdir(subdir)) != NULL) {
-									if (!is_dot_or_dotdot(subentry->d_name)) {
-											printf("%s\n", subentry->d_name);
-									}
-							}
-							closedir(subdir);
-					} else {
-							printf("%s\n", entry->d_name);
-					}
+					files[num_files++] = strdup(entry->d_name);
 			}
 	}
 
 	closedir(dir);
+
+	qsort(files, num_files, sizeof(char *), compare);
+
+	for (int i = 0; i < num_files; i++) {
+			printf("%s\n", files[i]);
+			free(files[i]);
+	}
 
 	return 0;
 }
