8ad4bd295a6c563f955285d3eb0b097a9fc9f77f
matternstsr <matternstsr@gmail.com>
2024-05-15 21:59:09 -0500

    hmm

--- ls/ls.c
+++ ls/ls.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
-int is_dot_or_dotdot(const char *name) {
+int is_dot_or_dotdot(const char *name) 
+{
 	return name[0] == '.' && (name[1] == '\0' || (name[1] == '.' && name[2] == '\0'));
 }
 
@@ -10,6 +12,7 @@ int main(void)
 {
 	DIR *dir;
 	struct dirent *entry;
+	struct stat statbuf;
 
 	dir = opendir(".");
 	if (dir == NULL) {
@@ -19,7 +22,27 @@ int main(void)
 
 	while ((entry = readdir(dir)) != NULL) {
 			if (!is_dot_or_dotdot(entry->d_name)) {
-					printf("%s\n", entry->d_name);
+					if (stat(entry->d_name, &statbuf) == -1) {
+							perror("stat");
+							exit(EXIT_FAILURE);
+					}
+					if (S_ISDIR(statbuf.st_mode)) {
+							printf("%s:\n", entry->d_name);
+							DIR *subdir = opendir(entry->d_name);
+							if (subdir == NULL) {
+									perror("opendir");
+									exit(EXIT_FAILURE);
+							}
+							struct dirent *subentry;
+							while ((subentry = readdir(subdir)) != NULL) {
+									if (!is_dot_or_dotdot(subentry->d_name)) {
+											printf("%s\n", subentry->d_name);
+									}
+							}
+							closedir(subdir);
+					} else {
+							printf("%s\n", entry->d_name);
+					}
 			}
 	}
 
