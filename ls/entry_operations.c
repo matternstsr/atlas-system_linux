f0e029f688c0dc5a9f9aacaa1240e7745200268e
matternstsr <matternstsr@gmail.com>
2024-05-17 15:10:15 -0500

    not sure thats going to do it

--- ls/entry_operations.c
+++ ls/entry_operations.c
@@ -6,34 +6,39 @@
 int printEntryName(DirectoryReader *reader) {
 	if (!reader || !reader->current_entry)
 			return -1;
-	if (reader->current_entry->d_name[0] != '.') {
-			if (reader->current_entry->d_type == DT_DIR) {
-					printf(ANSI_COLOR_BLUE "%s" ANSI_COLOR_RESET "  ", reader->current_entry->d_name);
-			} else {
-					printf("%s  ", reader->current_entry->d_name);
-			}
+	
+	/* Check if the entry is a directory */
+	int is_directory = (reader->current_entry->d_type == DT_DIR);
+
+	/* Print directories in blue */
+	if (is_directory) {
+			printf("\x1b[34m%s\x1b[0m\n", reader->current_entry->d_name);
+	} else {
+			printf("%s\n", reader->current_entry->d_name);
 	}
+
 	return 0;
 }
 
+
 /* Get directory entry type name */
 const char *getEntryTypeName(unsigned char d_type) {
-	switch (d_type) {
-			case DT_BLK:
-					return "block_device";
-			case DT_CHR:
-					return "character_device";
-			case DT_DIR:
-					return "directory";
-			case DT_FIFO:
-					return "named_pipe";
-			case DT_LNK:
-					return "symbolic_link";
-			case DT_REG:
-					return "file";
-			case DT_SOCK:
-					return "socket";
-			default: /* DT_UNKNOWN */
-					return "unknown";
-	}
+switch (d_type) {
+		case DT_BLK:
+				return "block_device";
+		case DT_CHR:
+				return "character_device";
+		case DT_DIR:
+				return "directory";
+		case DT_FIFO:
+				return "named_pipe";
+		case DT_LNK:
+				return "symbolic_link";
+		case DT_REG:
+				return "file";
+		case DT_SOCK:
+				return "socket";
+		default: /* DT_UNKNOWN */
+				return "unknown";
+}
 }
