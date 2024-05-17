cacbe1643cf1f38e8310fd9182c6e9cb52b2657e
matternstsr <matternstsr@gmail.com>
2024-05-17 17:32:22 -0500

    see where im at withg entryop

--- ls/entry_operations.c
+++ ls/entry_operations.c
@@ -1,53 +1,48 @@
-/* entry_operations.c */
-
 #include "directory_reader.h"
 
 /* Print directory entry name without ANSI escape codes */
-int printEntryName(DirectoryReader *reader) {
+int printEntryName(DirectoryReader *reader)
+{
 	int is_directory;
 	
 	if (!reader || !reader->current_entry)
-			return -1;
+			return (-1);
 
 	/* Skip hidden files (those starting with a dot) */
 	if (reader->current_entry->d_name[0] == '.')
-			return 0;
+			return (0);
 
 	/* Check if the entry is a directory */
 	is_directory = (reader->current_entry->d_type == DT_DIR);
 
 	/* Print directories */
-	if (is_directory) {
-			printf("%s\n", reader->current_entry->d_name);
-	} else {
-			printf("%s\n", reader->current_entry->d_name);
-	}
-
-	return 0;
+	if (is_directory)
+		printf("%s\n", reader->current_entry->d_name);
+	else
+		printf("%s\n", reader->current_entry->d_name);
+	return (0);
 }
-
-
-
-
 
 /* Get directory entry type name */
-const char *getEntryTypeName(unsigned char d_type) {
-switch (d_type) {
+const char *getEntryTypeName(unsigned char d_type)
+{
+	switch (d_type)
+	{
 		case DT_BLK:
-				return "block_device";
+			return ("block_device");
 		case DT_CHR:
-				return "character_device";
+			return ("character_device");
 		case DT_DIR:
-				return "directory";
+				return ("directory");
 		case DT_FIFO:
-				return "named_pipe";
+			return ("named_pipe");
 		case DT_LNK:
-				return "symbolic_link";
+			return ("symbolic_link");
 		case DT_REG:
-				return "file";
+			return ("file");
 		case DT_SOCK:
-				return "socket";
+			return ("socket");
 		default: /* DT_UNKNOWN */
-				return "unknown";
-}
+			return ("unknown");
+	}
 }
