1fe3a712567d660e04d0a105047e5d427686b988
matternstsr <matternstsr@gmail.com>
2024-05-17 17:44:48 -0500

    fixed mattset

--- ls/my_functions.c
+++ ls/my_functions.c
@@ -2,24 +2,27 @@
 #include "directory_reader.h"
 
 /* Custom memset function */
-void *mattset(void *ptr, int value, size_t num) {
-	unsigned char *p = ptr;
-	unsigned char val = (unsigned char)value; /* to ensure value is treated as unsigned */
-	while (num--) {
-			*p++ = val;
-	}
-	return ptr;
+void *mattset(void *ptr, int value, size_t num)
+{
+unsigned char *p = ptr;
+unsigned char val = (unsigned char)value; /* to ensure value is treated as unsigned */
+
+while (num--)
+*p++ = val;
+return (ptr);
 }
 
 /* Custom string comparison function */
-int mattcomp(const char *str1, const char *str2) {
-    while (*str1 && *str2) {
-        char c1 = tolower(*str1); /* Convert characters to lowercase for comparison */
-        char c2 = tolower(*str2);
-        if (c1 != c2)
-            return c1 - c2;
-        str1++;
-        str2++;
-    }
-    return *str1 - *str2;
+int mattcomp(const char *str1, const char *str2)
+{
+while (*str1 && *str2)
+{
+char c1 = tolower(*str1); /* Convert characters to lowercase for comparison */
+char c2 = tolower(*str2);
+if (c1 != c2)
+	return (c1 - c2);
+str1++;
+str2++;
+}
+return (*str1 - *str2);
 }
