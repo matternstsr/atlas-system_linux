a348036d504d1265055e4b333891694ea725144f
matternstsr <matternstsr@gmail.com>
2024-05-18 11:32:13 -0500

    think this will be worse but we will see

--- ls/main.c
+++ ls/main.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <dirent.h>
 
-
 int isDirectory(const char *path) {
     struct stat statbuf;
     if (lstat(path, &statbuf) == -1) {
@@ -19,70 +18,52 @@ int isDirectory(const char *path) {
 int main(int argc, char **argv) {
     int i;
     struct stat statbuf;
-    int has_multiple_dirs = argc > 2;
     DirectoryReader reader;
     const char *path;
     int type;
     int init_result;
 
     if (argc < 2) {
-        /* If no directory path provided, list contents of the current directory */
-        path = ".";
+        fprintf(stderr, "Usage: %s [DIRPATH]...\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    /* Process each directory path provided */
+    for (i = 1; i < argc; i++) {
+        path = argv[i];
         type = isDirectory(path);
+
+        if (type == 0) {
+            if (lstat(path, &statbuf) == -1) {
+                /* fprintf(stderr, "%s: cannot access %s: %s\n",
+                argv[0], path, strerror(errno)); */
+                continue;
+            }
+            printf("%s\n", path); /* Print the path if it's not a directory */
+            continue;
+        }
+
         if (type == 1) { /* Directory */
             if ((init_result = initDirectoryReader(&reader, path)) == -1) {
-                fprintf(stderr, "%s: cannot open directory %s: %s\n", argv[0], path, matt_strerror(errno));
-                return (EXIT_FAILURE);
+                fprintf(stderr, "%s: cannot open directory %s: %s\n", argv[0], path, strerror(errno));
+                return EXIT_FAILURE;
+            }
+
+            if (argc > 2) { /* Print directory path only if there are multiple directories */
+                printf("%s:\n", path);
             }
 
             if (forEachEntry(&reader, printEntryName) == -1) {
-                fprintf(stderr, "%s: error occurred parsing directory %s: %s\n", argv[0], path, matt_strerror(errno));
-                return (EXIT_FAILURE);
+                fprintf(stderr, "%s: error occurred parsing directory %s: %s\n", argv[0], path, strerror(errno));
+                return EXIT_FAILURE;
             }
 
             destroyDirectoryReader(&reader);
-        } else {
-            fprintf(stderr, "Usage: %s [DIRPATH]...\n", argv[0]);
-            return (EXIT_FAILURE);
-        }
-    } else {
-        /* If directory path provided, process each directory path */
-        for (i = 1; i < argc; i++) {
-            path = argv[i];
-            type = isDirectory(path);
 
-            if (type == 0) {
-                if (lstat(path, &statbuf) == -1) {
-                    /* fprintf(stderr, "%s: cannot access %s: %s\n",
-                    argv[0], path, matt_strerror(errno)); */
-                    continue;
-                }
-                printf("%s\n", path); /* Print the path if it's not a directory */
-                continue;
-            }
-
-            if (type == 1) { /* Directory */
-                if ((init_result = initDirectoryReader(&reader, path)) == -1) {
-                    fprintf(stderr, "%s: cannot open directory %s: %s\n", argv[0], path, matt_strerror(errno));
-                    return (EXIT_FAILURE);
-                }
-
-                if (has_multiple_dirs) { /* Print directory path only if there are multiple directories */
-                    printf("%s:\n", path);
-                }
-
-                if (forEachEntry(&reader, printEntryName) == -1) {
-                    fprintf(stderr, "%s: error occurred parsing directory %s: %s\n", argv[0], path, matt_strerror(errno));
-                    return (EXIT_FAILURE);
-                }
-
-                destroyDirectoryReader(&reader);
-
-                if (has_multiple_dirs && i < argc - 1) /* Print new line if there are more directories */
-                    printf("\n");
-            }
+            if (argc > 2 && i < argc - 1) /* Print new line if there are more directories */
+                printf("\n");
         }
     }
 
-    return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
