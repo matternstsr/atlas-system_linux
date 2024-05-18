7cbf36ba7d6168394fe987502a65b9ed2d977994
matternstsr <matternstsr@gmail.com>
2024-05-18 10:25:52 -0500

    not test: on valid folder

--- ls/main.c
+++ ls/main.c
@@ -27,6 +27,7 @@ int main(int argc, char **argv) {
     DirectoryReader reader;
     const char *path;
     int init_result;
+    int hasPrintedHeader = 0; /* Flag to track whether header has been printed */
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s [DIRPATH]...\n", argv[0]);
@@ -37,7 +38,7 @@ int main(int argc, char **argv) {
         path = argv[i];
 
         if (lstat(path, &statbuf) == -1) {
-            /* fprintf(stderr, "%s: cannot access %s: %s\n", argv[0], path, mattError(errno)); */
+            fprintf(stderr, "%s: cannot access %s: %s\n", argv[0], path, mattError(errno));
             continue;
         }
         if (!S_ISDIR(statbuf.st_mode)) {
@@ -47,21 +48,24 @@ int main(int argc, char **argv) {
 
         if ((init_result = initDirectoryReader(&reader, path)) == -1) {
             fprintf(stderr, "%s: cannot open directory %s: %s\n", argv[0], path, mattError(errno));
-            continue; /* Continue to next directory instead of returning immediately */
+            continue;
+        }
+
+        if (hasPrintedHeader) {
+            printf("\n"); /* Print newline before printing header if not the first directory */
+        } else {
+            hasPrintedHeader = 1; /* Set flag to true after printing the first header */
         }
 
         printf("%s:\n", path); /* Print the directory path */
 
         if (forEachEntry(&reader, printEntryName) == -1) {
             fprintf(stderr, "%s: error parsing directory %s: Parsing error\n", argv[0], path);
-            destroyDirectoryReader(&reader); /* Clean up before continuing */
-            continue; /* Continue to next directory */
+            destroyDirectoryReader(&reader);
+            continue;
         }
 
         destroyDirectoryReader(&reader);
-
-        if (i < argc - 1) /* Print new line if there are more directories */
-            printf("\n");
     }
 
     return EXIT_SUCCESS;
