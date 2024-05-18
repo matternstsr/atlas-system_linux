1acaf9d688478e2e36633e30c89c6c2bc8a3610c
matternstsr <matternstsr@gmail.com>
2024-05-17 21:51:47 -0500

    hmmm

--- ls/main.c
+++ ls/main.c
@@ -1,7 +1,9 @@
 /* main.c */
 
-#include "directory_reader.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "directory_reader.h"
 
 /**
  * main - Entry point of the program.
@@ -13,45 +15,46 @@
  *
  * Return: EXIT_SUCCESS if successful, EXIT_FAILURE if an error occurs.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "directory_reader.h"
+
 int main(int argc, char **argv)
 {
-	const char *directory_path;
-	int show_all = 1;
-	DirectoryReader reader;
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s [DIRPATH]...\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	/* Check for correct usage */
-	if (argc > 2)
-	{
-		fprintf(stderr, "Usage: %s [DIRPATH]\n", argv[0]);
-		return (EXIT_FAILURE);
-	}
-	/* Set directory path */
-	if (argc == 2)
-	{
-		directory_path = argv[1];
-		/*  Check if the user wants to show hidden files */
-		if (mattcomp(argv[1], "-a") == 0)
-		{
-		show_all = 0;
-		directory_path = ".";
-		}
-	}
-	else
-		directory_path = "."; /* Default to cur dir if no dir path prov */
-	/* Initialize directory reader */
-	if (initDirectoryReader(&reader, directory_path) == -1)
-	{
-	fprintf(stderr, "Failure opening directory '%s'\n", directory_path);
-	return (EXIT_FAILURE);
-	}
-	reader.show_all = show_all;
-	/* Iterate through directory entries and print them */
-	if (forEachEntry(&reader, printEntryName) == -1)
-	{
-	fprintf(stderr, "Error occurred parsing directory '%s'\n", directory_path);
-	return (EXIT_FAILURE);
-	}
-	/* Cleanup */
-	destroyDirectoryReader(&reader);
-	return (EXIT_SUCCESS);
+    for (int i = 1; i < argc; i++)
+    {
+        const char *directory_path = argv[i];
+        DirectoryReader reader;
+
+        if (strcmp(directory_path, "-a") == 0)
+        {
+            continue; // Skip to the next argument
+        }
+
+        if (initDirectoryReader(&reader, directory_path) == -1)
+        {
+            fprintf(stderr, "Failure opening directory '%s'\n", directory_path);
+            return EXIT_FAILURE;
+        }
+
+        printf("%s:\n", directory_path);
+        if (forEachEntry(&reader, printEntryName) == -1)
+        {
+            fprintf(stderr, "Error occurred parsing directory '%s'\n", directory_path);
+            return EXIT_FAILURE;
+        }
+        
+        destroyDirectoryReader(&reader);
+        printf("\n");
+    }
+
+    return EXIT_SUCCESS;
 }
