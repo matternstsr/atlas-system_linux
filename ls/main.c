14915b2f99b19d837c5135538478d5d76d748bcb
matternstsr <matternstsr@gmail.com>
2024-05-18 10:31:06 -0500

    trying somethign

--- ls/main.c
+++ ls/main.c
@@ -50,7 +50,7 @@ int main(int argc, char **argv) {
             continue; /* Continue to next directory instead of returning immediately */
         }
 
-        printf("%s:\n", path); /* Print the directory path */
+        /*printf("%s:\n", path);  Print the directory path */
 
         if (forEachEntry(&reader, printEntryName) == -1) {
             fprintf(stderr, "%s: error parsing directory %s: Parsing error\n", argv[0], path);
