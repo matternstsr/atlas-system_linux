5488d9bd533526f9407227aecfa8b86523692714
matternstsr <matternstsr@gmail.com>
2024-05-22 09:32:20 -0500

    fixed directory_error_mgmt.c:14: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:15: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:16: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:19: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:20: ERROR: code indent should use tabs where possibledirectory_error_mgmt.c:20: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:21: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:22: ERROR: code indent should use tabs where possibledirectory_error_mgmt.c:22: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:25: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:28: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:31: WARNING: please, no spaces at the start of a linedirectory_error_mgmt.c:31: ERROR: parentheses are required on a return statementdirectory_error_mgmt.c:32: WARNING: adding a line without newline at end of file

--- ls/directory_error_mgmt.c
+++ ls/directory_error_mgmt.c
@@ -10,23 +10,20 @@
  **/
 int error_message_printing(char *filename, char *argv0)
 {
-    /* Declare buffer and error_template variables */
-    char buffer[256];
-    char *error_template;
-    int error_code = 2;
+	/* Declare buffer and error_template variables */
+	char buffer[256];
+	char *error_template;
+	int error_code = 2;
 
-    /* Check errno for specific error code */
-    if (errno == 13)
-        error_template = "%s: cannot open directory %s";
-    else
-        error_template = "%s: cannot access %s";
-
-    /* Format error message using sprintf */
-    sprintf(buffer, error_template, argv0, filename);
-
-    /* Print error message with perror */
-    perror(buffer);
-
-    /* Return the error code */
-    return error_code;
+	/* Check errno for specific error code */
+	if (errno == 13)
+		error_template = "%s: cannot open directory %s";
+	else
+		error_template = "%s: cannot access %s";
+	/* Format error message using sprintf */
+	sprintf(buffer, error_template, argv0, filename);
+	/* Print error message with perror */
+	perror(buffer);
+	/* Return the error code */
+	return (error_code);
 }
