27f1ac1183a5df20b635caaee81907fb49d5b3cd
matternstsr <matternstsr@gmail.com>
2024-06-12 23:51:20 -0500

    I dont see where i went wrong

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -1,120 +1,81 @@
 #!/usr/bin/python3
 
-"""
-read_write_heap.py - Python script to find and replace a string in the heap of a running process.
-
-Usage:
-    read_write_heap.py pid search_string replace_string
-
-Arguments:
-    pid               PID of the running process.
-    search_string     String to search for in the heap (ASCII).
-    replace_string    String to replace the search string with (ASCII).
-
-Description:
-    This script reads the heap memory of a specified process using the /proc filesystem.
-    It searches for a given string in the heap and replaces it with another string.
-
-    The script first locates the heap memory range of the process by reading the /proc/[pid]/maps file.
-    Then, it reads the heap memory from /proc/[pid]/mem, searches for the specified search string,
-    and replaces it with the specified replace string.
-
-    If the search string is found and replaced successfully, it prints "SUCCESS!".
-    If the search string is not found, it prints "FAIL!".
-
-    If any error occurs during the process, it prints an error message and exits with status code 1.
-
-Example:
-    read_write_heap.py 1234 "Hello" "Hi"
-"""
-
-import subprocess
-import time
 import sys
 import os
+import re
 
-def read_value_in_memory(path, addr, l):
+def read_maps(pid):
     """
-    Read a value from memory at a specified address.
+    Read the contents of the '/proc/[pid]/maps' file for the given process ID.
 
     Args:
-        path: File path to read memory from.
-        addr: Address to read from.
-        l: Length of the value to read.
+        pid (int): The process ID.
 
     Returns:
-        The value read from memory.
+        str: The contents of the maps file.
     """
-    value_read = None
+    maps_file_path = "/proc/{}/maps".format(pid)
     try:
-        mem_file = open(path, 'rb')
-        mem_file.seek(addr)
-        value_read = mem_file.read(l)
-        mem_file.close()
-    except Exception as e:
-        print(e)
-    return value_read
+        with open(maps_file_path, 'r') as maps_file:
+            return maps_file.read()
+    except FileNotFoundError:
+        print("Error: Process with PID {} not found.".format(pid))
+        sys.exit(1)
 
-def read_write_heap(pid, search_string, replace_string):
+def find_heap_address(maps_content):
     """
-    Read and modify the heap of a specified process.
+    Find the start and end addresses of the heap from the maps content.
 
     Args:
-        pid: PID of the process.
-        search_string: String to search for in the heap.
-        replace_string: String to replace the search string with.
+        maps_content (str): Contents of the '/proc/[pid]/maps' file.
+
+    Returns:
+        tuple: A tuple containing the start and end addresses of the heap.
     """
-    add_in_memory = None
-    proc_py = None
-    try:
-        maps_filename = f"/proc/{pid}/maps"
-        mem_filename = f"/proc/{pid}/mem"
+    heap_regex = r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) \[heap\]'
+    match = re.search(heap_regex, maps_content)
+    if match:
+        return (int(match.group(1), 16), int(match.group(2), 16))
+    else:
+        print("Error: Heap address not found in process maps.")
+        sys.exit(1)
 
-        with open(maps_filename, mode="r") as maps_file:
-            for line in maps_file:
-                if "[heap]" in line:
-                    sline = line.split(' ')
-                    addr = sline[0].split("-")
-                    addr_start = int(addr[0], 16)
-                    addr_end = int(addr[1], 16)
+def replace_string_in_heap(pid, search_string, replace_string):
+    """
+    Find and replace a string in the heap of a running process.
 
-                    try:
-                        mem_file = open(mem_filename, 'rb')
-                        mem_file.seek(addr_start)
-                        heap = mem_file.read(addr_end - addr_start)
-                        add_in_memory = heap.find(bytes(search_string, "ASCII"))
-                        if add_in_memory != -1:
-                            add_in_memory += addr_start
-                        mem_file.close()
-                    except Exception as e:
-                        print(e)
-                    break
+    Args:
+        pid (int): The process ID.
+        search_string (str): The string to search for in the heap.
+        replace_string (str): The string to replace the found string with.
+    """
+    maps_content = read_maps(pid)
+    start, end = find_heap_address(maps_content)
 
-        if add_in_memory is not None:
-            previous_value = read_value_in_memory(mem_filename, add_in_memory, len(search_string))
-            proc_py = subprocess.Popen(['./read_write_heap.py', str(pid), search_string, replace_string])
-            time.sleep(2)
-            new_value = read_value_in_memory(mem_filename, add_in_memory, len(search_string))
+    with open("/proc/{}/mem".format(pid), 'rb+') as mem_file:
+        mem_file.seek(start)
+        heap_data = mem_file.read(end - start)
 
-            if previous_value != new_value:
-                print("SUCCESS!")
-            else:
-                print("FAIL!")
-    except Exception as e:
-        print(e)
-    finally:
-        if proc_py is not None:
-            proc_py.kill()
-        else:
-            print("FAIL!")  # If proc_py is not defined, consider the operation as failed
+        if bytes(search_string, 'ASCII') not in heap_data:
+            print("Error: Search string not found in the heap of process with PID {}.".format(pid))
+            sys.exit(1)
+
+        mem_file.seek(start + heap_data.index(bytes(search_string, 'ASCII')))
+        mem_file.write(bytes(replace_string, 'ASCII'))
 
 if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
     if len(sys.argv) != 4:
-        print("Usage: read_write_heap.py pid search_string replace_string")
+        print("Usage: {} pid search_string replace_string".format(sys.argv[0]))
         sys.exit(1)
 
     pid = sys.argv[1]
     search_string = sys.argv[2]
     replace_string = sys.argv[3]
 
-    read_write_heap(pid, search_string, replace_string)
+    # Check if the provided PID is a positive integer
+    if not pid.isdigit():
+        print("Error: PID must be a positive integer.")
+        sys.exit(1)
+
+    replace_string_in_heap(int(pid), search_string, replace_string)
