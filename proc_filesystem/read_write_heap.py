0c1a4777407c623f706a9ccf42563aa91280eb2f
matternstsr <matternstsr@gmail.com>
2024-06-12 22:14:39 -0500

    about to give up. dont know where to go with this

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -1,57 +1,64 @@
 #!/usr/bin/python3
-
 """
-Python - Python - /proc filesystem - 0. Hack the VM
-
-	Looks thru the heap of a given process for a str and rep with another one.
+Write a script that finds a string in the heap of a running process, and
+replaces it.
+- Usage: read_write_heap.py pid search_string replace_string
+-- where pid is the pid of the running process
+-- and strings are ASCII
+- The script should look only in the heap of the process
+- Output: you can print whatever you think is interesting
+- On usage error, print an error message on stdout and exit with status code 1
 """
-
-import sys
-import os
-
-def replace_string_in_heap(pid, search_string, replace_string):
-    try:
-        # Open maps file
-        maps_file = open("/proc/{}/maps".format(pid), 'r')
-        for line in maps_file:
-            fields = line.split()
-            if fields[-1] == "[heap]":
-                # Found the heap region
-                start_addr = int(fields[0].split("-")[0], 16)
-                end_addr = int(fields[0].split("-")[1], 16)
-                # Open mem file
-                mem_file = open("/proc/{}/mem".format(pid), 'rb+')
-                mem_file.seek(start_addr)
-                heap_data = mem_file.read(end_addr - start_addr)
-                # Search for the string
-                offset = heap_data.find(search_string.encode())
-                if offset != -1:
-                    mem_file.seek(start_addr + offset)
-                    mem_file.write(replace_string.encode() + b'\0')
-                    print("String replaced successfully.")
-                    mem_file.close()
-                    maps_file.close()
-                    return
-        print("String not found in heap.")
-        maps_file.close()
-    except Exception as e:
-        print("Error:", e)
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: {} pid search_string replace_string".format(sys.argv[0]))
-        sys.exit(1)
-
-    try:
-        pid = int(sys.argv[1])
-    except ValueError:
-        print("Error: pid must be an integer.")
-        sys.exit(1)
-
-    search_string = sys.argv[2]
-    replace_string = sys.argv[3]
-
-    replace_string_in_heap(pid, search_string, replace_string)
 
 if __name__ == "__main__":
-    main()
+    import os
+    import re
+    from sys import argv
+
+    if len(argv) != 4 or len(argv[2]) < len(argv[3]):
+        exit("Usage: ./read_write_heap.py pid search_string replace_string")
+    pid = int(argv[1])
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        exit("PID not found")
+    print(f"[~] PID : {pid}")
+    search_str = argv[2].encode()
+    replace_str = argv[3].encode()
+    try:
+        maps = open(f"/proc/{pid}/maps", "r")
+    except FileNotFoundError as err:
+        exit(err)
+    for line in maps:
+        if re.search(r"^.*\[heap\]$", line):
+            print("[~] HEAP LOCATED")
+            heap, perms, off, dev, inode, path_ = line.split(" ", maxsplit=5)
+            path = path_.strip()
+            print(f"\tPATHNAME: {path}")
+            print(f"\tADDRESS RANGE: {heap}")
+            print(f"\tPERMISSIONS: {perms}")
+            print(f"\tOFFSET: {off}")
+            print(f"\tDEV: {dev}")
+            print(f"\tINODE: {inode}")
+            break
+    maps.close()
+    heap_begin, heap_end = [int(addr, 16) for addr in heap.split("-")]
+    print(f"[~] HEAP BEGIN: [{hex(heap_begin)}] | HEAP END: [{hex(heap_end)}]")
+    try:
+        mem = open(f"/proc/{pid}/mem", "r+b")
+    except FileNotFoundError as err:
+        exit(err)
+    mem.seek(heap_begin)
+    address = mem.read(heap_end - heap_begin).find(search_str) + heap_begin
+    if address >= heap_begin:
+        print(f"[~] \"{argv[2]}\" FOUND AT {hex(address)}")
+        mem.seek(address)
+        mem.write(replace_str)
+        if len(replace_str) < len(search_str):
+            mem.seek(address + len(replace_str))
+            mem.write(b'\0')
+        print(f"[~] \"{argv[3]}\" WRITTEN AT {hex(address)}")
+    else:
+        print(f"[~] \"{argv[2]}\" NOT FOUND")
+    mem.close()
+    exit()
