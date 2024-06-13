b723cc29e29d5929155ad8de1b261ac1caf15848
matternstsr <matternstsr@gmail.com>
2024-06-12 21:39:26 -0500

    not me

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -1,116 +1,84 @@
 #!/usr/bin/python3
-"""
-Python - /proc filesystem - task 0. Hack the VM
 
-    Searches the heap of a process for a given string and replaces it with
-another.
+"""
+Python - Python - /proc filesystem - 0. Hack the VM
+
+	Looks thru the heap of a given process for a str and rep with another one.
 """
 
+import os
+import re
 from sys import argv
 
+def main():
+	# Check for correct command-line arguments
+	if len(argv) != 4 or len(argv[2]) < len(argv[3]):
+			exit("Usage: ./read_write_heap.py pid search_string replace_string")
 
-def read_write_heap(pid, get_str, give_str):
-    """
-    Search the heap from `pid` for `get_str` and replace
-    it with `give_str`.
+	# Extract PID from command-line arguments
+	process_id = int(argv[1])
 
-    Args:
-        pid (int): process id of currently ongoing process' heap to search
-        get_str (str): string to search for in heap
-        give_str (str): string to replace `read_str` with
-    """
+	# Check if PID exists
+	try:
+			os.kill(process_id, 0)
+	except OSError:
+			exit("PID not found")
 
-    flag = False
-    try:
-        num_pid = int(pid)
-    except (TypeError, ValueError):
-        flag = True
-    if num_pid < 0:
-        flag = True
+	# Print process ID
+	print(f"[~] Process ID: {process_id}")
 
-    if flag:
-        print("Missing or invalid pid!")
-        print("Usage: read_write_heap(pid, read_str, write_str)")
-        exit(1)
+	# Encode search and replace strings to bytes
+	search_string = argv[2].encode()
+	replace_string = argv[3].encode()
 
-    if type(get_str) != str or get_str == "":
-        print("Missing or invalid read string!")
-        print("Usage: read_write_heap(pid, read_str, write_str)")
-        exit(1)
+	try:
+			with open(f"/proc/{process_id}/maps", "r") as memory_map:
+					# Iterate through memory map to locate the heap
+					for line in memory_map:
+							if re.search(r"^.*\[heap\]$", line):
+									print("[~] HEAP LOCATED")
+									heap, permissions, offset, device, inode, path = line.split(" ", maxsplit=5)
+									path = path.strip()
+									print(f"\tPATHNAME: {path}")
+									print(f"\tADDRESS RANGE: {heap}")
+									print(f"\tPERMISSIONS: {permissions}")
+									print(f"\tOFFSET: {offset}")
+									print(f"\tDEVICE: {device}")
+									print(f"\tINODE: {inode}")
+									break
 
-    if type(give_str) != str:
-        print("Invalid write string!")
-        print("Usage: read_write_heap(pid, read_str, write_str)")
-        exit(1)
+	except FileNotFoundError as err:
+			exit(err)
 
-    try:
-        maps = open("/proc/{}/maps".format(pid), 'r')
-    except OSError as error:
-        print("Can't open file /proc/{}/maps: OSError: {}".format(pid, error))
-        exit(1)
-    print("[*] maps: /proc/{}/maps".format(pid))
-    print("[*] mem: /proc/{}/mem".format(pid))
+	# Extract the heap begin and end addresses
+	heap_begin, heap_end = [int(addr, 16) for addr in heap.split("-")]
+	print(f"[~] HEAP BEGIN: [{heap_begin:#x}] | HEAP END: [{heap_end:#x}]")
 
-    heap_data = None
-    for line in maps:
-        if "heap" in line:
-            heap_data = line.split()
-    maps.close()
+	try:
+			with open(f"/proc/{process_id}/mem", "r+b") as memory:
+					# Seek to the beginning of the heap in the process memory
+					memory.seek(heap_begin)
+					# Search for the search string in the heap
+					address = memory.read(heap_end - heap_begin).find(search_string) + heap_begin
+					if address >= heap_begin:
+							print(f"[~] \"{argv[2]}\" FOUND AT {address:#x}")
+							memory.seek(address)
+							memory.write(replace_string)
 
-    if heap_data is None:
-        print("No heap found!")
-        exit(1)
-    else:
-        print('\n'.join(("[*] Found: {}:".format(heap_data[-1]),
-                         "\tpathname = {}".format(heap_data[-1]),
-                         "\taddress range = {}".format(heap_data[0]),
-                         "\tpermissions = {}".format(heap_data[1]),
-                         "\toffset (in bytes) = {}".format(heap_data[2]),
-                         "\tinode = {}".format(heap_data[4]))))
+							# If the replace string is shorter, null terminate the remaining bytes
+							if len(replace_string) < len(search_string):
+									memory.seek(address + len(replace_string))
+									memory.write(b'\0')
+							print(f"[~] \"{argv[3]}\" WRITTEN AT {address:#x}")
+							print("SUCCESS!")  # Print "SUCCESS!" upon successful replacement
+					else:
+							print(f"[~] \"{argv[2]}\" NOT FOUND")
+							print("FAIL!")  # Print "FAIL!" if the search string is not found
+	except FileNotFoundError as err:
+			exit(err)
 
-    addr = heap_data[0].split('-')
-    print("[*] Addresses start [{}] | end [{}]".format(addr[0].lstrip('0'),
-                                                       addr[1].lstrip('0')))
-
-    perms = heap_data[1]
-    if 'r' not in perms:
-        print("Heap does not have read permission")
-        exit(0)
-    if 'w' not in perms:
-        print("Heap does not have write permission")
-        exit(0)
-
-    try:
-        mem = open("/proc/{}/mem".format(pid), 'rb+')
-    except OSError as error:
-        print("Can't open file /proc/{}/maps: OSError: {}".format(pid, error))
-        exit(1)
-
-    heap_start = int(addr[0], 16)
-    heap_end = int(addr[1], 16)
-    mem.seek(heap_start)
-    heap = mem.read(heap_end - heap_start)
-    str_offset = heap.find(bytes(get_str, "ASCII"))
-    if str_offset == -1:
-        print("Can't find {} in /proc/{}/mem".format(get_str, pid))
-        exit(1)
-    else:
-        print("[*] Found '{}' at {}".format(get_str, hex(str_offset)[2:]))
-
-    mem.seek(heap_start + str_offset)
-    mem.write(bytes(give_str + '\0', "ASCII"))
-    print("[*] Writing '{}' at {}".format(give_str,
-                                          hex(heap_start + str_offset)[2:]))
-
+	# Exit with a success status code
+	exit(0)
 
 if __name__ == "__main__":
-
-    if (len(argv) != 4):
-        print("Usage: {} <pid> <string to read> "
-              "<string to write>".format(argv[0]))
-        exit(1)
-    else:
-        pid = argv[1]
-        target_str = argv[2]
-        new_str = argv[3]
-        read_write_heap(pid, target_str, new_str)
+	main()
