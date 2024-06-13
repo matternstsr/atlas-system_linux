a9f4c8a170d9f23856b6124cf610e55b088fa21f
matternstsr <matternstsr@gmail.com>
2024-06-12 21:13:31 -0500

    about fried

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -3,60 +3,77 @@
 """
 Python - Python - /proc filesystem - 0. Hack the VM
 
-	Looks through the heap of a given process for string and replaces it with
-another one.
+	Looks thru the heap of a given process for a str and rep with another one.
 """
 
-import sys
 import os
+import re
+from sys import argv
 
-def find_and_replace(pid, search_string, replace_string):
-	"""
-	Finds and replaces a specified string in the heap memory of a process identified by PID.
+def main():
+	# Check for correct command-line arguments
+	if len(argv) != 4 or len(argv[2]) < len(argv[3]):
+		exit("Usage: ./read_write_heap.py pid search_string replace_string")
 
-	Args:
-		pid (int): Process ID of the target process.
-		search_string (str): String to search for in the heap memory.
-		replace_string (str): String to replace the found string with.
+	# Extract PID from command-line arguments
+	process_id = int(argv[1])
 
-	Returns:
-		None
-	"""
+	# Check if PID exists
 	try:
-		with open(f"/proc/{pid}/maps", "r") as mem_map:
-			for line in mem_map:
-				if line.endswith("[heap]\n"):
-					heap_start, heap_end = \
-						[int(x, 16) for x in line.split(" ")[0].split("-")]
-	except FileNotFoundError as error:
-		sys.exit(error)
-	print("Target memory location found")
+		os.kill(process_id, 0)
+	except OSError:
+		exit("PID not found")
+
+	# Print process ID
+	print(f"[~] Process ID: {process_id}")
+
+	# Encode search and replace strings to bytes
+	search_string = argv[2].encode()
+	replace_string = argv[3].encode()
 
 	try:
-		with open(f"/proc/{pid}/mem", "r+b") as heap_mem:
-			heap_mem.seek(heap_start)
-			mem = heap_mem.read(heap_end - heap_start)
-			str_at = mem.find(search_string.encode()) + heap_start
-			print(f"{search_string}:{str_at}")
-			if str_at > -1:
-				print(f"Target Located at {hex(str_at)}")
-				if len(search_string) > len(replace_string):
-					heap_mem.seek(str_at + len(replace_string))
-					heap_mem.write(b'\0')
-				else:
-					heap_mem.seek(str_at)
-					heap_mem.write(replace_string.encode())
-				print("Target Replaced")
+		with open(f"/proc/{process_id}/maps", "r") as memory_map:
+			# Iterate through memory map to locate the heap
+			for line in memory_map:
+				if re.search(r"^.*\[heap\]$", line):
+					print("[~] HEAP LOCATED")
+					heap, permissions, offset, device, inode, path = line.split(" ", maxsplit=5)
+					path = path.strip()
+					print(f"\tPATHNAME: {path}")
+					print(f"\tADDRESS RANGE: {heap}")
+					print(f"\tPERMISSIONS: {permissions}")
+					print(f"\tOFFSET: {offset}")
+					print(f"\tDEVICE: {device}")
+					print(f"\tINODE: {inode}")
+					break
+
+	except FileNotFoundError as err:
+		exit(err)
+
+	# Extract the heap begin and end addresses
+	heap_begin, heap_end = [int(addr, 16) for addr in heap.split("-")]
+	print(f"[~] HEAP BEGIN: [{heap_begin:#x}] | HEAP END: [{heap_end:#x}]")
+
+	try:
+		with open(f"/proc/{process_id}/mem", "r+b") as memory:
+			# Seek to the beginning of the heap in the process memory
+			memory.seek(heap_begin)
+			# Search for the search string in the heap
+			address = memory.read(heap_end - heap_begin).find(search_string) + heap_begin
+			if address >= heap_begin:
+				print(f"[~] \"{argv[2]}\" FOUND AT {address:#x}")
+				memory.seek(address)
+				memory.write(replace_string)
+				
+				# If the replace string is shorter, null terminate the remaining bytes
+				if len(replace_string) < len(search_string):
+					memory.seek(address + len(replace_string))
+					memory.write(b'\0')
+				print(f"[~] \"{argv[3]}\" WRITTEN AT {address:#x}")
 			else:
-				print("Unable to locate Target")
-	except FileNotFoundError as error:
-		sys.exit(error)
-	print("Mission Complete")
+				print(f"[~] \"{argv[2]}\" NOT FOUND")
+	except FileNotFoundError as err:
+		exit(err)
 
 if __name__ == "__main__":
-	if len(sys.argv) != 4:
-		sys.exit("Unsupported number of arguments")
-	pid = int(sys.argv[1])
-	search_str = sys.argv[2]
-	replace_str = sys.argv[3]
-	find_and_replace(pid, search_str, replace_str)
+	main()
