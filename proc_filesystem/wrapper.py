bd23aeda8f5c0338c66cdfb900489b7744c5134b
matternstsr <matternstsr@gmail.com>
2024-06-12 21:48:55 -0500

    wrapper2

--- proc_filesystem/wrapper.py
+++ proc_filesystem/wrapper.py
@@ -1,6 +1,8 @@
 import subprocess
 
 try:
-    subprocess.run(['./main_0.py'])
-except NameError:
-    print("NameError caught: proc_py is not defined.")
+    result = subprocess.run(['./main_0.py'], stderr=subprocess.PIPE, text=True)
+    if "NameError" in result.stderr:
+        print("NameError caught: proc_py is not defined.")
+except Exception as e:
+    print("An error occurred:", e)
