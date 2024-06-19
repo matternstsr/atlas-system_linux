0eabc5fdd8ce2bde3a63b1a17be7a824bd05886a
matternstsr <matternstsr@gmail.com>
2024-06-19 12:45:24 -0500

    i dint know why it hates that it works local

--- readelf/1-hreadelf.c
+++ readelf/1-hreadelf.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <elf.h>
+#include <elf.h> /* ELF data structures */
 
 void print_section_headers(FILE *file, Elf64_Ehdr ehdr) {
-    fseek(file, ehdr.e_shoff, SEEK_SET); 
-    Elf64_Shdr *shdr = (Elf64_Shdr *)malloc(ehdr.e_shentsize * ehdr.e_shnum);
+    Elf64_Shdr *shdr;
+    int i;
+
+    fseek(file, ehdr.e_shoff, SEEK_SET); /* Move to section header table offset */
+
+    /* Read section headers */
+    shdr = (Elf64_Shdr *)malloc(ehdr.e_shentsize * ehdr.e_shnum);
     fread(shdr, ehdr.e_shentsize, ehdr.e_shnum, file);
+
+    /* Print section headers */
     printf("There are %d section headers, starting at offset 0x%lx:\n\n", ehdr.e_shnum, ehdr.e_shoff);
     printf("Section Headers:\n");
     printf("  [Nr] Name              Type            Address          Off    Size   ES Flg Lk Inf Al\n");
 
-    for (int i = 0; i < ehdr.e_shnum; ++i) {
+    for (i = 0; i < ehdr.e_shnum; ++i) {
         printf("  [%2d] %-17s %-15s %016lx %06lx %06lx %02lx %c%c %3d %3d %2ld\n",
             i,
-            "", 
-            "", 
+            "", /* Name (we'll fetch this from the string table later) */
+            "", /* Type (we'll convert the type code) */
             (unsigned long)shdr[i].sh_addr,
             (unsigned long)shdr[i].sh_offset,
             (unsigned long)shdr[i].sh_size,
@@ -32,18 +39,20 @@ void print_section_headers(FILE *file, Elf64_Ehdr ehdr) {
 }
 
 int main(int argc, char *argv[]) {
+    FILE *file;
+    Elf64_Ehdr ehdr;
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
         return 1;
     }
 
-    FILE *file = fopen(argv[1], "rb");
+    file = fopen(argv[1], "rb");
     if (!file) {
         perror("Error opening file");
         return 1;
     }
 
-    Elf64_Ehdr ehdr;
     fread(&ehdr, sizeof(ehdr), 1, file);
 
     print_section_headers(file, ehdr);
