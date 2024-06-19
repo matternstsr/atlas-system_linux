30023c9c5112b08ffe7810c7d0fce64287cbcd14
matternstsr <matternstsr@gmail.com>
2024-06-19 03:05:30 -0500

    hmm

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -1,15 +1,44 @@
-/* main.c */
+#include "notelf.h"
 
-#include "myelf.h"
-
-/* Main function to execute the program */
 int main(int argc, char *argv[]) {
-    if (argc != 2) { /* Check for correct number of arguments */
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    int fd;
+    unsigned char ident[EI_NIDENT];
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <elf-file>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    sparcbigendian32(argv[1]); /* Call function to print ELF header */
+    /* Open ELF file */
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return EXIT_FAILURE;
+    }
+
+    /* Read ELF header identification bytes */
+    if (read(fd, ident, EI_NIDENT) != EI_NIDENT) {
+        perror("read");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    /* Close file */
+    close(fd);
+
+    /* Determine and print ELF header information */
+    if (ident[EI_CLASS] == ELFCLASS64) {
+        readelf_header64(argv[1]);
+    } else if (ident[EI_CLASS] == ELFCLASS32) {
+        if (ident[EI_DATA] == ELFDATA2MSB && ident[EI_MAG3] == ELFMAG3) {
+            sparcbigendian32(argv[1]);
+        } else {
+            readelf_header32(argv[1]);
+        }
+    } else {
+        fprintf(stderr, "Unknown ELF class\n");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
