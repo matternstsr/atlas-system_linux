ea2332889bcb891457e7cb9e0362dea8c7b6b99c
matternstsr <matternstsr@gmail.com>
2024-06-19 12:57:57 -0500

    trying

--- readelf/1-hreadelf.c
+++ readelf/1-hreadelf.c
@@ -9,22 +9,22 @@ void print_section_headers(FILE *file, Elf64_Ehdr ehdr) {
     char *shstrtab;
     int i;
 
-    // Move to section header table offset
+    /* Move to section header table offset */
     fseek(file, ehdr.e_shoff, SEEK_SET);
 
-    // Read section headers
+     /* Read section headers */
     shdr = (Elf64_Shdr *)malloc(ehdr.e_shentsize * ehdr.e_shnum);
     fread(shdr, ehdr.e_shentsize, ehdr.e_shnum, file);
 
-    // Find string table section header
+     /* Find string table section header */
     strtab_shdr = &shdr[ehdr.e_shstrndx];
 
-    // Allocate memory for section header string table
+     /* Allocate memory for section header string table */
     shstrtab = (char *)malloc(strtab_shdr->sh_size);
     fseek(file, strtab_shdr->sh_offset, SEEK_SET);
     fread(shstrtab, strtab_shdr->sh_size, 1, file);
 
-    // Print section headers
+     /* Print section headers */
     printf("There are %d section headers, starting at offset 0x%lx:\n\n", ehdr.e_shnum, ehdr.e_shoff);
     printf("Section Headers:\n");
     printf("  [Nr] Name              Type            Address          Off    Size   ES Flg Lk Inf Al\n");
@@ -32,8 +32,8 @@ void print_section_headers(FILE *file, Elf64_Ehdr ehdr) {
     for (i = 0; i < ehdr.e_shnum; ++i) {
         printf("  [%2d] %-17s %-15s %016lx %06lx %06lx %02lx %c%c %3d %3d %2ld\n",
             i,
-            &shstrtab[shdr[i].sh_name], // Section name from string table
-            "", // Placeholder for type (to be filled)
+            &shstrtab[shdr[i].sh_name],  /* Section name from string table */
+            "",  /* Placeholder for type (to be filled) */
             (unsigned long)shdr[i].sh_addr,
             (unsigned long)shdr[i].sh_offset,
             (unsigned long)shdr[i].sh_size,
