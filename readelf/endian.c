6a5f5e5eb6307a123e2b0098361e76cf0a0b6f7c
matternstsr <matternstsr@gmail.com>
2024-06-20 14:36:52 -0500

    seewhathappens

--- readelf/endian.c
+++ readelf/endian.c
@@ -18,58 +18,81 @@ uint32_t swap32(uint32_t val, bool swap)
 	return (val);
 }
 
-/* Function to read and print ELF header for Sparc big endian 32-bit ELF */
+/* Function to read ELF header for Sparc big endian 32-bit ELF */
+int read_sparcbigendian32(const char *filename, Elf32_Ehdr *ehdr)
+{
+    int fd;
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        perror("open");
+        return -1;
+    }
+
+    if (read(fd, ehdr, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
+    {
+        perror("read");
+        close(fd);
+        return -1;
+    }
+
+    close(fd);
+    return 0;
+}
+
+/* Function to print ELF header information */
+void print_elf_header(const Elf32_Ehdr *ehdr)
+{
+    int i;
+
+    /* Print ELF Header */
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (i = 0; i < EI_NIDENT; ++i)
+        printf("%02x ", ehdr->e_ident[i]);
+    printf("\n");
+    printf("  Class:                             ELF32\n");
+    printf("  Data:                              2's complement, big endian\n");
+    printf("  Version:                           %u (current)\n",
+           (unsigned int)ehdr->e_ident[EI_VERSION]);
+    print_osabi(ehdr->e_ident[EI_OSABI]);
+    printf("  ABI Version:                       %u\n",
+           (unsigned int)ehdr->e_ident[EI_ABIVERSION]);
+    print_file_type(bswap_16(ehdr->e_type));
+    print_machine_type(bswap_16(ehdr->e_machine));
+    printf("  Version:                           0x%x\n",
+           bswap_32(ehdr->e_version));
+    printf("  Entry point address:               0x%lx\n",
+           (unsigned long)bswap_32(ehdr->e_entry));
+    printf("  Start of program headers:          %u (bytes into file)\n",
+           bswap_32(ehdr->e_phoff));
+    printf("  Start of section headers:          %u (bytes into file)\n",
+           bswap_32(ehdr->e_shoff));
+    printf("  Flags:                             0x%x\n",
+           bswap_32(ehdr->e_flags));
+    printf("  Size of this header:               %u (bytes)\n",
+           bswap_16(ehdr->e_ehsize));
+    printf("  Size of program headers:           %u (bytes)\n",
+           bswap_16(ehdr->e_phentsize));
+    printf("  Number of program headers:         %u\n", bswap_16(ehdr->e_phnum));
+    printf("  Size of section headers:           %u (bytes)\n",
+           bswap_16(ehdr->e_shentsize));
+    printf("  Number of section headers:         %u\n", bswap_16(ehdr->e_shnum));
+    printf("  Section header string table index: %u\n",
+           bswap_16(ehdr->e_shstrndx));
+}
+
+/* Wrapper function to combine reading and printing ELF header */
 void sparcbigendian32(const char *filename)
 {
-	int fd, i;
-	Elf32_Ehdr ehdr;
+    Elf32_Ehdr ehdr;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		perror("open");
-		exit(EXIT_FAILURE);
-	}
-	if (read(fd, &ehdr, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
-	{
-		perror("read");
-		close(fd);
-		exit(EXIT_FAILURE);
-	}
-	close(fd);
-	/* Print ELF Header */
-	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	for (i = 0; i < EI_NIDENT; ++i)
-		printf("%02x ", ehdr.e_ident[i]);
-	printf("\n");
-	printf("  Class:                             ELF32\n");
-	printf("  Data:                              2's complement, big endian\n");
-	printf("  Version:                           %u (current)\n",
-				(unsigned int)ehdr.e_ident[EI_VERSION]);
-	print_osabi(ehdr.e_ident[EI_OSABI]);
-	printf("  ABI Version:                       %u\n",
-				(unsigned int)ehdr.e_ident[EI_ABIVERSION]);
-	print_file_type(bswap_16(ehdr.e_type));
-	print_machine_type(bswap_16(ehdr.e_machine));
-	printf("  Version:                           0x%x\n",
-				bswap_32(ehdr.e_version));
-	printf("  Entry point address:               0x%lx\n",
-				(unsigned long)bswap_32(ehdr.e_entry));
-	printf("  Start of program headers:          %u (bytes into file)\n",
-				bswap_32(ehdr.e_phoff));
-	printf("  Start of section headers:          %u (bytes into file)\n",
-				bswap_32(ehdr.e_shoff));
-	printf("  Flags:                             0x%x\n",
-				bswap_32(ehdr.e_flags));
-	printf("  Size of this header:               %u (bytes)\n",
-				bswap_16(ehdr.e_ehsize));
-	printf("  Size of program headers:           %u (bytes)\n",
-				bswap_16(ehdr.e_phentsize));
-	printf("  Number of program headers:         %u\n", bswap_16(ehdr.e_phnum));
-	printf("  Size of section headers:           %u (bytes)\n",
-				bswap_16(ehdr.e_shentsize));
-	printf("  Number of section headers:         %u\n", bswap_16(ehdr.e_shnum));
-	printf("  Section header string table index: %u\n",
-				bswap_16(ehdr.e_shstrndx));
+    if (read_sparcbigendian32(filename, &ehdr) == -1)
+    {
+        fprintf(stderr, "Error reading ELF header from %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    print_elf_header(&ehdr);
 }
