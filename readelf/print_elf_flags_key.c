efc774b274bcec7721bcc0226f8f75230e7dc619
matternstsr <matternstsr@gmail.com>
2024-06-19 23:58:28 -0500

    dont tell me its a space?

--- readelf/print_elf_flags_key.c
+++ readelf/print_elf_flags_key.c
@@ -5,7 +5,7 @@ void print_elf_flags_key(void)
     printf("Key to Flags:\n");
     printf("  W (write), A (alloc), X (execute), M (merge), S (strings), l (large)\n");
     printf("  I (info), L (link order), G (group), T (TLS), E (exclude), x (unknown)\n");
-    printf("  O (extra OS processing required), o (OS specific), p (processor specific)\n");
+    printf("  O (extra OS processing required), o (OS specific), p (processor specific) \n");
 }
 
 void print_flag_key(void)
