1fdffee010bc301696e1626e216f575e2866a45c
matternstsr <matternstr@gmail.com>
2024-06-25 21:48:12 +0000

    annoying

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -2,49 +2,63 @@ global asm_strcmp
 section .text
 
 asm_strcmp:
-	push rbp
-	mov rbp, rsp
-	mov QWORD [rbp - 8], rdi
-	mov QWORD [rbp - 16], rsi
-	jmp .test_s1_v_s2
-.strings_to_cmp:
-	add QWORD [rbp - 8], 1
-	add QWORD [rbp - 16], 1
-.test_s1_v_s2:
-	mov rax, QWORD [rbp - 8]
-	movzx eax, BYTE [rax]
-	test al, al
-	je .test_s1_more_s2
-	mov rax, QWORD [rbp - 16]
-	movzx eax, BYTE [rax]
-	test al, al
-	je .test_s1_more_s2
-	mov rax, QWORD [rbp - 8]
-	movzx edx, BYTE [rax]
-	mov rax, QWORD [rbp - 16]
-	movzx eax, BYTE [rax]
-	cmp dl, al
-	je .strings_to_cmp
-.test_s1_more_s2:
-	mov rax, QWORD [rbp - 8]
-	movzx edx, BYTE [rax]
-	mov rax, QWORD [rbp - 16]
-	movzx eax, BYTE [rax]
-	cmp dl, al
-	jle .test_s1_less_s2
-	mov eax, 1
-	jmp .return
-.test_s1_less_s2:
-	mov rax, QWORD [rbp - 8]
-	movzx edx, BYTE [rax]
-	mov rax, QWORD [rbp - 16]
-	movzx eax, BYTE [rax]
-	cmp dl, al
-	jge .return_0
-	mov eax, -1
-	jmp .return
+    push rbp
+    mov QWORD PTR [rbp - 8], rdi
+    mov QWORD PTR [rbp - 16], rsi
+    jmp .compare_strings
+
+.compare_strings:
+    ; Load and compare bytes from s1 and s2
+    mov rax, QWORD PTR [rbp - 8]
+    movzx eax, BYTE PTR [rax]
+    test al, al
+    je .check_s1_more_s2
+
+    mov rax, QWORD PTR [rbp - 16]
+    movzx eax, BYTE PTR [rax]
+    test al, al
+    je .check_s1_more_s2
+
+    mov rax, QWORD PTR [rbp - 8]
+    movzx edx, BYTE PTR [rax]
+
+    mov rax, QWORD PTR [rbp - 16]
+    movzx eax, BYTE PTR [rax]
+
+    cmp dl, al
+    je .compare_strings
+
+.check_s1_more_s2:
+    ; Check if s1 > s2
+    mov rax, QWORD PTR [rbp - 8]
+    movzx edx, BYTE PTR [rax]
+
+    mov rax, QWORD PTR [rbp - 16]
+    movzx eax, BYTE PTR [rax]
+
+    cmp dl, al
+    jle .check_s1_less_s2
+
+    mov eax, 1 ; Return 1 if s1 > s2
+    jmp .return
+
+.check_s1_less_s2:
+    ; Check if s1 < s2
+    mov rax, QWORD PTR [rbp - 8]
+    movzx edx, BYTE PTR [rax]
+
+    mov rax, QWORD PTR [rbp - 16]
+    movzx eax, BYTE PTR [rax]
+
+    cmp dl, al
+    jge .return_0
+
+    mov eax, -1 ; Return -1 if s1 < s2
+    jmp .return
+
 .return_0:
-	mov eax, 0
+    mov eax, 0 ; Return 0 if s1 == s2
+
 .return:
-	pop rbp
-	ret
+    pop rbp
+    ret
