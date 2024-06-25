a6db7b3186bfbdd5ac612e4018dc0f090874fdce
matternstsr <matternstr@gmail.com>
2024-06-25 19:07:14 +0000

    cleaned up and see where thats at

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -3,39 +3,55 @@ global asm_strcmp
 section .text
 
 asm_strcmp:
-    ; Prologue
-    push rbp     ; Save the current base pointer onto the stack
-    mov rbp, rsp ; Set up a new base pointer
+    push rbp
+    mov rbp, rsp
 
-    ; Arguments passed in registers:
-    ; rdi = s1 (first string)
-    ; rsi = s2 (second string)
-
-    xor rcx, rcx        ; Initialize rcx to zero (counter for the loop)
+    ; Load parameters s1 and s2
+    mov rdi, QWORD [rbp + 8]     ; rdi = s1
+    mov rsi, QWORD [rbp + 16]    ; rsi = s2
 
 .loop_compare:
     ; Load characters from s1 and s2
-    mov al, [rdi + rcx] ; Load byte from s1 into al
-    mov dl, [rsi + rcx] ; Load byte from s2 into dl
+    mov al, BYTE [rdi]           ; Load byte from s1 into al
+    mov dl, BYTE [rsi]           ; Load byte from s2 into dl
 
     ; Compare characters
-    cmp al, dl
-    jne .end_loop       ; If characters are not equal, exit loop
+    cmp al, 0                    ; Check if *s1 (al) is null terminator
+    je .return_result            ; If *s1 is null, jump to return result
+    cmp dl, 0                    ; Check if *s2 (dl) is null terminator
+    je .return_result            ; If *s2 is null, jump to return result
+    cmp al, dl                   ; Compare characters
+    jne .decide_result           ; If charc are not =, jump to decide result
 
-    ; Check if end of string (null terminator)
-    test al, al         ; Check if al (current char of s1) is zero
-    jz .end_loop        ; If zero, end of both strings reached
+    ; Increment pointers and continue loop
+    inc rdi                      ; Increment s1
+    inc rsi                      ; Increment s2
+    jmp .loop_compare
 
-    ; Increment counter and loop
-    inc rcx             ; rcx is the register that is being incremented
-    jmp .loop_compare   ; "goto" begining (loop_compare)
+.decide_result:
+    ; Compare results based on character comparison
+    movzx eax, al                ; Zero-extend al (s1 character) into eax
+    movzx edx, dl                ; Zero-extend dl (s2 character) into edx
+    sub eax, edx                 ; Calc diff between s1 and s2 characters
+    jmp .return                  ; Jump to return with result
 
-.end_loop:
-    ; Calculate result based on comparison
-    mov rax, 0
-    mov al, [rdi + rcx] ; Load next character from s1
-    sub al, [rsi + rcx] ; Subtract character from s2
-    ret
+.return_result:
+    ; Return based on the comparison of s1 and s2
+    mov eax, 0                   ; Default result is 0 (equal)
+    cmp al, dl                   ; Compare characters
+    jg .greater_than             ; If s1 > s2, jump to greater_than
+    jl .less_than                ; If s1 < s2, jump to less_than
 
+.greater_than:
+    ; Return 1 if s1 > s2
+    mov eax, 1                   ; Result is 1
+    jmp .return                  ; Jump to return with result
+
+.less_than:
+    ; Return -1 if s1 < s2
+    mov eax, -1                  ; Result is -1
+
+.return:
     ; Epilogue
-    pop rbp             ; used to remove (pop) the topmost value
+    pop rbp                      ; Restore base pointer
+    ret                          ; Return to caller with result in eax
