c5b8f9047d15d0275938e25a4666d381a35fa092
matternstsr <matternstr@gmail.com>
2024-06-25 21:28:12 +0000

    trying to find the good one that I had

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -1,80 +1,49 @@
 global asm_strcmp
 section .text
+
 asm_strcmp:
     push rbp
     mov rbp, rsp
-    mov QWORD [rbp - 8], rdi   ; s1
-    mov QWORD [rbp - 16], rsi  ; s2
+    mov rdi, QWORD [rbp + 16]  ; s1
+    mov rsi, QWORD [rbp + 24]  ; s2
 
-    ; Handle case where both strings are empty
-    mov rax, QWORD [rbp - 8]
-    test rax, rax
-    je .s1_empty
-    mov rax, QWORD [rbp - 16]
-    test rax, rax
-    je .s2_empty
+    ; Check if both strings are empty
+    test rdi, rdi
+    jz .check_s2_empty
+    test rsi, rsi
+    jz .s1_not_empty_s2_empty
 
     ; Compare strings character by character
-.test_loop:
-    mov rax, QWORD [rbp - 8]
-    movzx eax, BYTE [rax]
-    test al, al
-    je .strings_equal
-
-    mov rax, QWORD [rbp - 16]
-    movzx eax, BYTE [rax]
-    test al, al
-    je .strings_equal
-
-    mov rax, QWORD [rbp - 8]
-    movzx edx, BYTE [rax]
-    mov rax, QWORD [rbp - 16]
-    movzx eax, BYTE [rax]
-    cmp dl, al
+.compare_loop:
+    mov al, BYTE [rdi]
+    cmp al, BYTE [rsi]
     jne .strings_not_equal
+    test al, al
+    jz .strings_equal
 
-    add QWORD [rbp - 8], 1
-    add QWORD [rbp - 16], 1
-    jmp .test_loop
+    inc rdi
+    inc rsi
+    jmp .compare_loop
 
 .strings_equal:
     mov eax, 0
     jmp .return
 
 .strings_not_equal:
-    mov rax, QWORD [rbp - 8]
-    movzx edx, BYTE [rax]
-    mov rax, QWORD [rbp - 16]
-    movzx eax, BYTE [rax]
-    cmp dl, al
-    jl .s1_less_than_s2
-    jg .s1_greater_than_s2
+    movzx eax, BYTE [rdi]
+    movzx edx, BYTE [rsi]
+    sub eax, edx
+    jmp .return
 
-.s1_less_than_s2:
+.check_s2_empty:
+    test rsi, rsi
+    jz .strings_equal
     mov eax, -1
     jmp .return
 
-.s1_greater_than_s2:
+.s1_not_empty_s2_empty:
     mov eax, 1
-    jmp .return
-
-.s1_empty:
-    mov rax, QWORD [rbp - 16]
-    test rax, rax
-    je .strings_equal
-    mov eax, -1
-    jmp .return
-
-.s2_empty:
-    mov eax, 1
-    jmp .return
 
 .return:
     pop rbp
     ret
-
-;Correct output - case: asm_strcmp("Holberton School", "Holberton School")
-;Correct output - case: asm_strcmp("", "")
-;Correct output - case: Comparing a very long string with itself
-;Correct output - case: Comparing a very long string with itself (1 char differ)
-;both betty
