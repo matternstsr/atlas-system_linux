abf6966cf891dbebea56380e270a76965a5c456b
matternstsr <matternstr@gmail.com>
2024-06-25 19:13:52 +0000

    hmm that is odd

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -10,23 +10,42 @@ asm_strcmp:
     mov rdi, QWORD [rbp + 8]     ; rdi = s1
     mov rsi, QWORD [rbp + 16]    ; rsi = s2
 
-.loop_compare:
-    ; Load characters from s1 and s2
+    ; Handle special case where both strings are empty ("")
     mov al, BYTE [rdi]           ; Load byte from s1 into al
-    mov dl, BYTE [rsi]           ; Load byte from s2 into dl
+    test al, al                  ; Test if *s1 is null terminator
+    jz .check_s2_empty           ; Jump to check if s2 is empty if s1 is empty
 
+    mov dl, BYTE [rsi]           ; Load byte from s2 into dl
+    test dl, dl                  ; Test if *s2 is null terminator
+    jz .return_result            ; If s1 not empty & s2 is, ret 1 (s1 > s2)
+
+.loop_compare:
     ; Compare characters
-    cmp al, 0                    ; Check if *s1 (al) is null terminator
-    je .return_result            ; If *s1 is null, jump to return result
-    cmp dl, 0                    ; Check if *s2 (dl) is null terminator
-    je .return_result            ; If *s2 is null, jump to return result
     cmp al, dl                   ; Compare characters
-    jne .decide_result           ; If charc are not =, jump to decide result
+    jne .decide_result           ; If chars are not equal, jump to decide result
 
     ; Increment pointers and continue loop
     inc rdi                      ; Increment s1
+    mov al, BYTE [rdi]           ; Load byte from updated s1 into al
+    test al, al                  ; Test if *s1 is null terminator
+    jz .check_s2_empty           ; if s1 reaches end & s2 is empty Jump to check
+
     inc rsi                      ; Increment s2
+    mov dl, BYTE [rsi]           ; Load byte from updated s2 into dl
+    test dl, dl                  ; Test if *s2 is null terminator
+    jz .return_result            ; If s1 not empty and s2 is, ret -1 (s1 < s2)
+
     jmp .loop_compare
+
+.check_s2_empty:
+    ; Check if s2 is empty
+    mov dl, BYTE [rsi]           ; Load byte from s2 into dl
+    test dl, dl                  ; Test if *s2 is null terminator
+    jz .return_result            ; If both s1 & s2 are empty, ret 0 (s1 == s2)
+
+    ; s1 is empty but s2 is not empty, return -1 (s1 < s2)
+    mov eax, -1
+    jmp .return
 
 .decide_result:
     ; Compare results based on character comparison
@@ -37,19 +56,22 @@ asm_strcmp:
 
 .return_result:
     ; Return based on the comparison of s1 and s2
-    mov eax, 0                   ; Default result is 0 (equal)
     cmp al, dl                   ; Compare characters
     jg .greater_than             ; If s1 > s2, jump to greater_than
     jl .less_than                ; If s1 < s2, jump to less_than
 
+    ; s1 and s2 are equal
+    mov eax, 0
+    jmp .return
+
 .greater_than:
     ; Return 1 if s1 > s2
-    mov eax, 1                   ; Result is 1
-    jmp .return                  ; Jump to return with result
+    mov eax, 1
+    jmp .return
 
 .less_than:
     ; Return -1 if s1 < s2
-    mov eax, -1                  ; Result is -1
+    mov eax, -1
 
 .return:
     ; Epilogue
