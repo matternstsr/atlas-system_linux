4163d42c84cba598b4749e25c562c3cda9315520
matternstsr <matternstr@gmail.com>
2024-06-25 21:13:09 +0000

    go back!

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -1,4 +1,5 @@
 global asm_strcmp
+
 section .text
 
 asm_strcmp:
@@ -6,39 +7,29 @@ asm_strcmp:
     push rbp
     mov rbp, rsp
 
-    ; Load parameters s1, s2, and n
-    mov rdi, QWORD [rbp + 8]     ; rdi = s1
-    mov rsi, QWORD [rbp + 16]    ; rsi = s2
-    mov rdx, QWORD [rbp + 24]    ; rdx = n
-
-    ; Initialize counters and result storage
-    xor rcx, rcx                 ; i = 0 (counter)
-    mov eax, 0                   ; diff = 0 (result)
+    ; Load parameters s1 and s2
+    mov rdi, QWORD [rbp + 16]     ; rdi = s1
+    mov rsi, QWORD [rbp + 24]     ; rsi = s2
 
     ; Comparison loop
 .loop_compare:
-    ; Check if i < n (compare up to n characters)
-    cmp rcx, rdx
-    jge .return_result           ; If i >= n, jump to return result
-
     ; Load characters from s1 and s2
     mov al, BYTE [rdi]           ; Load s1 character into al
     mov dl, BYTE [rsi]           ; Load s2 character into dl
 
     ; Check for null terminators
-    test al, al
-    je .return_result            ; If *s1 (al) is null, jump to return result
-    test dl, dl
-    je .return_result            ; If *s2 (dl) is null, jump to return result
+    cmp al, 0                    ; Check if *s1 (al) is null
+    je .return_result            ; If *s1 is null, jump to return result
+    cmp dl, 0                    ; Check if *s2 (dl) is null
+    je .return_result            ; If *s2 is null, jump to return result
 
     ; Compare characters
     cmp al, dl                   ; Compare s1 and s2 characters
     jne .decide_result           ; If not equal, decide result
 
-    ; Increment pointers and counters
+    ; Increment pointers and continue loop
     inc rdi                      ; Increment s1 pointer
     inc rsi                      ; Increment s2 pointer
-    inc rcx                      ; Increment counter
     jmp .loop_compare            ; Continue comparison loop
 
 .decide_result:
@@ -61,6 +52,7 @@ asm_strcmp:
 .greater_than:
     ; Return 1 if s1 > s2
     mov eax, 1
+    jmp .return                  ; Jump to return
 
 .less_than:
     ; Return -1 if s1 < s2
