97d595db86811890e28aa1a62d17d0c8c9945d75
matternstsr <matternstr@gmail.com>
2024-06-25 17:48:30 +0000

    goign back

--- libasm/0-strlen.asm
+++ libasm/0-strlen.asm
@@ -1,26 +1,17 @@
-BITS 64
-
 global asm_strlen
 
 section .text
 
 asm_strlen:
-    push rbp                    ; Save the base pointer
-    mov rbp, rsp                ; Set up the base pointer
+    xor rax, rax         ; Clear rax (set rax to zero, which will hold the length)
+.loop_start:
+    movzx rcx, BYTE [rdi] ; Load the byte at the address stored in rdi into rcx
+    test rcx, rcx         ; Test if rcx (current byte) is zero
+    jz .end               ; If rcx is zero, jump to .end (end of string)
 
-    mov QWORD [rbp - 8], 0      ; Initialize len to 0
-
-.loop:
-    mov rax, QWORD [rbp + 16]   ; Load str into rax (first argument)
-    movzx eax, byte [rax]       ; Load byte at [rax] into eax, zero-extend to 32 bits
-    test al, al                 ; Check if AL (current byte) is zero (null terminator)
-    jz .end                     ; If zero, exit loop
-
-    add QWORD [rbp - 8], 1      ; Increment len
-    add QWORD [rbp + 16], 1     ; Move to the next character in str
-    jmp .loop                   ; Repeat loop
+    inc rdi               ; Move to the next character (increment rdi)
+    inc rax               ; Increment the length counter
+    jmp .loop_start       ; Jump back to .loop_start to continue the loop
 
 .end:
-    mov rax, QWORD [rbp - 8]    ; Load len into rax
-    pop rbp                     ; Restore rbp
-    ret                         ; Return with rax holding the length of str
+    ret                    ; Return with the length of the string in rax
