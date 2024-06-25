bf7f92c2032bfad4ae1a8ff45df142cab108a0f2
matternstsr <matternstr@gmail.com>
2024-06-25 22:17:31 +0000

    changed the vals

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -5,47 +5,31 @@ asm_strcmp:
     push rbp
     mov rbp, rsp
 
-    ; Function prologue
-    mov rdi, QWORD [rbp + 8]   ; First argument (s1)
-    mov rsi, QWORD [rbp + 16]   ; Second argument (s2)
-
 .compare_strings:
     ; Load and compare bytes from s1 and s2
-    mov al, BYTE [rdi]
-    test al, al
-    je .check_s1_more_s2
-
-    mov dl, BYTE [rsi]
-    test dl, dl
-    je .check_s1_more_s2
-
+    mov al, [rdi]
+    mov dl, [rsi]
+    
+    ; If characters differ, return difference
     cmp al, dl
-    jne .return_compare_result
-
-    ; Increment pointers
+    jne .return_diff
+    
+    ; If both characters are null, strings are equal
+    test al, al
+    je .return_0
+    
+    ; Increment pointers and continue comparison
     inc rdi
     inc rsi
     jmp .compare_strings
 
-.check_s1_more_s2:
-    ; Check if s1 > s2
-    mov al, BYTE [rdi]
-    test al, al
-    jz .return_0 ; s1 is empty, so equal if s2 is also empty
-
-    mov eax, -1 ; Return -1 if s1 > s2
+.return_diff:
+    sub eax, edx    ; Return difference between characters
     jmp .return
-
-.return_compare_result:
-    ; Compare result in al (-1 if s1 < s2, 1 if s1 > s2)
-    cmp al, dl
-    setg al ; al = 1 if s1 > s2
-    setl al ; al = -1 if s1 < s2
 
 .return_0:
-    mov eax, 1 ; Return 1 if s1 == s2
-    jmp .return
-
+    xor eax, eax    ; Return 0 (strings are equal)
+    
 .return:
     pop rbp
     ret
