f5f3be624a87ab7b430b6ab51b8f85af009c845d
Matt <matternstsr@gmail.com>
2024-06-27 12:30:55 -0500

    started strncmp

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -30,13 +30,13 @@ asm_strncmp:
     cmp al, dl
     jl .less_than   ; al < dl
     jg .greater_than ; al > dl
+    cmp rdx, 0    ; Check if reached n
+    jz .equal
 
     ; Characters are equal, move to next
     inc rdi
     inc rsi
     dec rdx
-    cmp rdx, 0    ; Check if reached n
-    jz .equal
 
     jmp .asm_strncmp_loop
 
