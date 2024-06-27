3348a151abe415ea5fd6c2460814a7d9f714745a
Matt <matternstsr@gmail.com>
2024-06-27 11:09:05 -0500

    redid the loop. wasnst actually going through the loop was exiting with NOTHING.

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -8,44 +8,46 @@ asm_strcmp:
     ; Arguments:
     ; rdi = first string (S1)
     ; rsi = second string (S2)
+    xor eax, eax   ; Initialize eax (return value) to 0
 
-    ; Initialize return value to 0
-    xor eax, eax
+.asm_strcmp_loop:
+    ; Load byte from S1 and S2
+    mov al, byte [rdi]
+    mov dl, byte [rsi]
 
-    .compare_loop:
-        ; Load bytes from S1 and S2
-        mov al, byte [rdi]
-        mov dl, byte [rsi]
+    ; Check if both characters are null
+    test al, al
+    jz .end_of_s1
+    test dl, dl
+    jz .end_of_s2
 
-        ; Compare bytes
-        cmp al, dl
-        jne .not_equal
+    ; Compare bytes
+    cmp al, dl
+    jl .end_of_s1
+    jg .end_of_s2
 
-        ; Check if end of S1 ('\0')
-        test al, al
-        jz .strings_equal
+    ; Characters are equal, move to next
+    inc rdi
+    inc rsi
+    jmp .asm_strcmp_loop
 
-        ; Move to next characters
-        inc rdi
-        inc rsi
 
-        ; Continue loop
-        jmp .compare_loop
+.end_of_s1:
+    ; End of S1 reached
+    test dl, dl
+    jz .equal_and_end  ; Both are null, strings are equal
+    mov eax, -1       ; S1 is null, S2 is not null
+    jmp .exit
 
-    .not_equal:
-        ; Calculate the difference
-        sub al, dl
-        jmp .exit
+.end_of_s2:
+    ; End of S2 reached
+    mov eax, 1        ; S1 is not null, S2 is null
+    jmp .exit
 
-    .strings_equal:
-        ; Both strings are equal, check for termination
-        test dl, dl
-        jz .exit
+.equal_and_end:
+    ; Both strings are null
+    xor eax, eax      ; Return 0 (strings are equal)
 
-        ; S1 is shorter than S2
-        mov eax, -1
-        jmp .exit
-
-    .exit:
-        pop rbp
-        ret
+.exit:
+    pop rbp
+    ret
