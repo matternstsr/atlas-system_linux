8f838ba0d5e0aa996cefde9661520ee1b50cb2ea
Matt <matternstsr@gmail.com>
2024-06-28 01:46:33 -0500

    not sure im getting anywhere

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,58 +1,45 @@
-BITS 64
-
 global asm_strncmp
+
 section .text
 
-asm_strncmp:
+.asm_strncmp:
     push rbp
     mov rbp, rsp
+    push rbx
+    jmp .start
 
-    ; Arguments:
-    ; rdi = first string (S1)
-    ; rsi = second string (S2)
-    ; rdx = size_t n (number of characters to compare)
+.start:
+    test rdx, rdx
+    jz .isnull
+    jmp .next
 
-    xor eax, eax   ; Initialize eax (return value) to 0
+.isnull:
+    cmp bl, al
+    jl .isnega
+    xor rax, rax
+    jmp .end
 
-    ; Loop through the strings
-.asm_strncmp_loop:
-    ; Load byte from S1 and S2
-    mov al, byte [rdi]
-    mov dl, byte [rsi]
+.isnega:
+    mov rax, -1
+    jmp .end
 
-    cmp al, dl
-    jl .less_than   ; al < dl
-    jg .greater_than ; al > dl
+.ispos:
+    mov rax, 1
 
-    ; Characters are equal, move to next
+.next:
+    movzx ebx, byte [rdi]
+    movzx eax, byte [rsi]
+    cmp bl, 0x00
+    jz .isnull
+    cmp bl, al
+    jg .ispos
+    jl .isnega
     inc rdi
     inc rsi
-    inc eax       ; Increment counter for remaining characters
-	test edx, eax
-	je .same
-    jmp .asm_strncmp_loop  ; Continue loop
+    dec rdx
+    jnz .next
 
-.less_than:
-    ; S1 < S2
-	cmp edx, eax
-	je .same
-	xor eax, eax
-    mov eax, -1
-    jmp .exit
-
-.greater_than:
-    ; S1 > S2
-	cmp edx, eax
-	je .same
-	xor eax, eax
-    mov eax, 1
-    jmp .exit
-
-.same:
-    ; Strings are equal up to n characters
-    xor eax, eax
-	mov eax, 0
-
-.exit:
+.end:
+    pop rbx
     pop rbp
     ret
