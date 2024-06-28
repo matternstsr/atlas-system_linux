acdf2354f8b8b1a30ba3783018e911779fb5ed71
Matt <matternstsr@gmail.com>
2024-06-28 12:08:47 -0500

    only one syscall

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -11,12 +11,10 @@ asm_putc:
     ; Set up syscall to write character to stdout
     mov rdi, 1            ; file descriptor 1 (stdout)
     mov rsi, rsp          ; pointer to the character to print
-    mov BYTE [rsi], dil   ; move the character to the lower byte of rsi
+    ;mov BYTE [rsi], dil   ; move the character to the lower byte of rsi
     mov rdx, 1            ; number of bytes to write (just 1 byte)
     mov rax, 1            ; syscall number for write
-
     syscall               ; invoke syscall to write to stdout
-
     ; Cleanup and return
     pop rdi               ; Restore rdi
     pop rbp               ; Function epilogue
