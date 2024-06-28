d34e0365b5e562f560ae35368e7aad97dd621c3a
Matt <matternstsr@gmail.com>
2024-06-28 11:47:25 -0500

    move insteaD

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -10,12 +10,14 @@ global asm_putc
 
 asm_putc:
     ; Prepare arguments for write call
-    mov rax, 1          ; syscall number for write
-    mov rdi, 1          ; file descriptor 1 (stdout)
-    mov rsi, rsp        ; pointer to the character to print (in this case, in the lower 8 bits of rdi)
-    mov BYTE [rsp], dil ; move character to the lower byte of rsi
-    mov rdx, 1          ; number of bytes to write (just 1 byte)
-    
-.LOOP:
-    syscall             
-    cmp eax
+    mov rax, 1           ; syscall number for write
+    mov rdi, 1           ; file descriptor 1 (stdout)
+    mov rsi, rsp         ; pointer to the character to print (in this case, in the lower 8 bits of rsi)
+    mov BYTE [rsp], dil  ; move the character to the lower byte of rsi
+    mov rdx, 1           ; number of bytes to write (just 1 byte)
+
+    syscall              ; invoke syscall to write to stdout
+
+    ; Return 1 byte written (always 1 for a single character)
+    mov rax, 1
+    ret
