f874e95129b71b3b40b5a44acc73adf0a45e0b0a
Matt <matternstsr@gmail.com>
2024-06-28 01:50:18 -0500

    going back out of that rabbit hole

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,44 +1,45 @@
 global asm_strncmp
 
 section .text
-asm_strncmp:
+
+.asm_strncmp:
     push rbp
     mov rbp, rsp
     push rbx
-
-    ; Start of function
-    cmp rdx, 0        ; Compare n with 0
-    jz .isnull        ; Jump to .isnull if n is 0
-    jmp .next         ; Otherwise, jump to .next
-
-.isnull:
-    cmp byte [rdi], 0 ; Compare byte in *s1 with 0
-    jl .isnega        ; Jump to .isnega if *s1 < 0
-    xor rax, rax      ; Set return value to 0 (equal strings)
-    jmp .end          ; Jump to .end
-
-.isnega:
-    mov rax, -1       ; Set return value to -1 (*s1 < *s2)
-    jmp .end          ; Jump to .end
-
-.ispos:
-    mov rax, 1        ; Set return value to 1 (*s1 > *s2)
+    jmp .start
 
 .next:
-    movzx ebx, byte [rdi] ; Load byte from *s1 into bl
-    movzx eax, byte [rsi] ; Load byte from *s2 into al
-    cmp bl, 0x00       ; Compare byte in bl with 0
-    jz .isnull         ; Jump to .isnull if bl is 0
-    cmp bl, al         ; Compare bl and al
-    jg .ispos          ; Jump to .ispos if bl > al
-    jl .isnega         ; Jump to .isnega if bl < al
-    inc rdi            ; Increment s1 pointer
-    inc rsi            ; Increment s2 pointer
-    dec rdx            ; Decrement n
-    jnz .next          ; Jump to .next if n != 0
+    movzx ebx, byte [rdi]     ; Load byte from [rdi] (first string) into ebx
+    movzx eax, byte [rsi]     ; Load byte from [rsi] (second string) into eax
+    cmp bl, 0x00               ; Compare byte from [rdi] to null terminator
+    jz .isnull                 ; Jump to .isnull if end of first string reached
+    cmp bl, al                 ; Compare bytes from [rdi] and [rsi]
+    jg .ispos                  ; Jump to .ispos if byte from [rdi] > byte from [rsi]
+    jl .isnega                 ; Jump to .isnega if byte from [rdi] < byte from [rsi]
+    inc rdi                    ; Increment pointer of first string
+    inc rsi                    ; Increment pointer of second string
+    dec rdx                    ; Decrement length counter
+    jnz .next                  ; Jump to .next if length counter is not zero
+
+.isnull:
+    cmp bl, al                 ; Compare bytes from [rdi] and [rsi] again at end
+    jl .isnega                 ; Jump to .isnega if byte from [rdi] < byte from [rsi]
+    xor rax, rax               ; Set rax to 0 (strings are equal)
+    jmp .end                   ; Jump to .end
+
+.isnega:
+    mov rax, -1                ; Set rax to -1 (first string is less than second)
+    jmp .end                   ; Jump to .end
+
+.ispos:
+    mov rax, 1                 ; Set rax to 1 (first string is greater than second)
 
 .end:
-    pop rbx
-    pop rbp
-    ret                ; Return
+    pop rbx                    ; Restore rbx
+    pop rbp                    ; Restore rbp
+    ret                        ; Return from function
 
+.start:
+    test rdx, rdx              ; Test if rdx (length) is zero
+    jz .isnull                 ; Jump to .isnull if length is zero
+    jmp .next                  ; Otherwise, start comparison loop
