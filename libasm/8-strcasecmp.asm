0fececdf457dca433e685853bfb819514f4be7c5
Matt <matternstsr@gmail.com>
2024-06-28 14:53:02 -0500

    hmm

--- libasm/8-strcasecmp.asm
+++ libasm/8-strcasecmp.asm
@@ -1,56 +1,70 @@
 BITS 64
+
 global asm_strcasecmp
 section .text
-
 ; Arguments:
 ;   rdi = pointer to first string (S1)
 ;   rsi = pointer to second string (S2)
+;   rdx = size_t n (number of characters to compare)
 
 asm_strcasecmp:
-    xor eax, eax        ; Clear eax (return value)
-    xor rcx, rcx        ; Clear rcx (index)
+	push rbp
+	mov rbp, rsp
+	xor rax, rax
+	push rbx
+	push rcx
 
-.LOOP:
-    movzx r8, BYTE [rdi+rcx]   ; Load byte from S1[rcx] into r8
-    movzx r9, BYTE [rsi+rcx]   ; Load byte from S2[rcx] into r9
+.BOOP:
+    movzx r8d, BYTE [rdi]   ; Load byte from S1[rcx] into r8
+    movzx r9d, BYTE [rsi]   ; Load byte from S2[rcx] into r9
 
-    ; Check for end of strings
-    test r8b, r8b
-    jz .END              ; If r8b is 0, strings are equal
-    test r9b, r9b
-    jz .END              ; If r9b is 0, strings are equal
+	cmp r8b, 0x00
+	jz .calc
+	cmp r9b, 0x00
+	jz .calc
 
+.convert:
     ; Convert r8 to lowercase if it's an uppercase letter
-    cmp r8b, 'A'
-    jl .COMPARE
-    cmp r8b, 'Z'
-    jg .COMPARE
+    cmp r8b, 65
+    jl .CHECK_LOWER1
+    cmp r8b, 90
+    jg .CHECK_LOWER1
     add r8b, 32
 
+.CHECK_LOWER1:
     ; Convert r9 to lowercase if it's an uppercase letter
-    cmp r9b, 'A'
+    cmp r9b, 65
     jl .COMPARE
-    cmp r9b, 'Z'
+    cmp r9b, 90
     jg .COMPARE
     add r9b, 32
 
 .COMPARE:
     ; Compare characters after conversion
     cmp r8b, r9b
-    jne .DIFFERENT       ; If characters are different, exit
+    je .next_chars     ; If equal, check end condition
 
-    ; Move to next character
-    inc rcx              ; Move to next character index
-    jmp .LOOP            ; Repeat loop
+.calc:
+    ; Characters are different
+	xor rax, rax
+	sub r8b, r9b
+	movsx rax, r8b
+    jmp .END             ; Exit loop
 
-.DIFFERENT:
-    ; Calculate difference and return
-    movsx eax, r8b       ; Move r8b (as signed integer) into eax
-    movsx ebx, r9b       ; Move r9b (as signed integer) into ebx
-    sub eax, ebx         ; Subtract ebx from eax (character difference)
-    ret                  ; Exit function
+.next_chars:
+    inc rdi
+	inc rsi
+    jmp .BOOP
 
 .END:
-    ; Return zero if strings are equal in length
-    xor eax, eax
-    ret
+	pop rcx
+	pop rbx
+	pop rbp
+    ret                  ; Return from function
+
+
+
+
+
+
+
