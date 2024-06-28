fa197de4936778d9c9b6e00d54a9630760e23808
Matt <matternstsr@gmail.com>
2024-06-28 01:24:34 -0500

    not sure im getting anywhere

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,31 +1,58 @@
 BITS 64
 
-section .text
 global asm_strncmp
+section .text
 
 asm_strncmp:
+    push rbp
+    mov rbp, rsp
+
+    ; Arguments:
+    ; rdi = first string (S1)
+    ; rsi = second string (S2)
+    ; rdx = size_t n (number of characters to compare)
+
+    xor eax, eax   ; Initialize eax (return value) to 0
+
+    ; Loop through the strings
+.asm_strncmp_loop:
+    ; Load byte from S1 and S2
+    mov al, byte [rdi]
+    mov dl, byte [rsi]
+
+    cmp al, dl
+    jl .less_than   ; al < dl
+    jg .greater_than ; al > dl
+
+    ; Characters are equal, move to next
+    inc rdi
+    inc rsi
+    inc eax       ; Increment counter for remaining characters
+	test edx, eax
+	je .same
+    jmp .asm_strncmp_loop  ; Continue loop
+
+.less_than:
+    ; S1 < S2
+	cmp edx, eax
+	je .same
+	xor eax, eax
+    mov eax, -1
+    jmp .exit
+
+.greater_than:
+    ; S1 > S2
+	cmp edx, eax
+	je .same
+	xor eax, eax
+    mov eax, 1
+    jmp .exit
+
+.same:
+    ; Strings are equal up to n characters
     xor eax, eax
-    xor rcx, rcx
-    jmp .loop
+	mov eax, 0
 
-.loop:
-    cmp rcx, rdx
-    je .end
-
-    mov bl, BYTE [rdi + rcx]
-    mov r8b, BYTE [rsi + rcx]
-    cmp bl, r8b
-    je .continue
-
-.notsame:
-    movsx eax, bl
-    movsx ebx, r8b
-    sub eax, ebx
-    jmp .end
-
-.continue:
-    inc rcx
-    jmp .loop
-
-.end:
+.exit:
+    pop rbp
     ret
