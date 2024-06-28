9236231deab0a8ae41a1cb6bf80b1270544ad111
Matt <matternstsr@gmail.com>
2024-06-28 11:27:16 -0500

    cant use done?

--- libasm/5-memcpy.asm
+++ libasm/5-memcpy.asm
@@ -1,3 +1,5 @@
+BITS 64
+
 section .text
 global asm_memcpy
 
@@ -9,17 +11,21 @@ global asm_memcpy
 ;;   rdx = n (number of bytes to copy)
 ;; Returns:
 ;;   rax = dest (pointer to the beginning of the destination buffer)
-asm_memcpy:
-    XOR ECX, ECX       ; Clear ECX register (ECX = 0)
-    MOV EAX, EDI       ; Move destination pointer (EDI) to EAX (EAX = dest)
+memcpy:
+    xor rcx, rcx        ; Clear rcx (counter)
+    jmp .LOOP           ; Jump to start the copying loop
 
-.loop:
-    CMP ECX, EDX       ; Compare ECX (counter) with EDX (number of bytes to copy)
-    JE end             ; If ECX == EDX, jump to end (copying finished)
+.INCR:
+    inc rcx             ; Increment rcx (counter)
 
-    MOV EDX, [ESI]     ; Move 4 bytes from source pointer (ESI) to EDX
-    MOV [EAX], EDX     ; Move 4 bytes from EDX to destination pointer (EAX)
+.LOOP:
+    cmp rcx, rdx        ; Compare rcx (counter) with rdx (number of bytes)
+    je .END             ; If rcx == rdx, jump to end (copying finished)
 
-    INC ESI            ; Increment source pointer (ESI)
-    INC EAX            ; Increment destination pointer (EAX)
-    JMP .loop          ; Jump back to .loop to continue copying
+    mov al, BYTE [rsi+rcx]      ; Load 1 byte from src+rcx to al
+    mov BYTE [rdi+rcx], al      ; Store 1 byte from al to dest+rcx
+    jmp .INCR           ; Jump to increment counter and continue copying
+
+.END:
+    mov rax, rdi        ; Move dest to rax (return value)
+    ret                 ; Return from function
