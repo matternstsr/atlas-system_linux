caad3e01eb56a18aa9bcad6560d3d6ccb058dd59
Matt <matternstsr@gmail.com>
2024-06-28 12:51:41 -0500

    MOre

--- libasm/0-strlen.asm
+++ libasm/0-strlen.asm
@@ -1,17 +1,30 @@
+BITS 64
+
 global asm_strlen
 
 section .text
+	;; size_t asm_strlen(const char *str)
+	;; {
+	;;         size_t len = 0;
+	;;         for (; *str; str++)
+	;;                 len++;
+	;;         return (len);
+	;; }
 
 asm_strlen:
-    xor rax, rax          ; Clear rax (set rax to zero, which will hold the length)
-.loop_start:
-    movzx rcx, BYTE [rdi] ; Load the byte at the address stored in rdi into rcx
-    test rcx, rcx         ; Test if rcx (current byte) is zero
-    jz .end               ; If rcx is zero, jump to .end (end of string)
-
-    inc rdi               ; Move to the next character (increment rdi)
-    inc rax               ; Increment the length counter
-    jmp .loop_start       ; Jump back to .loop_start to continue the loop
-
-.end:
-    ret                    ; Return with the length of the string in rax
+	push rbp
+	mov rbp, rsp
+	mov QWORD [rbp - 24], rdi
+	mov QWORD [rbp - 8], 0
+	jmp .testing_str
+.length_of_str:
+	add QWORD [rbp - 8], 1
+	add QWORD [rbp - 24], 1
+.testing_str:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	test al, al
+	jne .length_of_str
+	mov rax, QWORD [rbp - 8]
+	pop rbp
+	ret
