d7379920183912526e044e984479c313f801db1b
Matt <matternstsr@gmail.com>
2024-06-27 23:58:58 -0500

    was good before this

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -3,66 +3,56 @@ BITS 64
 global asm_strncmp
 section .text
 
-;; int asm_strncmp(const char *s1, const char *s2, size_t n)
 asm_strncmp:
     push rbp
     mov rbp, rsp
-    sub rsp, 16                ; Reserve space on stack for local variables
 
-    mov rsi, rdi               ; s1
-    mov rdi, rsi               ; s2
-    mov rcx, rdx               ; n
+    ; Arguments:
+    ; rdi = first string (S1)
+    ; rsi = second string (S2)
+    ; rdx = size_t n (number of characters to compare)
 
-    xor rax, rax               ; diff
-    xor rdx, rdx               ; i
+    xor eax, eax   ; Initialize eax (return value) to 0
 
-.loop:
-    cmp edx, ecx               ; Compare i with n
-    jae .end_loop
+    ; Loop through the strings
+.asm_strncmp_loop:
+    ; Load byte from S1 and S2
+    mov al, byte [rdi]
+    mov dl, byte [rsi]
 
-    mov al, [rsi + rdx]        ; Load *s1
-    mov bl, [rdi + rdx]        ; Load *s2
+    cmp al, dl
+    jl .less_than   ; al < dl
+    jg .greater_than ; al > dl
 
-    cmp al, bl                 ; Compare characters
-    jne .check_diff
+    ; Characters are equal, move to next
+    inc rdi
+    inc rsi
+    inc eax       ; Increment counter for remaining characters
+	test edx, eax
+	jge .same
+    jmp .asm_strncmp_loop  ; Continue loop
 
-    test al, al                ; Check if end of string s1
-    jz .diff_true
-
-    inc rdx                    ; Increment i
-
-    jmp .loop
-
-.check_diff:
-    movsx rax, al              ; Convert to signed
-    movsx rbx, bl              ; Convert to signed
-    sub rax, rbx               ; Calculate difference
-    jmp .return_diff           ; Jump to return_diff
-
-.diff_true:
-    cmp rax, 0
-    jg .return_positive
-
-    cmp rax, 0
-    jl .return_negative
-
-.return_zero:
-    xor eax, eax
-    jmp .return
-
-.return_positive:
-    mov eax, 1
-    jmp .return
-
-.return_negative:
+.less_than:
+    ; S1 < S2
+	cmp edx, eax
+	je .same
+	xor eax, eax
     mov eax, -1
-    jmp .return
+    jmp .exit
 
-.end_loop:
-    mov eax, edx               ; Return i (number of characters compared)
-.return:
-    leave
+.greater_than:
+    ; S1 > S2
+	cmp edx, eax
+	je .same
+	xor eax, eax
+    mov eax, 1
+    jmp .exit
+
+.same:
+    ; Strings are equal up to n characters
+    xor eax, eax
+	mov eax, 0
+
+.exit:
+    pop rbp
     ret
-
-.return_diff:
-    jmp .check_diff             ; Define the label for return_diff
