ebc6185b2af755bfef5c57d5749bb9cc60427a40
Matt <matternstsr@gmail.com>
2024-06-28 02:09:21 -0500

    ahhhhhhhhhhhhhhhhhhhhhhhhh

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,45 +1,59 @@
+BITS 64
+
 global asm_strncmp
 
 section .text
 
-.asm_strncmp:
-    push rbp
-    mov rbp, rsp
-    push rbx
-    jmp .start
+asm_strncmp:
+    ; Function prologue
+    push rbp         ; Save base pointer
+    mov rbp, rsp     ; Set up base pointer
+    push rbx         ; Save rbx register which will be used in the function
 
-.next:
-    movzx ebx, byte [rdi]     ; Load byte from [rdi] (first string) into ebx
-    movzx eax, byte [rsi]     ; Load byte from [rsi] (second string) into eax
-    cmp bl, 0x00               ; Compare byte from [rdi] to null terminator
-    jz .isnull                 ; Jump to .isnull if end of first string reached
-    cmp bl, al                 ; Compare bytes from [rdi] and [rsi]
-    jg .ispos                  ; Jump to .ispos if byte from [rdi] > byte from [rsi]
-    jl .isnega                 ; Jump to .isnega if byte from [rdi] < byte from [rsi]
-    inc rdi                    ; Increment pointer of first string
-    inc rsi                    ; Increment pointer of second string
-    dec rdx                    ; Decrement length counter
-    jnz .next                  ; Jump to .next if length counter is not zero
+next:
+    ; Load characters from s1 (rdi) and s2 (rsi) into ebx (s1) and eax (s2)
+    movzx ebx, byte [rdi]
+    movzx eax, byte [rsi]
+    
+    ; Check for null termination of s1 (rdi)
+    cmp bl, 0x00
+    jz isnull        ; If s1 is null terminated, handle accordingly
+    
+    ; Compare characters in s1 (bl) and s2 (al)
+    cmp bl, al
+    jg ispos         ; If s1 > s2, jump to ispos
+    jl isnega        ; If s1 < s2, jump to isnega
+    
+    ; Increment pointers to s1 (rdi) and s2 (rsi), decrement counter (rdx)
+    inc rdi
+    inc rsi
+    dec rdx
+    cmp rdx, 0
+    ; Check if more characters need to be compared
+    jnz isnull         ; If rdx is not zero, continue comparing
+    jmp next; Loop it
+    
+isnull:
+    ; Check for null termination of s2 (rsi)
+    cmp bl, al
+    jl isnega       ; If s1 < s2 and s1 is null terminated, jump to isnega
+    
+    ; Strings are equal up to n characters
+    xor rax, rax    ; Set return value to 0 (equal)
+    jmp endit         ; Jump to function epilogue
 
-.isnull:
-    cmp bl, al                 ; Compare bytes from [rdi] and [rsi] again at end
-    jl .isnega                 ; Jump to .isnega if byte from [rdi] < byte from [rsi]
-    xor rax, rax               ; Set rax to 0 (strings are equal)
-    jmp .end                   ; Jump to .end
+isnega:
+    ; s1 < s2
+    mov rax, -1     ; Set return value to -1
+    jmp endit         ; Jump to function epilogue
 
-.isnega:
-    mov rax, -1                ; Set rax to -1 (first string is less than second)
-    jmp .end                   ; Jump to .end
+ispos:
+    ; s1 > s2
+    mov rax, 1      ; Set return value to 1
+    jmp endit         ; Jump to function epilogue
 
-.ispos:
-    mov rax, 1                 ; Set rax to 1 (first string is greater than second)
-
-.end:
-    pop rbx                    ; Restore rbx
-    pop rbp                    ; Restore rbp
-    ret                        ; Return from function
-
-.start:
-    test rdx, rdx              ; Test if rdx (length) is zero
-    jz .isnull                 ; Jump to .isnull if length is zero
-    jmp .next                  ; Otherwise, start comparison loop
+endit:
+    ; Function epilogue
+    pop rbx         ; Restore rbx register
+    pop rbp         ; Restore base pointer
+    ret             ; exit out
