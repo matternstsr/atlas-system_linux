6dda306a1b0d808ac9b3f2bde47817f71db637b9
Matt <matternstsr@gmail.com>
2024-06-28 10:04:54 -0500

    3

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -1,62 +1,26 @@
-BITS 64
+section .text
 
 global asm_strstr
-section .text
-    
+
 asm_strstr:
     push rbp
     mov rbp, rsp
     push rbx
+    push rdi
+    push rsi
+    push rdx
 
-    ; Null check for haystack or needle
+    ; Load parameters into registers
+    mov rdi, rdi ; rdi = haystack (string to search)
+    mov rsi, rsi ; rsi = needle (substring to find)
+
+    ; If haystack or needle is NULL, return NULL
     test rdi, rdi
     jz .return_null
     test rsi, rsi
-    jz .return_haystack
-
-    ; Get length of needle
-    mov rcx, 0
-.get_length:
-    cmp byte [rsi + rcx], 0
-    jz .outer_loop
-
-    inc rcx
-    jmp .get_length
-
-.outer_loop:
-    ; Move to the next character in haystack
-    inc rdi
-    mov rcx, 0
-
-.inner_loop:
-    cmp byte [rdi + rcx], byte [rsi + rcx]
-    jnz .end_of_search
-
-    inc rcx
-    cmp byte [rsi + rcx], 0
-    jz .return_found
-
-    jmp .inner_loop
-
-.return_found:
-    mov rax, rdi
-    jmp .exit
-
-.return_null:
-    xor rax, rax ; Return NULL if haystack is NULL
-    jmp .exit
-
-.return_haystack:
-    mov rax, rdi ; Return haystack if needle is NULL
-    jmp .exit
-
-.end_of_search:
-    cmp byte [rdi], 0 ; End of haystack reached
     jz .return_null
 
-    jmp .outer_loop
-
-.exit:
-    pop rbx
-    pop rbp
-    ret
+    ; Get lengths of haystack and needle
+    mov rcx, rdi ; rcx = haystack (use rcx to iterate through haystack)
+    mov rdx, rsi ; rdx = needle (use rdx to iterate through needle)
+    call strlen   ;get length of haystack
