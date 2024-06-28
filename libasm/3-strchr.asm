2bab46c90d70b16718f371d0d6c81b32d11fb8fd
Matt <matternstsr@gmail.com>
2024-06-28 09:33:07 -0500

    3

--- libasm/3-strchr.asm
+++ libasm/3-strchr.asm
@@ -1,44 +1,38 @@
-BITS 64
-
 global asm_strchr
 section .text
 
-;; char *asm_strchr(const char *s, int c);
 asm_strchr:
     push rbp
     mov rbp, rsp
     push rbx
 
-    ; Load character to find (c) into al
-    movzx eax, BYTE [rsi]
-
-    ; Loop through the string
 .get_next_char:
-    ; Load byte from s (rdi) into bl
-    movzx ebx, BYTE [rdi]
+    movzx ebx, BYTE [rdi]   ; Load the byte from memory address in rdi into ebx (s)
+    movzx eax, BYTE [rsi]   ; Load the byte from memory address in rsi into eax (c)
 
-    ; Compare current character (bl) with c (al)
-    cmp bl, al
-    jz .return_found     ; If match found, return pointer to current character
+.run_loop:
+    cmp bl, 0x00            ; Compare the byte in ebx (s) with 0x00 (end-of-string)
+    jz .null_found          ; If s[i] == 0x00, jump to .null_found
 
-    ; Check for end of string ('\0')
-    test bl, bl
-    jz .not_found        ; If end of string, return NULL
+    cmp bl, al              ; Compare the byte in ebx (s) with eax (c)
+    jz .return_found        ; If s[i] == c, jump to .return_found
 
-    ; Move to the next character in the string
-    inc rdi
-    jmp .get_next_char   ; Repeat loop
+    inc rdi                 ; Increment the pointer to s (rdi) to move to the next character
+    movzx ebx, BYTE [rdi]   ; Load the next byte from memory into ebx (s)
+    jmp .run_loop           ; Jump back to .run_loop to continue searching
+
+.null_found:
+    cmp al, 0x00            ; Check if c == 0x00 (end-of-search character)
+    jz .return_found        ; If c is end-of-search character, jump to .return_found
+    xor rax, rax            ; Otherwise, set return value rax to 0 (not found)
+    jmp .exit               ; Jump to .exit
 
 .return_found:
-    ; If match found, return pointer to the found character
-    mov rax, rdi
-    jmp .exit
-
-.not_found:
-    ; If end of string ('\0') is reached without finding c, return NULL
-    xor rax, rax        ; Set rax to 0 (NULL)
+    xor rax, rax            ; Set return value rax to 0 (not found)
+    mov rdi, rax            ; Set rdi (return value convention) to 0 (not found)
+    jmp .exit               ; Jump to .exit
 
 .exit:
-    pop rbx
-    pop rbp
-    ret
+    pop rbx                 ; Restore rbx
+    pop rbp                 ; Restore rbp
+    ret                     ; Return from asm_strchr function
