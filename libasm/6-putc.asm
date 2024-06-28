058dceaeb4ad568755162a2ed4d43d07e47e69c8
Matt <matternstsr@gmail.com>
2024-06-28 11:37:08 -0500

    cant use syscalls

---
+++ libasm/6-putc.asm
@@ -0,0 +1,19 @@
+section .text
+global asm_putc
+
+;; Function: asm_putc
+;; Purpose: Print a single character to standard output and return the number of bytes written
+;; Arguments:
+;;   rdi = c (character to be printed, passed in the lower 8 bits of rdi)
+;; Returns:
+;;   rax = number of bytes written (always 1 in this case)
+
+asm_putc:
+    ; Prepare arguments for write call
+    mov rax, 1          ; syscall number for write
+    mov rdi, 1          ; file descriptor 1 (stdout)
+    mov rsi, rsp        ; pointer to the character to print (in this case, in the lower 8 bits of rdi)
+    mov rdx, 1          ; number of bytes to write (just 1 byte)
+    ; Return 1 byte written (always 1 for a single character)
+    mov rax, 1
+    ret
