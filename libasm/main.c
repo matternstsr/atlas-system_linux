ffae2953995ff340f442303ae5b78230bd6466cd
Matt <matternstsr@gmail.com>
2024-06-27 22:16:56 -0500

    trying?

--- libasm/main.c
+++ libasm/main.c
@@ -5,29 +5,42 @@
 
 #include "libasm.h"
 
-#define S1  "Holberton School"
-#define S2  "Holberton"
-#define N   strlen(S1)
+#define A1  "Holberton School"
+#define A2  "Holberton"
+#define B1  "Holberton"
+#define B2  "Holberton School"
 
 int main(void)
 {
-    // Print value of N
-    printf("Value of N: %zu\n", N);
+    // For the first case: asm_strncmp("Holberton School", "Holberton", strlen("Holberton"))
+    printf("Case 1:\n");
 
-    // Print "asm_strncmp(S1, S2, N)"
-    printf("asm_strncmp(\"%s\", \"%s\", %zu)\n", S1, S2, N);
+    size_t N1 = strlen(A2);
 
-    // Perform the comparison
-    int std_result = strncmp(S1, S2, N);
-    int asm_result = asm_strncmp(S1, S2, N);
+    // Print values
+    printf("a1: \"%s\"\n", A1);
+    printf("a2: \"%s\"\n", A2);
+    printf("N: %zu\n", N1);
 
-    // Print the results
-    printf("strncmp result: %d\n", std_result);
-    printf("asm_strncmp result: %d\n", asm_result);
+    // Perform comparison
+    assert(strncmp(A1, A2, N1) == asm_strncmp(A1, A2, N1));
 
-    // Compare results
-    assert(std_result == asm_result);
+    printf("All good!\n\n");
+
+    // For the second case: asm_strncmp("Holberton", "Holberton School", strlen("Holberton"))
+    printf("Case 2:\n");
+
+    size_t N2 = strlen(B1);
+
+    // Print values
+    printf("b1: \"%s\"\n", B1);
+    printf("b2: \"%s\"\n", B2);
+    printf("N: %zu\n", N2);
+
+    // Perform comparison
+    assert(strncmp(B1, B2, N2) == asm_strncmp(B1, B2, N2));
 
     printf("All good!\n");
+
     return EXIT_SUCCESS;
 }
