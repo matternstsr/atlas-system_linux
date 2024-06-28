5676726c52262def39bb09a6548422009e46c657
Matt <matternstsr@gmail.com>
2024-06-28 11:21:34 -0500

    cant use done?

--- libasm/5-memcpy.asm
+++ libasm/5-memcpy.asm
@@ -10,28 +10,20 @@ global asm_memcpy
 ;; Returns:
 ;;   rax = dest (pointer to the beginning of the destination buffer)
 asm_memcpy:
-    push rbx            ; Preserve rbx
-    mov rbx, rdx        ; Store n in rbx 
-    
-    test rbx, rbx       ; Check if n == 0
-    je .finish            ; If n == 0, nothing to copy
-    
-    cld                 ; Clear direction flag 
+    XOR ECX, ECX       ; Clear ECX register (ECX = 0)
+    MOV EAX, EDI       ; Move destination pointer (EDI) to EAX (EAX = dest)
 
-.copy_loop:
-    cmp rbx, 0          ; Check if n == 0
-    je .finish            ; If n == 0, exit loop
-    
-    mov al, [rsi]       ; Load 1 byte from src to al
-    mov [rdi], al       ; Store 1 byte from al to dest
-    
-    inc rdi             ; Move dest pointer forward by 1 byte
-    inc rsi             ; Move src pointer forward by 1 byte
-    dec rbx             ; Decrement n by 1
-    
-    jmp .copy_loop      ; Repeat until all bytes copied
+.loop:
+    CMP ECX, EDX       ; Compare ECX (counter) with EDX (number of bytes to copy)
+    JE end             ; If ECX == EDX, jump to end (copying finished)
 
-.finish:
-    pop rbx             ; Restore rbx
-    mov rax, rdi        ; Return dest (rax holds return value)
+    MOV EDX, [ESI]     ; Move 4 bytes from source pointer (ESI) to EDX
+    MOV [EAX], EDX     ; Move 4 bytes from EDX to destination pointer (EAX)
+
+    INC ESI            ; Increment source pointer (ESI)
+    INC EAX            ; Increment destination pointer (EAX)
+    JMP .loop          ; Jump back to .loop to continue copying
+
+end:
+    mov rax, rdi
     ret
