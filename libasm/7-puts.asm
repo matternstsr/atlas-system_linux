df80fd3e293dd98e7120104441588a76831d053e
Matt <matternstsr@gmail.com>
2024-06-28 12:34:22 -0500

    going back

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -3,10 +3,8 @@ global asm_puts
 
 ;; Prototype: size_t asm_puts(const char *str)
 asm_puts:
-    push rbp
+    push rbp            ; Function prologue
     mov rbp, rsp
-    push rdx
-    push rsi
 
     ; Get the length of the string using asm_strlen
     mov rsi, rdi        ; Pass the argument (str) to asm_strlen
@@ -14,8 +12,8 @@ asm_puts:
 
     ; Set up syscall to write the string to stdout
     mov rax, 1          ; syscall number for write
-    mov rdx, rax        ; pointer to the string (already in rdi)
     mov rdi, 1          ; file descriptor 1 (stdout)
+    mov rdx, rax        ; length of the string
     syscall             ; invoke syscall to write to stdout
 
     ; Cleanup and return
