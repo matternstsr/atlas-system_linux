e5bb06ff08bf58ec2dc9f6eba51c49cf7979a423
Matt <matternstsr@gmail.com>
2024-06-28 19:11:39 -0500

    trying so bit shifting to 64

--- libasm/10-strspn.asm
+++ libasm/10-strspn.asm
@@ -10,13 +10,15 @@ asm_strspn:
     mov     r8, rdi         ; r8 = s (source string)
     mov     r9, rsi         ; r9 = accept (accept string)
 
+    ; Loop through source string
 .loop:
     movzx   edx, byte [r8]  ; Load byte from s into edx
     test    dl, dl          ; Check if end of string s ('\0')
-    jz      .end
+    jz      .end            ; If end of s, exit loop
 
     mov     rcx, r9         ; rcx = accept (reset pointer to accept string)
 
+    ; Check each character in accept
 .check_accept:
     movzx   ebx, byte [rcx] ; Load byte from accept into ebx
     test    bl, bl          ; Check if end of accept string ('\0')
@@ -26,12 +28,12 @@ asm_strspn:
     je      .accept_found   ; If match found, increment and continue
 
     inc     rcx             ; Move to next character in accept
-    jmp     .check_accept   ; Continue checking
+    jmp     .check_accept   ; Continue checking accept string
 
 .accept_found:
     inc     rax             ; Increment count (length of matched segment)
     inc     r8              ; Move to next character in s
-    jmp     .loop           ; Continue looping
+    jmp     .loop           ; Continue looping through source string
 
 .increment_count:
     inc     rax             ; Final increment to count if character matches accept
@@ -39,6 +41,7 @@ asm_strspn:
 .end:
     pop     rbp
     ret                     ; Return from function
+
 
 
 ;Correct output - case: asm_strspn("Holberton", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
