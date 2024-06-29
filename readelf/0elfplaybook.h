c031b12745d294238955b5d828224ab6ee3f12cd
Matt <matternstsr@gmail.com>
2024-06-28 23:38:03 -0500

    betty

--- readelf/0elfplaybook.h
+++ readelf/0elfplaybook.h
@@ -23,21 +23,19 @@
 #define MAX_SECTIONS 1000
 
 /* Function prototypes */
-/*void readelf_header32(const char *filename);*/
+void readelf_header32(const char *filename);
 void readelf_header64(const char *filename);
 void print_osabi(unsigned char osabi);
 void print_file_type(uint16_t type);
 void print_machine_type(uint16_t machine);
 void sparcbigendian32(const char *filename);
 
+/* Function prototypes */
+void readelf_header32(const char *filename);
+void readelf_header64(const char *filename);
+
 /* Utility functions for endian swapping */
 uint16_t swap16(uint16_t val, bool swap);
 uint32_t swap32(uint32_t val, bool swap);
-
-/* helpers*/
-void print_section_headers(const Elf64_Ehdr *ehdr);
-void print_program_headers(const Elf64_Ehdr *ehdr);
-void print_file_information(const Elf64_Ehdr *ehdr);
-void print_elf_identification(const Elf64_Ehdr *ehdr);
 
 #endif /* ELF_H */
