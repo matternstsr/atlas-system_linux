5a2db57d4ae8257382997dd27f60e55b092e6685
matternstsr <matternstsr@gmail.com>
2024-07-02 21:55:02 +0000

    betty ()}

--- readelf/0elf_sections.c
+++ readelf/0elf_sections.c
@@ -25,53 +25,53 @@ const char *flags_to_string(unsigned long flags)
 /* Function to read section headers from an ELF file */
 Elf64_Shdr *read_section_headers(FILE *fp, Elf64_Ehdr *ehdr)
 {
-    Elf64_Shdr *shdr = NULL;
-    long shoff;
-    size_t shdr_size;
+	Elf64_Shdr *shdr = NULL;
+	long shoff;
+	size_t shdr_size;
 
-    /* Seek to section header table offset */
-    shoff = ehdr->e_shoff;
-    fseek(fp, shoff, SEEK_SET);
-    shdr_size = ehdr->e_shentsize * ehdr->e_shnum;
-    shdr = malloc(shdr_size);
-    if (shdr == NULL)
-    {
-        fprintf(stderr, "Error: Failed to allocate memory for section headers\n");
-        return NULL;
-    }
+	/* Seek to section header table offset */
+	shoff = ehdr->e_shoff;
+	fseek(fp, shoff, SEEK_SET);
+	shdr_size = ehdr->e_shentsize * ehdr->e_shnum;
+	shdr = malloc(shdr_size);
+	if (shdr == NULL)
+	{
+		fprintf(stderr, "Error: Failed to allocate memory for section headers\n");
+		return NULL;
+	}
 
-    /* Read section headers */
-    fread(shdr, ehdr->e_shentsize, ehdr->e_shnum, fp);
+	/* Read section headers */
+	fread(shdr, ehdr->e_shentsize, ehdr->e_shnum, fp);
 
-    return shdr;
+	return shdr;
 }
 
 /* Function to display section headers from an ELF file */
 void display_section_headers(Elf64_Ehdr *ehdr, Elf64_Shdr *shdr, char *shstrtab)
 {
-    int i;
+	int i;
 
-    /* Print section headers */
-    printf("There are %d section headers, starting at offset 0x%lx:\n\n", ehdr->e_shnum, (long)ehdr->e_shoff);
-    printf("Section Headers:\n");
-    printf("  [Nr] Name              Type            Addr     Off    Size   ES Flg Lk Inf Al\n");
+	/* Print section headers */
+	printf("There are %d section headers, starting at offset 0x%lx:\n\n", ehdr->e_shnum, (long)ehdr->e_shoff);
+	printf("Section Headers:\n");
+	printf("  [Nr] Name              Type            Addr     Off    Size   ES Flg Lk Inf Al\n");
 
-    for (i = 0; i < ehdr->e_shnum; i++)
-    {
-        const char *name = &shstrtab[shdr[i].sh_name];
-        printf("  [%2d] %-17s %-15s %08lx %06lx %06lx %02lx %3s %2d %3d %2d\n",
-               i, name, section_type_to_string(shdr[i].sh_type),
-               (unsigned long)shdr[i].sh_addr,
-               (unsigned long)shdr[i].sh_offset,
-               (unsigned long)shdr[i].sh_size,
-               (unsigned long)shdr[i].sh_entsize,
-               flags_to_string(shdr[i].sh_flags), (int)shdr[i].sh_link,
-               (int)shdr[i].sh_info, (int)shdr[i].sh_addralign);
+	for (i = 0; i < ehdr->e_shnum; i++)
+	{
+		const char *name = &shstrtab[shdr[i].sh_name];
+		printf("  [%2d] %-17s %-15s %08lx %06lx %06lx %02lx %3s %2d %3d %2d\n",
+			i, name, section_type_to_string(shdr[i].sh_type),
+			(unsigned long)shdr[i].sh_addr,
+			(unsigned long)shdr[i].sh_offset,
+			(unsigned long)shdr[i].sh_size,
+			(unsigned long)shdr[i].sh_entsize,
+			flags_to_string(shdr[i].sh_flags), (int)shdr[i].sh_link,
+			(int)shdr[i].sh_info, (int)shdr[i].sh_addralign);
 
-        if (i == 39)   /* Stop printing after the 40th section header */
-            break;
-    }
+		if (i == 39)   /* Stop printing after the 40th section header */
+			break;
+	}
 
-    /* Print Key to Flags */
-    print_elf_flags_key();
+	/* Print Key to Flags */
+	print_elf_flags_key();
 }
