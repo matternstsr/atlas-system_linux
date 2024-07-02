5c5d4ec9c83ef29ff6a50ccadb0495262d00471a
matternstsr <matternstsr@gmail.com>
2024-07-02 20:40:53 +0000

    over 80 try 2

---
+++ readelf/2-switch_endianness_sections.c
@@ -0,0 +1,39 @@
+#include "2-hreadelf.h"
+
+/**
+* switch_endianness_sections_64 - Switches endianness of 64-bit ELF section headers if necessary.
+* @elf_header: Pointer to the ELF header structure.
+* @index: Index of the section header to switch endianness.
+*/
+void switch_endianness_sections_64(elf_t *elf_header, size_t index)
+{
+	elf_header->s64[index].sh_name       = swap_endian_uint32(elf_header->s64[index].sh_name);
+	elf_header->s64[index].sh_type       = swap_endian_uint32(elf_header->s64[index].sh_type);
+	elf_header->s64[index].sh_flags      = swap_endian_uint64(elf_header->s64[index].sh_flags);
+	elf_header->s64[index].sh_addr       = swap_endian_uint64(elf_header->s64[index].sh_addr);
+	elf_header->s64[index].sh_offset     = swap_endian_uint64(elf_header->s64[index].sh_offset);
+	elf_header->s64[index].sh_size       = swap_endian_uint64(elf_header->s64[index].sh_size);
+	elf_header->s64[index].sh_link       = swap_endian_uint32(elf_header->s64[index].sh_link);
+	elf_header->s64[index].sh_info       = swap_endian_uint32(elf_header->s64[index].sh_info);
+	elf_header->s64[index].sh_addralign  = swap_endian_uint64(elf_header->s64[index].sh_addralign);
+	elf_header->s64[index].sh_entsize    = swap_endian_uint64(elf_header->s64[index].sh_entsize);
+}
+
+/**
+* switch_endianness_sections_32 - Switches endianness of 32-bit ELF section headers if necessary.
+* @elf_header: Pointer to the ELF header structure.
+* @index: Index of the section header to switch endianness.
+*/
+void switch_endianness_sections_32(elf_t *elf_header, size_t index)
+{
+	elf_header->s32[index].sh_name       = swap_endian_uint32(elf_header->s32[index].sh_name);
+	elf_header->s32[index].sh_type       = swap_endian_uint32(elf_header->s32[index].sh_type);
+	elf_header->s32[index].sh_flags      = swap_endian_uint32(elf_header->s32[index].sh_flags);
+	elf_header->s32[index].sh_addr       = swap_endian_uint32(elf_header->s32[index].sh_addr);
+	elf_header->s32[index].sh_offset     = swap_endian_uint32(elf_header->s32[index].sh_offset);
+	elf_header->s32[index].sh_size       = swap_endian_uint32(elf_header->s32[index].sh_size);
+	elf_header->s32[index].sh_link       = swap_endian_uint32(elf_header->s32[index].sh_link);
+	elf_header->s32[index].sh_info       = swap_endian_uint32(elf_header->s32[index].sh_info);
+	elf_header->s32[index].sh_addralign  = swap_endian_uint32(elf_header->s32[index].sh_addralign);
+	elf_header->s32[index].sh_entsize    = swap_endian_uint32(elf_header->s32[index].sh_entsize);
+}
