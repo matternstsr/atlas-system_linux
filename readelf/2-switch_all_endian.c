10b417f951625314139f2e744e914a86c600e48d
matternstsr <matternstsr@gmail.com>
2024-07-02 20:52:50 +0000

    over 80 try 4

--- readelf/2-switch_all_endian.c
+++ readelf/2-switch_all_endian.c
@@ -2,36 +2,6 @@
 
 
 /**
-* switch_endianness_program_headers - Switches endianness of ELF program headers if necessary.
-* @elf_header: Pointer to the ELF header structure.
-* @index: Index of the program header to switch endianness.
-*/
-void switch_endianness_program_headers(elf_t *elf_header, size_t index)
-{
-	if (IS_BE(elf_header->e64)) {
-		if (!IS_32(elf_header->e64)) {
-			elf_header->p64[index].p_type = swap_endian_uint32(elf_header->p64[index].p_type);
-			elf_header->p64[index].p_offset = swap_endian_uint64(elf_header->p64[index].p_offset);
-			elf_header->p64[index].p_vaddr = swap_endian_uint64(elf_header->p64[index].p_vaddr);
-			elf_header->p64[index].p_paddr = swap_endian_uint64(elf_header->p64[index].p_paddr);
-			elf_header->p64[index].p_filesz = swap_endian_uint64(elf_header->p64[index].p_filesz);
-			elf_header->p64[index].p_memsz = swap_endian_uint64(elf_header->p64[index].p_memsz);
-			elf_header->p64[index].p_align = swap_endian_uint64(elf_header->p64[index].p_align);
-			elf_header->p64[index].p_flags = swap_endian_uint32(elf_header->p64[index].p_flags);
-		} else {
-			elf_header->p32[index].p_type = swap_endian_uint32(elf_header->p32[index].p_type);
-			elf_header->p32[index].p_offset = swap_endian_uint32(elf_header->p32[index].p_offset);
-			elf_header->p32[index].p_vaddr = swap_endian_uint32(elf_header->p32[index].p_vaddr);
-			elf_header->p32[index].p_paddr = swap_endian_uint32(elf_header->p32[index].p_paddr);
-			elf_header->p32[index].p_filesz = swap_endian_uint32(elf_header->p32[index].p_filesz);
-			elf_header->p32[index].p_memsz = swap_endian_uint32(elf_header->p32[index].p_memsz);
-			elf_header->p32[index].p_align = swap_endian_uint32(elf_header->p32[index].p_align);
-			elf_header->p32[index].p_flags = swap_endian_uint32(elf_header->p32[index].p_flags);
-		}
-	}
-}
-
-/**
 * switch_all_endian - switches all endians if need be
 * @h: address of our internal struct
 */
