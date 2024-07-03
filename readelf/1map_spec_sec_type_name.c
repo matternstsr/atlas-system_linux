0c37dd13432104eb9334cb680eaf43aa61e6eacf
matternstsr <matternstsr@gmail.com>
2024-07-03 01:20:56 +0000

    changeing up the switch case to be closer to 2

--- readelf/1map_spec_sec_type_name.c
+++ readelf/1map_spec_sec_type_name.c
@@ -1,15 +1,5 @@
 #include "1-hreadelf.h"
 
-static const SpecialSectionTypeMapping special_section_type_mappings[] = {
-	{ SHT_GNU_verdef, "VERDEF" },
-	{ SHT_GNU_verneed, "VERNEED" },
-	{ SHT_GNU_versym, "VERSYM" },
-	{ SHT_GNU_HASH, "GNU_HASH" },
-	{ SHT_INIT_ARRAY, "INIT_ARRAY" },
-	{ SHT_FINI_ARRAY, "FINI_ARRAY" },
-	{ SHT_RELA, "RELA" },
-};
-
 const char *map_spec_sec_type_name(uint32_t sh_type)
 {
     switch (sh_type)
