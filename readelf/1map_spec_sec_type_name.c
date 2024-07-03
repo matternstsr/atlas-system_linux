e63f38c8632500a527ab266b068394ca5326af20
matternstsr <matternstsr@gmail.com>
2024-07-03 01:23:27 +0000

    changeing up the switch case to be closer to 2

--- readelf/1map_spec_sec_type_name.c
+++ readelf/1map_spec_sec_type_name.c
@@ -1,24 +1,29 @@
 #include "1-hreadelf.h"
 
+static const SpecialSectionTypeMapping special_section_type_mappings[] = {
+	{ SHT_GNU_verdef, "VERDEF" },
+	{ SHT_GNU_verneed, "VERNEED" },
+	{ SHT_GNU_versym, "VERSYM" },
+	{ SHT_GNU_HASH, "GNU_HASH" },
+	{ SHT_INIT_ARRAY, "INIT_ARRAY" },
+	{ SHT_FINI_ARRAY, "FINI_ARRAY" },
+	{ SHT_RELA, "RELA" },
+};
+
 const char *map_spec_sec_type_name(uint32_t sh_type)
 {
-    switch (sh_type)
-    {
-        case SHT_GNU_verdef:
-            return "VERDEF";
-        case SHT_GNU_verneed:
-            return "VERNEED";
-        case SHT_GNU_versym:
-            return "VERSYM";
-        case SHT_GNU_HASH:
-            return "GNU_HASH";
-        case SHT_INIT_ARRAY:
-            return "INIT_ARRAY";
-        case SHT_FINI_ARRAY:
-            return "FINI_ARRAY";
-        case SHT_RELA:
-            return "RELA";
-        default:
-            return "is this it";
-    }
+		/* Calculate number of elements in special_section_type_mappings */
+	size_t numps = sizeof(special_section_type_mappings) /
+								sizeof(special_section_type_mappings[0]);
+	size_t i;
+
+	for (i = 0; i < numps ; ++i)
+	{
+		if (special_section_type_mappings[i].type == sh_type)
+			return (special_section_type_mappings[i].name);
+	}
+	return ("UNKNOWN");  /* Default return for unknown types */
 }
+
+case SHT_GNU_verdef:        return "VERDEF";
+		case SHT_GNU_verneed:       return "VERNEED";
