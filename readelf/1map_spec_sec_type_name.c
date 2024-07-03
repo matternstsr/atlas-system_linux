21a9145f0d85e4db58f0ec5c6ca358cf58fc25ec
matternstsr <matternstsr@gmail.com>
2024-07-03 01:34:08 +0000

    changeing up the switch case to be closer to 2

--- readelf/1map_spec_sec_type_name.c
+++ readelf/1map_spec_sec_type_name.c
@@ -1,13 +1,15 @@
 #include "1-hreadelf.h"
 
-static const SpecialSectionTypeMapping special_section_type_mappings[] = {
-	{ SHT_GNU_verdef, "VERDEF" },
-	{ SHT_GNU_verneed, "VERNEED" },
-	{ SHT_GNU_versym, "VERSYM" },
-	{ SHT_GNU_HASH, "GNU_HASH" },
-	{ SHT_INIT_ARRAY, "INIT_ARRAY" },
-	{ SHT_FINI_ARRAY, "FINI_ARRAY" },
-	{ SHT_RELA, "RELA" },
+static const SectionTypeMapping additional_section_type_mappings[] = {
+    { SHT_GNU_verdef, "VERDEF" },
+    { SHT_GNU_verneed, "VERNEED" },
+    { SHT_GNU_versym, "VERSYM" },
+    { SHT_GNU_HASH, "GNU_HASH" },
+    { SHT_INIT_ARRAY, "INIT_ARRAY" },
+    { SHT_FINI_ARRAY, "FINI_ARRAY" },
+    { SHT_RELA, "RELA" },
+    { SHT_SUNW_version, ".SUNW_version" },   // Added .SUNW_version mapping
+    { SHT_SUNW_versym, ".SUNW_versym" },     // Added .SUNW_versym mapping
 };
 
 const char *map_spec_sec_type_name(uint32_t sh_type)
--- readelf/1res_spec_sec_type_name.c
+++ readelf/1res_spec_sec_type_name.c
@@ -1,13 +1,15 @@
 #include "1-hreadelf.h"
 
 static const SectionTypeMapping additional_section_type_mappings[] = {
-	{ SHT_GNU_verdef, "VERDEF" },
-	{ SHT_GNU_verneed, "VERNEED" },
-	{ SHT_GNU_versym, "VERSYM" },
-	{ SHT_GNU_HASH, "GNU_HASH" },
-	{ SHT_INIT_ARRAY, "INIT_ARRAY" },
-	{ SHT_FINI_ARRAY, "FINI_ARRAY" },
-	{ SHT_RELA, "RELA" },
+    { SHT_GNU_verdef, "VERDEF" },
+    { SHT_GNU_verneed, "VERNEED" },
+    { SHT_GNU_versym, "VERSYM" },
+    { SHT_GNU_HASH, "GNU_HASH" },
+    { SHT_INIT_ARRAY, "INIT_ARRAY" },
+    { SHT_FINI_ARRAY, "FINI_ARRAY" },
+    { SHT_RELA, "RELA" },
+    { SHT_SUNW_version, ".SUNW_version" },   // Added .SUNW_version mapping
+    { SHT_SUNW_versym, ".SUNW_versym" },     // Added .SUNW_versym mapping
 };
 
 
