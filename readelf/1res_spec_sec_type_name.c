348a08b69aad129c0ff288bcb81e4f3aef96541c
matternstsr <matternstsr@gmail.com>
2024-07-03 02:06:32 +0000

    changeing up the switch case to be closer to 2

--- readelf/1res_spec_sec_type_name.c
+++ readelf/1res_spec_sec_type_name.c
@@ -1,16 +1,3 @@
-#include "1-hreadelf.h"
-
-static const SectionTypeMapping additional_section_type_mappings[] = {
-	{ SHT_GNU_verdef, "VERDEF" },
-	{ SHT_GNU_verneed, "VERNEED" },
-	{ SHT_GNU_versym, "VERSYM" },
-	{ SHT_GNU_HASH, "GNU_HASH" },
-	{ SHT_INIT_ARRAY, "INIT_ARRAY" },
-	{ SHT_FINI_ARRAY, "FINI_ARRAY" },
-	{ SHT_RELA, "RELA" },
-};
-
-
 /**
  * res_spec_sec_type_name - Resolve specific section type name based on sh_type
  * @sh_type: The section type to resolve
@@ -26,15 +13,5 @@ static const SectionTypeMapping additional_section_type_mappings[] = {
  */
 const char *res_spec_sec_type_name(uint32_t sh_type)
 {
-	/* Calculate number of elements in additional_section_type_mappings */
-	size_t num_mappings = sizeof(additional_section_type_mappings) /
-												sizeof(additional_section_type_mappings[0]);
-	size_t i;
-
-	for (i = 0; i < num_mappings; ++i)
-	{
-		if (additional_section_type_mappings[i].type == sh_type)
-			return (additional_section_type_mappings[i].name);
-	}
-	return ("UNKNOWN");  /* Default return for unknown types */
+	return (0);
 }
