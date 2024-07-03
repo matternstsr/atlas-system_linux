2ca6e429e4e3ebf6f1b3655d9ee097a89988abdd
matternstsr <matternstsr@gmail.com>
2024-07-03 01:53:43 +0000

    changeing up the switch case to be closer to 2

--- readelf/1-hreadelf.h
+++ readelf/1-hreadelf.h
@@ -22,19 +22,6 @@
 #define SHT_SUNW_version  0x6ffffffd
 #define SHT_SUNW_versym   0x6ffffffe
 
-// Array of mappings
-static const SectionTypeMapping additional_section_type_mappings[] = {
-    { SHT_GNU_verdef, "VERDEF" },
-    { SHT_GNU_verneed, "VERNEED" },
-    { SHT_GNU_versym, "VERSYM" },
-    { SHT_GNU_HASH, "GNU_HASH" },
-    { SHT_INIT_ARRAY, "INIT_ARRAY" },
-    { SHT_FINI_ARRAY, "FINI_ARRAY" },
-    { SHT_RELA, "RELA" },
-    { SHT_SUNW_version, "SUNW_version" },
-    { SHT_SUNW_versym, "SUNW_versym" },
-};
-
 // Define mapping structure
 typedef struct {
     uint32_t type;
