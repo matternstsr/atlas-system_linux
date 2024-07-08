57d93332ea2e7fe34c0707320112682bc9708efa
Matt <matternstsr@gmail.com>
2024-07-08 10:04:47 -0500

    signals.h

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -1,5 +1,18 @@
 #include "signals.h"
 
+/*
+ * Description:
+ *   This function is the signal handler for SIGINT (Ctrl-C) signals. 
+ *   It prints "Gotcha! [<signum>]" to standard output, where <signum> 
+ *   is the signal number received. It ensures the output is immediately
+ *   flushed using fflush(stdout).
+ *
+ * Parameters:
+ *   signum: The signal number received (typically SIGINT).
+ *
+ * Returns:
+ *   None.
+ */
 /* Function to handle SIGINT signal */
 void sigint_handler(int signum)
 {
@@ -7,6 +20,18 @@ void sigint_handler(int signum)
 	fflush(stdout);
 }
 
+/*
+ * Description:
+ *   This function sets up a signal handler for the SIGINT signal (Ctrl-C).
+ *   It installs `sigint_handler` as the handler function for SIGINT using
+ *   the `signal()` function from `<signal.h>`.
+ *
+ * Parameters:
+ *   None.
+ *
+ * Returns:
+ *   0 on success, or -1 if an error occurs while setting the signal handler.
+ */
 /* Function to set up signal handler for SIGINT */
 int handle_signal(void)
 {
