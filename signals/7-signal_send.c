a3aece09175c091555ff0b094158a656f5e35ec0
Matt <matternstsr@gmail.com>
2024-07-08 12:21:31 -0500

    all

---
+++ signals/7-signal_send.c
@@ -0,0 +1,28 @@
+#include "signals.h"
+
+/**
+ * main - Sends SIGINT signal to a process given its PID
+ * @argc: Number of arguments
+ * @argv: Array of arguments (including program name and PID)
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
+int main(int argc, char *argv[])
+{
+    pid_t pid
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <pid>\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+
+    pid = atoi(argv[1]);
+
+    if (pid <= 0) {
+        printf("Invalid PID: %s\n", argv[1]);
+        return (EXIT_FAILURE);
+    }
+
+    return (EXIT_SUCCESS);
+}
