9ed700bd059762b604c481b4c3fca7a1057e32e9
Matt <matternstsr@gmail.com>
2024-07-08 11:45:45 -0500

    all

--- signals/signals.h
+++ signals/signals.h
@@ -28,4 +28,13 @@ void sigquit_handler(int signum, siginfo_t *info, void *context);
 /* Function to set up signal handler for SIGQUIT */
 int trace_signal_sender(void);
 
+/* Signal handler for SIGINT */
+void sigint_handler(int signum);
+
+/* Function to set up signal handler for SIGINT and handle it once */
+void handle_sigint_and_exit(void);
+
+
+
+
 #endif /* SIGNALS_H */
