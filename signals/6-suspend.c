24ad13d859d92c2185efaddb36632582bc84844d
Matt <matternstsr@gmail.com>
2024-07-08 11:43:14 -0500

    all

---
+++ signals/6-suspend.c
@@ -0,0 +1,25 @@
+#include "signal.h"
+
+/* Signal handler for SIGINT */
+void sigint_handler(int signum) {
+	printf("Caught %d\nSignal received\n", signum);
+	fflush(stdout);
+	signal(SIGINT, SIG_DFL); /* Restore default handler */
+	raise(SIGINT); /* Raise SIGINT again to exit */
+}
+
+/* Function to set up signal handler for SIGINT and handle it once */
+void handle_sigint_and_exit(void) {
+	struct sigaction siggy;
+	siggy.sa_handler = sigint_handler;
+	sigemptyset(&siggy.sa_mask);
+	siggy.sa_flags = 0;
+
+	sigaction(SIGINT, &siggy, NULL);
+
+}
+
+int main() {
+	handle_sigint_and_exit(); /* Call func to setup handler & handle SIGINT */
+	return (EXIT_SUCCESS);
+}
