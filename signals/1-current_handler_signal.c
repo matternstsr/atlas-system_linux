24828f6d8f94dbd1ce392bc83a490b7c4d469b2c
Matt <matternstsr@gmail.com>
2024-07-08 10:12:52 -0500

    all

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -1,6 +1,16 @@
 #include "signals.h"
 
-/* Function to retrieve current handler of SIGINT */
+/**
+ * current_handler_signal - Retrieve current handler for SIGINT
+ * 
+ * This function temporarily sets the SIGINT signal handler to SIG_DFL
+ * (default handler), retrieves the current handler, and restores the
+ * original handler. It returns a pointer to the current SIGINT signal
+ * handler function.
+ *
+ * Return:
+ *   Pointer to the current SIGINT signal handler function, or NULL on failure.
+ */
 void (*current_handler_signal(void))(int)
 {
 	void (*current_handler)(int) = signal(SIGINT, SIG_DFL);
