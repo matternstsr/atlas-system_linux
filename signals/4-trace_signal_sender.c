9db83289376f3e5aeed0e6138f2203f2a2daa249
Matt <matternstsr@gmail.com>
2024-07-08 11:20:51 -0500

    all

--- signals/4-trace_signal_sender.c
+++ signals/4-trace_signal_sender.c
@@ -21,14 +21,13 @@ void sigquit_handler(int signum, siginfo_t *info, void *context)
 }
 
 /**
- * sigquit_handler - Signal handler for SIGQUIT
- * @signum: Signal number received
- * @info: Pointer to siginfo_t structure containing details about the signal
- * @context: Pointer to context information (unused in this handler)
- *
- * This function handles the SIGQUIT signal (Control-\ in a shell) by printing
- * "SIGQUIT sent by <pid>" to standard output, where <pid> is the PID of the
- * process that sent the signal.
+ * trace_signal_sender - Set up signal handler for SIGQUIT
+ * 
+ * Return: 0 on success, -1 on error
+ * 
+ * This function sets up a signal handler for the SIGQUIT signal (Control-\ in a shell)
+ * using the sigaction() function from <signal.h>. It installs `sigquit_handler`
+ * as the handler function for SIGQUIT signals.
  */
 int trace_signal_sender(void)
 {
