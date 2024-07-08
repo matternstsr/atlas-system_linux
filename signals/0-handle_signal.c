5ec15a5fb17dd94e7403c4fb5aa57964790bbbc6
Matt <matternstsr@gmail.com>
2024-07-08 09:59:02 -0500

    Fixxing betty

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -3,8 +3,8 @@
 /* Function to handle SIGINT signal */
 void sigint_handler(int signum)
 {
-printf("Gotcha! [%d]\n", signum);
-fflush(stdout);
+	printf("Gotcha! [%d]\n", signum);
+	fflush(stdout);
 }
 
 /* Function to set up signal handler for SIGINT */
@@ -13,7 +13,7 @@ int handle_signal(void)
 	/* Set the signal handler */
 	if (signal(SIGINT, sigint_handler) == SIG_ERR)
 		/* If an error occurs while setting the signal handler */
-		return -1;
+		return (-1);
 	/* Return 0 on success */
-	return 0;
+	return (0);
 }
