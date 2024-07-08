0dd83350de6121a745be402dfa6d38ea9bd444f6
Matt <matternstsr@gmail.com>
2024-07-08 12:14:00 -0500

    all

--- signals/6-suspend.c
+++ signals/6-suspend.c
@@ -3,9 +3,10 @@
 /**
  * sigint_handler - Signal handler for SIGINT
  * @signum: Signal number received (should be SIGINT)
- * 
- * This function handles the SIGINT signal (Ctrl-C) by printing "Caught %d" where
- * %d is the signal number received, followed by "Signal received". It then restores
+ *
+ * This function handles the SIGINT signal (Ctrl-C)
+ * by printing "Caught %d" where %d is the signal number received,
+ * followed by "Signal received". It then restores
  * the default handler for SIGINT and raises SIGINT again to exit the program.
  */
 void sigint_handler(int signum)
@@ -18,10 +19,11 @@ void sigint_handler(int signum)
 
 /**
  * handle_sigint_and_exit - Set up signal handler for SIGINT and handle it once
- * 
- * This function sets up a signal handler for SIGINT using sigaction and handles
- * the SIGINT signal once by calling sigint_handler. After handling SIGINT, it
- * suspends the program until another signal is received.
+ *
+ * This function sets up a signal handler for SIGINT using sigaction
+ * and handles the SIGINT signal once by calling sigint_handler.
+ * After handling SIGINT, it suspends the program until another signal
+ * is received.
  */
 void handle_sigint_and_exit(void)
 {
@@ -35,7 +37,15 @@ void handle_sigint_and_exit(void)
 
 	pause();
 }
-
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point of the program. It calls
+ * handle_sigint_and_exit to set up the SIGINT signal handler and handle it
+ * once. After handling SIGINT, the function returns EXIT_SUCCESS.
+ *
+ * Return: EXIT_SUCCESS upon successful execution
+ */
 int main(void)
 {
 	handle_sigint_and_exit(); /* Call func to setup handler & handle SIGINT */
