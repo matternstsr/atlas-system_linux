3d3a610a26c72467121c809ec979108d450a9a69
Matt <matternstsr@gmail.com>
2024-07-08 12:23:51 -0500

    all

--- signals/7-signal_send.c
+++ signals/7-signal_send.c
@@ -7,22 +7,22 @@
  *
  * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
  */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     pid_t pid;
 
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("Usage: %s <pid>\n", argv[0]);
-        return (EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     pid = atoi(argv[1]);
 
     if (pid <= 0) {
-        printf("Invalid PID: %s\n", argv[1]);
-        return (EXIT_FAILURE);
+        return EXIT_FAILURE;
+    }
+    if (kill(pid, SIGINT) == -1) {
+        return EXIT_FAILURE;
     }
 
-    return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
