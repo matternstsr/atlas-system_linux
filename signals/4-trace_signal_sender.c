a8a8489cc3d70c397aec3c412b55aefc8352da57
Matt <matternstsr@gmail.com>
2024-07-08 11:18:54 -0500

    all

--- signals/4-trace_signal_sender.c
+++ signals/4-trace_signal_sender.c
@@ -1,17 +1,35 @@
 #include "signals.h"
 
-/* Signal handler for SIGQUIT */
+/**
+ * sigquit_handler - Signal handler for SIGQUIT
+ * @signum: Signal number received
+ * @info: Pointer to siginfo_t structure containing details about the signal
+ * @context: Pointer to context information (unused in this handler)
+ * 
+ * This function handles the SIGQUIT signal (Control-\ in a shell) by printing
+ * "SIGQUIT sent by <pid>" to standard output, where <pid> is the PID of the
+ * process that sent the signal.
+ */
 void sigquit_handler(int signum, siginfo_t *info, void *context)
 {
 	(void)context;
 	if (signum == SIGQUIT)
-	{	
+	{
 		/* Print the message */
 		printf("SIGQUIT sent by %d\n", info->si_pid);
 	}
 }
 
-/* Function to set up signal handler for SIGQUIT */
+/**
+ * sigquit_handler - Signal handler for SIGQUIT
+ * @signum: Signal number received
+ * @info: Pointer to siginfo_t structure containing details about the signal
+ * @context: Pointer to context information (unused in this handler)
+ * 
+ * This function handles the SIGQUIT signal (Control-\ in a shell) by printing
+ * "SIGQUIT sent by <pid>" to standard output, where <pid> is the PID of the
+ * process that sent the signal.
+ */
 int trace_signal_sender(void)
 {
 	struct sigaction siggy;
