9ae8205bcef476e0472a2c19409d0e289093f83a
Matt <matternstsr@gmail.com>
2024-07-08 13:20:07 -0500

    all

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -1,12 +1,6 @@
 #include <sys/types.h>
 #include <signal.h>
 
-/**
- * Check if a process exists given its PID.
- * 
- * @param pid The PID of the process to check.
- * @return 1 if the process with the PID exists, 0 otherwise.
- */
 int pid_exist(pid_t pid)
 {
 	return (!kill(pid, 0));
