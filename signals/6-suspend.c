e1a045d716c17b9a757a52448b147bcad57edf14
Matt <matternstsr@gmail.com>
2024-07-08 12:09:29 -0500

    all

--- signals/6-suspend.c
+++ signals/6-suspend.c
@@ -1,6 +1,13 @@
 #include "signals.h"
 
-/* Signal handler for SIGINT */
+/**
+ * sigint_handler - Signal handler for SIGINT
+ * @signum: Signal number received (should be SIGINT)
+ * 
+ * This function handles the SIGINT signal (Ctrl-C) by printing "Caught %d" where
+ * %d is the signal number received, followed by "Signal received". It then restores
+ * the default handler for SIGINT and raises SIGINT again to exit the program.
+ */
 void sigint_handler(int signum)
 {
 	printf("Caught %d\nSignal received\n", signum);
@@ -9,7 +16,13 @@ void sigint_handler(int signum)
 	raise(SIGINT); /* Raise SIGINT again to exit */
 }
 
-/* Function to set up signal handler for SIGINT and handle it once */
+/**
+ * handle_sigint_and_exit - Set up signal handler for SIGINT and handle it once
+ * 
+ * This function sets up a signal handler for SIGINT using sigaction and handles
+ * the SIGINT signal once by calling sigint_handler. After handling SIGINT, it
+ * suspends the program until another signal is received.
+ */
 void handle_sigint_and_exit(void)
 {
 	struct sigaction siggy;
