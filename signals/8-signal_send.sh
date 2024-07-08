efa97d8fe1c2c1ebfc8511e273de3b9d6ffb5c02
Matt <matternstsr@gmail.com>
2024-07-08 12:38:07 -0500

    all

--- signals/8-signal_send.sh
+++ signals/8-signal_send.sh
@@ -9,6 +9,17 @@ fi
 # Get the PID from the first argument
 pid=$1
 
+# Check if PID is a positive number
+if ! [[ "$pid" =~ ^[0-9]+$ ]]; then
+	exit 1
+fi
+
 # Send SIGQUIT signal to the current process
 kill -QUIT "$pid"
+
+# Check if the kill command was successful
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
 exit 0
