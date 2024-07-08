e24d7b0e76dabdc61e2803f31600c7378472b5aa
Matt <matternstsr@gmail.com>
2024-07-08 09:50:15 -0500

    pushing progress

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -2,7 +2,8 @@
 #include "signals.h"
 
 /* Function to retrieve current handler of SIGINT */
-void (*current_handler_signal(void))(int) {
+void (*current_handler_signal(void))(int)
+{
     void (*current_handler)(int) = signal(SIGINT, SIG_DFL);
     signal(SIGINT, current_handler);
     return current_handler;
