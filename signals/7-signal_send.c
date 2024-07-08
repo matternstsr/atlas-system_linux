502f6a0f1fd5cc463c59164c689254568f78b68f
Matt <matternstsr@gmail.com>
2024-07-08 12:26:53 -0500

    all

--- signals/7-signal_send.c
+++ signals/7-signal_send.c
@@ -11,14 +11,15 @@ int main(int argc, char *argv[])
 {
 	pid_t pid;
 
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		printf("Usage: %s <pid>\n", argv[0]);
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 	pid = atoi(argv[1]);
 	if (pid <= 0)
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	if (kill(pid, SIGINT) == -1)
-		return EXIT_FAILURE;
-	return EXIT_SUCCESS;
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
