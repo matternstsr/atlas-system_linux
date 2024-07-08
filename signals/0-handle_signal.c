f36ebbe3be29a0eff78b4a1ff33e6b3308cb0c3d
Matt <matternstsr@gmail.com>
2024-07-08 09:37:05 -0500

    Testing

---
+++ signals/0-handle_signal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signals.h>
+
+/* Function to handle SIGINT signal */
+void sigint_handler(int signum) {
+    printf("Gotcha! [%d]\n", signum);
+}
+
+/* Function to set up signal handler for SIGINT */
+int handle_signal(void) {
+    /* Set the signal handler */
+    if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+        /* If an error occurs while setting the signal handler */
+        return -1;
+    }
+    /* Return 0 on success */
+    return 0;
+}
---
+++ signals/1-current_handler_signal.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <signals.h>
+
+/* Function to retrieve current handler of SIGINT */
+void (*current_handler_signal(void))(int) {
+    void (*current_handler)(int) = signal(SIGINT, SIG_DFL);
+    signal(SIGINT, current_handler);
+    return current_handler;
+}
