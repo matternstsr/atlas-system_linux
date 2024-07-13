61cdd8290156060b81283a2b4df150e95b4c5dc4
matternstsr <matternstsr@gmail.com>
2024-07-13 16:51:25 -0500

    hnm.c:11:5: error: ‘for’ loop initial declarations are only allowed in C99 mode

--- nm_objdump/elf_parser.c
+++ nm_objdump/elf_parser.c
@@ -1,143 +1,159 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <elf.h>
 #include "elf_parser.h"
 
 #define MAX_SYMBOLS 1024
 
-typedef struct {
-    Elf64_Sym sym;
-    char *name;
+typedef struct
+{
+	Elf64_Sym sym;
+	char *name;
 } SymbolEntry;
 
-static int compare_symbols(const void *a, const void *b) {
-    const SymbolEntry *sa = (const SymbolEntry *)a;
-    const SymbolEntry *sb = (const SymbolEntry *)b;
-    return strcmp(sa->name, sb->name);
+static int compare_symbols(const void *a, const void *b)
+{
+	const SymbolEntry *sa = (const SymbolEntry *)a;
+	const SymbolEntry *sb = (const SymbolEntry *)b;
+	return strcmp(sa->name, sb->name);
 }
 
-static const char *get_symbol_type(Elf64_Sym *sym) {
-    switch (ELF64_ST_TYPE(sym->st_info)) {
-        case STT_NOTYPE:
-            return " ";
-        case STT_OBJECT:
-            return "B";
-        case STT_FUNC:
-            return "T";
-        case STT_SECTION:
-            return "S";
-        case STT_FILE:
-            return "f";
-        case STT_COMMON:
-            return "C";
-        case STT_TLS:
-            return "T";
-        case STT_GNU_IFUNC:
-            return "i";
-        default:
-            return "?";
-    }
+static const char *get_symbol_type(Elf64_Sym *sym)
+{
+	switch (ELF64_ST_TYPE(sym->st_info))
+	{
+		case STT_NOTYPE:
+			return " ";
+		case STT_OBJECT:
+			return "B";
+		case STT_FUNC:
+			return "T";
+		case STT_SECTION:
+			return "S";
+		case STT_FILE:
+			return "f";
+		case STT_COMMON:
+			return "C";
+		case STT_TLS:
+			return "T";
+		case STT_GNU_IFUNC:
+			return "i";
+		default:
+			return "?";
+	}
 }
 
-static int parse_symbols(FILE *file) {
-    Elf64_Ehdr ehdr;
-    if (fread(&ehdr, 1, sizeof(ehdr), file) != sizeof(ehdr)) {
-        fprintf(stderr, "Error reading ELF header\n");
-        return -1;
-    }
+static int parse_symbols(FILE *file)
+{
+	Elf64_Ehdr ehdr;
+	if (fread(&ehdr, 1, sizeof(ehdr), file) != sizeof(ehdr))
+	{
+		fprintf(stderr, "Error reading ELF header\n");
+		return -1;
+	}
 
-    if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0) {
-        fprintf(stderr, "Not an ELF file\n");
-        return -1;
-    }
+	if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		fprintf(stderr, "Not an ELF file\n");
+		return -1;
+	}
 
-    if (ehdr.e_ident[EI_CLASS] != ELFCLASS64) {
-        fprintf(stderr, "Only 64-bit ELF files are supported\n");
-        return -1;
-    }
+	if (ehdr.e_ident[EI_CLASS] != ELFCLASS64)
+	{
+		fprintf(stderr, "Only 64-bit ELF files are supported\n");
+		return -1;
+	}
 
-    fseek(file, ehdr.e_shoff, SEEK_SET);
-    Elf64_Shdr shdr;
-    if (fread(&shdr, 1, sizeof(shdr), file) != sizeof(shdr)) {
-        fprintf(stderr, "Error reading section header\n");
-        return -1;
-    }
+	fseek(file, ehdr.e_shoff, SEEK_SET);
+	Elf64_Shdr shdr;
+	if (fread(&shdr, 1, sizeof(shdr), file) != sizeof(shdr))
+	{
+		fprintf(stderr, "Error reading section header\n");
+		return -1;
+	}
 
-    Elf64_Shdr strtab_section_header;
-    fseek(file, ehdr.e_shoff + ehdr.e_shentsize * ehdr.e_shstrndx, SEEK_SET);
-    if (fread(&strtab_section_header, 1, sizeof(strtab_section_header), file) !=
-        sizeof(strtab_section_header)) {
-        fprintf(stderr, "Error reading section header\n");
-        return -1;
-    }
+	Elf64_Shdr strtab_section_header;
+	fseek(file, ehdr.e_shoff + ehdr.e_shentsize * ehdr.e_shstrndx, SEEK_SET);
+	if (fread(&strtab_section_header, 1, sizeof(strtab_section_header), file) !=
+		sizeof(strtab_section_header))
+		{
+		fprintf(stderr, "Error reading section header\n");
+		return -1;
+	}
 
-    char *shstrtab = malloc(strtab_section_header.sh_size);
-    if (!shstrtab) {
-        fprintf(stderr, "Memory allocation error\n");
-        return -1;
-    }
+	char *shstrtab = malloc(strtab_section_header.sh_size);
+	if (!shstrtab)
+	{
+		fprintf(stderr, "Memory allocation error\n");
+		return -1;
+	}
 
-    fseek(file, strtab_section_header.sh_offset, SEEK_SET);
-    if (fread(shstrtab, 1, strtab_section_header.sh_size, file) != strtab_section_header.sh_size) {
-        fprintf(stderr, "Error reading section header\n");
-        return -1;
-    }
+	fseek(file, strtab_section_header.sh_offset, SEEK_SET);
+	if (fread(shstrtab, 1, strtab_section_header.sh_size, file) != strtab_section_header.sh_size)
+	{
+		fprintf(stderr, "Error reading section header\n");
+		return -1;
+	}
 
-    int num_symbols = 0;
-    SymbolEntry symbols[MAX_SYMBOLS];
+	int num_symbols = 0;
+	SymbolEntry symbols[MAX_SYMBOLS];
 
-    fseek(file, ehdr.e_shoff, SEEK_SET);
-    for (int i = 0; i < ehdr.e_shnum; ++i) {
-        if (fread(&shdr, 1, sizeof(shdr), file) != sizeof(shdr)) {
-            fprintf(stderr, "Error reading section header\n");
-            return -1;
-        }
+	fseek(file, ehdr.e_shoff, SEEK_SET);
+	for (int i = 0; i < ehdr.e_shnum; ++i)
+	{
+		if (fread(&shdr, 1, sizeof(shdr), file) != sizeof(shdr))
+		{
+			fprintf(stderr, "Error reading section header\n");
+			return -1;
+		}
 
-        if (shdr.sh_type == SHT_SYMTAB || shdr.sh_type == SHT_DYNSYM) {
-            int symbol_count = shdr.sh_size / sizeof(Elf64_Sym);
-            fseek(file, shdr.sh_offset, SEEK_SET);
+		if (shdr.sh_type == SHT_SYMTAB || shdr.sh_type == SHT_DYNSYM)
+		{
+			int symbol_count = shdr.sh_size / sizeof(Elf64_Sym);
+			fseek(file, shdr.sh_offset, SEEK_SET);
 
-            for (int j = 0; j < symbol_count; ++j) {
-                Elf64_Sym sym;
-                if (fread(&sym, 1, sizeof(sym), file) != sizeof(sym)) {
-                    fprintf(stderr, "Error reading symbol\n");
-                    return -1;
-                }
+			for (int j = 0; j < symbol_count; ++j)
+			{
+				Elf64_Sym sym;
+				if (fread(&sym, 1, sizeof(sym), file) != sizeof(sym))
+				{
+					fprintf(stderr, "Error reading symbol\n");
+					return -1;
+				}
 
-                if (sym.st_name == 0) continue;  // Skip unnamed symbols
+				if (sym.st_name == 0) continue;  // Skip unnamed symbols
 
-                if (num_symbols < MAX_SYMBOLS) {
-                    symbols[num_symbols].sym = sym;
-                    symbols[num_symbols].name = strdup(shstrtab + sym.st_name);
-                    num_symbols++;
-                }
-            }
-        }
-    }
+				if (num_symbols < MAX_SYMBOLS)
+				{
+					symbols[num_symbols].sym = sym;
+					symbols[num_symbols].name = strdup(shstrtab + sym.st_name);
+					num_symbols++;
+				}
+			}
+		}
+	}
 
-    qsort(symbols, num_symbols, sizeof(SymbolEntry), compare_symbols);
+	qsort(symbols, num_symbols, sizeof(SymbolEntry), compare_symbols);
 
-    for (int i = 0; i < num_symbols; ++i) {
-        printf("%016lx %s %s\n", (unsigned long)symbols[i].sym.st_value,
-               get_symbol_type(&symbols[i].sym), symbols[i].name);
-        free(symbols[i].name);
-    }
+	for (int i = 0; i < num_symbols; ++i)
+	{
+		printf("%016lx %s %s\n", (unsigned long)symbols[i].sym.st_value,
+			get_symbol_type(&symbols[i].sym), symbols[i].name);
+		free(symbols[i].name);
+	}
 
-    free(shstrtab);
-    return 0;
+	free(shstrtab);
+	return 0;
 }
 
-int process_file(const char *filename) {
-    FILE *file = fopen(filename, "rb");
-    if (!file) {
-        perror("fopen");
-        return -1;
-    }
+int process_file(const char *filename)
+{
+	FILE *file = fopen(filename, "rb");
+	if (!file)
+	{
+		perror("fopen");
+		return -1;
+	}
 
-    int ret = parse_symbols(file);
+	int ret = parse_symbols(file);
 
-    fclose(file);
-    return ret;
+	fclose(file);
+	return ret;
 }
--- nm_objdump/elf_parser.h
+++ nm_objdump/elf_parser.h
@@ -1,6 +1,12 @@
 #ifndef ELF_PARSER_H
 #define ELF_PARSER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <elf.h>
+
 int process_file(const char *filename);
 
 #endif /* ELF_PARSER_H */
