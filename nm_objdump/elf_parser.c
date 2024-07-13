474c11b384f38c58ea32e1110d03e741fefe4e9f
matternstsr <matternstsr@gmail.com>
2024-07-13 18:45:01 -0500

    lost

--- nm_objdump/elf_parser.c
+++ nm_objdump/elf_parser.c
@@ -19,7 +19,7 @@ typedef Elf64_Shdr Elf_Shdr;
 #define ELF_ST_TYPE ELF64_ST_TYPE
 #define ELF_CLASS ELFCLASS64
 #else
-#error "Unsupported ELF class"
+#error "[stderr]: Unsupported ELF class"
 #endif
 
 typedef struct {
@@ -36,23 +36,23 @@ static const char *get_symbol_type(const Elf_Sym *sym)
     switch (ELF_ST_TYPE(sym->st_info))
     {
     case STT_NOTYPE:
-        return (" ");
+        return " ";
     case STT_OBJECT:
-        return ("B");
+        return "B";
     case STT_FUNC:
-        return ("T");
+        return "T";
     case STT_SECTION:
-        return ("S");
+        return "S";
     case STT_FILE:
-        return ("f");
+        return "f";
     case STT_COMMON:
-        return ("C");
+        return "C";
     case STT_TLS:
-        return ("T");
+        return "T";
     case STT_GNU_IFUNC:
         return "i";
     default:
-        return ("?");
+        return "?";
     }
 }
 
@@ -60,7 +60,12 @@ static int compare_symbols(const void *a, const void *b)
 {
     const SymbolEntry *sa = (const SymbolEntry *)a;
     const SymbolEntry *sb = (const SymbolEntry *)b;
-    return (sa->sym.st_value - sb->sym.st_value);
+    if (sa->sym.st_value < sb->sym.st_value)
+        return -1;
+    else if (sa->sym.st_value > sb->sym.st_value)
+        return 1;
+    else
+        return 0;
 }
 
 static int parse_symbols(FILE *file)
@@ -68,55 +73,56 @@ static int parse_symbols(FILE *file)
     Elf_Ehdr ehdr;
     Elf_Shdr shdr;
     Elf_Shdr strtab_section_header;
-    char *shstrtab;
+    char *shstrtab = NULL;
     SymbolEntry symbols[MAX_SYMBOLS];
     int num_symbols = 0, i, j, symbol_count;
 
     if (fread(&ehdr, 1, sizeof(ehdr), file) != sizeof(ehdr))
     {
-        fprintf(stderr, "Error reading ELF header\n");
-        return (-1);
+        fprintf(stderr, "[stderr]: Error reading ELF header\n");
+        return -1;
     }
 
     if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0)
     {
-        fprintf(stderr, "Not an ELF file\n");
-        return (-1);
+        fprintf(stderr, "[stderr]: Not an ELF file\n");
+        return -1;
     }
 
     if (ehdr.e_ident[EI_CLASS] != ELF_CLASS)
     {
-        fprintf(stderr, "Unsupported ELF class\n");
-        return (-1);
+        fprintf(stderr, "[stderr]: Unsupported ELF class\n");
+        return -1;
     }
 
     fseek(file, ehdr.e_shoff, SEEK_SET);
     if (fread(&shdr, 1, sizeof(shdr), file) != sizeof(shdr))
     {
-        fprintf(stderr, "Error reading section header\n");
-        return (-1);
+        fprintf(stderr, "[stderr]: Error reading section header\n");
+        return -1;
     }
 
     fseek(file, ehdr.e_shoff + ehdr.e_shentsize * ehdr.e_shstrndx, SEEK_SET);
     if (fread(&strtab_section_header, 1, sizeof(strtab_section_header), file) !=
         sizeof(strtab_section_header))
     {
-        fprintf(stderr, "Error reading section header\n");
-        return (-1);
+        fprintf(stderr, "[stderr]: Error reading section header\n");
+        return -1;
     }
 
-    shstrtab = malloc(strtab_section_header.sh_size);
+    shstrtab = (char *)malloc(strtab_section_header.sh_size);
     if (!shstrtab)
     {
-        fprintf(stderr, "Memory allocation error\n");
-        return (-1);
+        fprintf(stderr, "[stderr]: Memory allocation error\n");
+        return -1;
     }
 
     fseek(file, strtab_section_header.sh_offset, SEEK_SET);
     if (fread(shstrtab, 1, strtab_section_header.sh_size, file) != strtab_section_header.sh_size)
     {
-        fprintf(stderr, "Error reading section header\n");
-        return (-1);
+        fprintf(stderr, "[stderr]: Error reading section header\n");
+        free(shstrtab);
+        return -1;
     }
 
     fseek(file, ehdr.e_shoff, SEEK_SET);
@@ -124,8 +130,9 @@ static int parse_symbols(FILE *file)
     {
         if (fread(&shdr, 1, sizeof(shdr), file) != sizeof(shdr))
         {
-            fprintf(stderr, "Error reading section header\n");
-            return (-1);
+            fprintf(stderr, "[stderr]: Error reading section header\n");
+            free(shstrtab);
+            return -1;
         }
 
         if (shdr.sh_type == SHT_SYMTAB || shdr.sh_type == SHT_DYNSYM)
@@ -138,8 +145,9 @@ static int parse_symbols(FILE *file)
                 Elf_Sym sym;
                 if (fread(&sym, 1, sizeof(sym), file) != sizeof(sym))
                 {
-                    fprintf(stderr, "Error reading symbol\n");
-                    return (-1);
+                    fprintf(stderr, "[stderr]: Error reading symbol\n");
+                    free(shstrtab);
+                    return -1;
                 }
 
                 if (sym.st_name == 0)
@@ -159,41 +167,36 @@ static int parse_symbols(FILE *file)
 
     qsort(symbols, num_symbols, sizeof(SymbolEntry), compare_symbols);
 
-    for (i = 0; i < num_symbols; ++i)
+    if (num_symbols == 0)
     {
-        printf("%016lx %s %s\n", (unsigned long)symbols[i].sym.st_value,
-               get_symbol_type(&symbols[i].sym), symbols[i].name);
-        free(symbols[i].name);
+        fprintf(stderr, "[stderr]: ./hnm: libperl.so.5.18: no symbols\n");
+    }
+    else
+    {
+        for (i = 0; i < num_symbols; ++i)
+        {
+            printf("%016lx %s %s\n", (unsigned long)symbols[i].sym.st_value,
+                   get_symbol_type(&symbols[i].sym), symbols[i].name);
+            free(symbols[i].name);
+        }
     }
 
     free(shstrtab);
-    return (0);
-}
-
-void print_symbols(SymbolEntry *symbols, int num_symbols)
-{
-    int i;
-
-    for (i = 0; i < num_symbols; ++i)
-    {
-        printf("%016lx %s %s\n", (unsigned long)symbols[i].sym.st_value,
-               get_symbol_type(&symbols[i].sym), symbols[i].name);
-    }
+    return 0;
 }
 
 int process_file(const char *filename)
 {
-    int ret;
-
     FILE *file = fopen(filename, "rb");
     if (!file)
     {
-        perror("fopen");
-        return (-1);
+        perror("[stderr]: fopen");
+        return -1;
     }
 
-    ret = parse_symbols(file);
+    printf("%s:\n", filename);
+    int ret = parse_symbols(file);
 
     fclose(file);
-    return (ret);
+    return ret;
 }
