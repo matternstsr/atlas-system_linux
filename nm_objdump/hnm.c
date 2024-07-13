fa4cea3ae38115c33e3daaf4d55f2426a02a2574
matternstsr <matternstsr@gmail.com>
2024-07-13 17:38:32 -0500

    bunches of betty

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -2,13 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+
 	if (argc < 2)
 	{
 		fprintf(stderr, "Usage: %s [objfile...]\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
 
-	for (int i = 1; i < argc; ++i)
+	for (i = 1; i < argc; ++i)
 	{
 		const char *filename = argv[i];
 		printf("%s:\n", filename);
