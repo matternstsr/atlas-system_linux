9dff58856167d8e77c550ced964934ed58410e0e
matternstsr <matternstsr@gmail.com>
2024-07-14 23:51:26 -0500

    going back what a mess

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -1,43 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <elf.h>
 #include "elf_parser.h"
-/* #include <libelf.h> */
 
-#define MAX_SYMBOLS 1024
-
-typedef struct {
-    Elf64_Sym sym;
-    char *name;
-} SymbolEntry;
-
-static int compare_symbols(const void *a, const void *b) {
-    const SymbolEntry *sa = (const SymbolEntry *)a;
-    const SymbolEntry *sb = (const SymbolEntry *)b;
-    return strcmp(sa->name, sb->name);
-}
-
-static const char *get_symbol_type(Elf64_Sym *sym) {
-    switch (ELF64_ST_TYPE(sym->st_info)) {
-        case STT_NOTYPE:
-            return " ";
-        case STT_OBJECT:
-            return "B";
-        case STT_FUNC:
-            return "T";
-        case STT_SECTION:
-            return "S";
-        case STT_FILE:
-            return "f";
-        case STT_COMMON:
-            return "C";
-        case STT_TLS:
-            return "T";
-        case STT_GNU_IFUNC:
-            return "i";
-        default:
-            return "?";
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [objfile...]\n", argv[0]);
+        return EXIT_FAILURE;
     }
+
+    for (int i = 1; i < argc; ++i) {
+        const char *filename = argv[i];
+        printf("%s:\n", filename);
+        if (process_file(filename) != 0) {
+            fprintf(stderr, "Error processing file: %s\n", filename);
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
