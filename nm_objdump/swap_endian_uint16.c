1a3e343ecf12d013e0fc018a96be4e95f443d011
matternstsr <matternstsr@gmail.com>
2024-07-15 20:24:54 -0500

    adding swapping..not sure this will work for 64 the same way.. betty is gonna hate me  lol

--- nm_objdump/swap_endian_uint16.c
+++ nm_objdump/swap_endian_uint16.c
@@ -3,9 +3,7 @@
 /**
  * swap_endian_uint16 - Switches endianness of a 16-bit unsigned integer.
  * @value: The unsigned integer whose bytes to switch.
- *
  * This function swaps the endianness of a 16-bit unsigned integer.
- *
  * Return: Unsigned integer with switched bytes.
  */
 uint16_t swap_endian_uint16(uint16_t value)
--- nm_objdump/swap_endian_uint32.c
+++ nm_objdump/swap_endian_uint32.c
@@ -3,9 +3,7 @@
 /**
  * swap_endian_uint32 - Swaps endianness of a 32-bit unsigned integer.
  * @num: The unsigned integer whose bytes to swap.
- *
  * This function swaps the endianness of a 32-bit unsigned integer.
- *
  * Return: The unsigned integer with swapped bytes.
  */
 uint32_t swap_endian_uint32(uint32_t num)
