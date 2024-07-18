dc23e1af71806d3f1855222fef703b98a9ca2c62
matternstsr <matternstsr@gmail.com>
2024-07-17 22:40:15 -0500

    not getting the formatting that i need

--- nm_objdump/hobjdump.c
+++ nm_objdump/hobjdump.c
@@ -15,11 +15,9 @@ int main(int ac, char **argv)
         return (fprintf(stderr, USAGE), EXIT_FAILURE);/*Print use on insu args*/
     if (ac == 2)
         return (process_file(argv[1], 0, argv));/*Proc single file only 1 arg*/
-
     /* Process multiple files */
     while (*++_argv)
         ret += process_file(*_argv, 1, argv);
-
     return (ret);/* Return cumulative status */
 }
 
@@ -43,6 +41,7 @@ int process_file(char *file_name, int multiple, char **argv)
 	exit_status = process_and_print_symbols(&elf_header, fd, argv, file_name);
 	return (exit_status); /* return exit status */
 }
+
 
 /**
 * open_and_validate_elf - Open and validate an ELF file.
@@ -100,7 +99,6 @@ int process_and_print_symbols(elf_t *elf_header, int fd, char **argv,
 	size_t num_printed = 0;
 
 	swap_all_endian(elf_header); /* Swap endianness of ELF header */
-
 	exit_status = print_all_symbol_tables(elf_header, fd, &num_printed);
 	/*Print symbol tables */
 	if (exit_status != EXIT_SUCCESS)
@@ -108,12 +106,10 @@ int process_and_print_symbols(elf_t *elf_header, int fd, char **argv,
 				argv[0], file_name);
 	else if (num_printed == 0)
 		fprintf(stderr, "%s: %s: no symbols\n", argv[0], file_name);
-
 	free(elf_header->s32); /* Free allocated memory */
 	free(elf_header->s64);
 	free(elf_header->p32);
 	free(elf_header->p64);
-
 	close(fd); /* Close the file */
 	return (exit_status); /* return exit status */
 }
@@ -163,39 +159,30 @@ void essential_information(elf_t *elf_header, char *string_table)
     unsigned long flags = 0;  /* Flags for ELF file */
 
     printf("architecture: ");  /* Print architecture label */
-
     if (Is_BigE(elf_header->e64))
         printf("UNKNOWN!");  /* Print "UNKNOWN!" for big endian format */
     else if (IS_64)
         printf("i386:x86-64");  /* Print "i386:x86-64" for 64-bit format */
     else
         printf("i386");  /* Print "i386" for default format */
-
     if (EGET(e_type) == ET_REL)
         flags |= HAS_RELOC;  /* Set HAS_RELOC flag if type is relocatable */
     else if (EGET(e_type) == ET_EXEC)
         flags |= EXEC_P;  /* Set EXEC_P flag if type is executable */
     else if (EGET(e_type) == ET_DYN)
         flags |= DYNAMIC;  /* Set DYNAMIC flag if type is dynamic */
-
     if (EGET(e_phnum) > 0)
         flags |= D_PAGED;  /* Set D_PAGED flag if program header table exists */
-
     if (name_search(elf_header, string_table, ".symtab"))
         flags |= HAS_SYMS;  /* Set HAS_SYMS flag if .symtab section exists */
-
     if (name_search(elf_header, string_table, ".dynsym"))
         flags |= HAS_SYMS;  /* Set HAS_SYMS flag if .dynsym section exists */
-
     if (name_search(elf_header, string_table, ".locals"))
         flags |= HAS_LOCALS;  /* Set HAS_LOCALS flag if .locals section exists */
-
     if (name_search(elf_header, string_table, ".lineno"))
         flags |= HAS_LINENO;  /* Set HAS_LINENO flag if .lineno section exists */
-
     if (name_search(elf_header, string_table, ".debug"))
         flags |= HAS_DEBUG;  /* Set HAS_DEBUG flag if .debug section exists */
-
     printf(", flags 0x%08lx:\n", flags);  /* Print flags */
     ef_flags(flags);  /* Print flag details */
     printf("start address 0x%0*lx\n\n", IS_64 ? 16 : 8, EGET(e_entry));  /* Print start address */
@@ -217,13 +204,11 @@ int name_search(elf_t *elf_header, char *string_table, char *section_name)
     size_t i = 0;  /* Loop index */
 
     section_name = strdup(section_name);  /* Duplicate section name */
-
     for (i = 1; i < EGET(e_shnum); i++)
     {
         if (!strcmp(string_table + SGET(i, sh_name), section_name))
             return 1;  /* Return true if section found */
     }
-
     return 0;  /* Return false if section not found */
 }
 
@@ -246,36 +231,25 @@ size_t empty_section(elf_t *elf_header, int fd, size_t i, char *string_table)
     char buf[32] = {0};  /* Buffer for formatting */
 
     printf("Contents of section %s:\n", string_table + SGET(i, sh_name));  /* Print section name */
-
     data = read_data(elf_header, fd, SGET(i, sh_offset), SGET(i, sh_size));  /* Read section data */
     if (!data)
         return 0;  /* Return if data read fails */
-
     addr_len = MAX(4, sprintf(buf, "%lx", SGET(i, sh_addr) + SGET(i, sh_size)));  /* Calculate address length */
-
     for (i_off = 0; i_off < SGET(i, sh_size); i_off += 0x10)
     {
         j_max = MIN(0x10, SGET(i, sh_size) - i_off);  /* Calculate maximum bytes to print */
-
         printf(" %0*lx ", (int)addr_len, SGET(i, sh_addr) + i_off);  /* Print starting address */
-
         for (j = 0; j < j_max; j++)
             printf("%02x%s", data[i_off + j], !((j + 1) % 4) ? " " : "");  /* Print hex bytes */
-
         for (j = j_max; j < 0x10; j++)
             printf("%s%s", "  ", !((j + 1) % 4) ? " " : "");  /* Print padding */
-
         printf(" ");
-        
         for (j = 0; j < j_max; j++)
             printf("%c", (data[i_off + j] >= 32 && data[i_off + j] <= 126) ? data[i_off + j] : '.');  /* Print ASCII representation */
-
         for (j = j_max; j < 0x10; j++)
             printf("%c", ' ');  /* Print ASCII padding */
-        
         printf("\n");
     }
-
     free(data);  /* Free data buffer */
     return numps;  /* Return number of symbols printed */
     (void)fd;  /* Suppress unused parameter warning */
@@ -307,10 +281,8 @@ int empty_sections(elf_t *elf_header, int fd, size_t *num_printed)
 
     for (i = 0; i < EGET(e_shnum); i++)
         swap_all_endian_section(elf_header, i);  /* Swap endianness each sec */
-
     string_table = read_string_table(elf_header, fd);  /* Read string table */
     essential_information(elf_header, string_table);  /* Print ELF hdr info*/
-
     for (i = 1; i < EGET(e_shnum); i++)
     {
         if (strcmp(string_table + SGET(i, sh_name), ".dynstr") &&
@@ -321,10 +293,8 @@ int empty_sections(elf_t *elf_header, int fd, size_t *num_printed)
             SGET(i, sh_type) == SHT_STRTAB ||
             !SGET(i, sh_size)))
             continue;  /* Skip sections that do not meet the criteria */
-
         *num_printed += empty_section(elf_header, fd, i, string_table);  /* Dump section */
     }
-
     free(string_table);  /* Free string table */
     return 0;  /* Return success */
 }
