e3a716f81b8c209e7e79407cb528c2d693bb7ea3
matternstsr <matternstsr@gmail.com>
2024-07-17 21:32:49 -0500

    cleaning up betty

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -40,11 +40,11 @@ int process_file(char *file_name, int multiple, char **argv) {
 
     fd = open_and_validate_elf(file_name, &elf_header, argv);
     if (fd == -1) {
-        return EXIT_FAILURE; /* return failure if ELF file couldn't be processed */
+        return EXIT_FAILURE; /* return failure if ELF file couldn't be proc */
     }
 
     if (multiple) {
-        printf("\n%s:\n", file_name); /* Print filename if processing multiple files */
+        printf("\n%s:\n", file_name); /* Print filename if proc mult files */
     }
 
     exit_status = process_and_print_symbols(&elf_header, fd, argv, file_name);
@@ -59,7 +59,8 @@ int process_file(char *file_name, int multiple, char **argv) {
  * @argv: Argument vector passed to main
  * return: File descriptor on success, -1 on failure
  */
-int open_and_validate_elf(char *file_name, elf_t *elf_header, char **argv) {
+int open_and_validate_elf(char *file_name, elf_t *elf_header, char **argv)
+{
     int fd;
     size_t r;
 
@@ -69,18 +70,24 @@ int open_and_validate_elf(char *file_name, elf_t *elf_header, char **argv) {
     if (fd == -1)
         return -1; /* return failure if file couldn't be opened */
 
-    r = read(fd, &elf_header->e64, sizeof(elf_header->e64)); /* Read ELF header */
-    if (r != sizeof(elf_header->e64) || !is_elf_file((char *)&elf_header->e64)) {
-        fprintf(stderr, "%s: %s: File format not recognized\n", argv[0], file_name);
+    r = read(fd, &elf_header->e64, sizeof(elf_header->e64)); /* ReadELFheader */
+    if (r != sizeof(elf_header->e64) || !is_elf_file((char *)&elf_header->e64))
+	{
+        fprintf(stderr, "%s: %s: File format not recognized\n",
+				argv[0], file_name);
         close(fd);
         return -1; /* return failure if ELF header is not valid */
     }
 
-    if (IS_32(elf_header->e64)) { /* Check if ELF file is 32-bit */
+    if (IS_32(elf_header->e64))
+	{ /* Check if ELF file is 32-bit */
         lseek(fd, 0, SEEK_SET); /* Rewind file pointer */
-        r = read(fd, &elf_header->e32, sizeof(elf_header->e32)); /* Read 32-bit ELF header */
-        if (r != sizeof(elf_header->e32) || !is_elf_file((char *)&elf_header->e32)) {
-            fprintf(stderr, "%s: %s: File format not recognized for 32-bit ELF\n", argv[0], file_name);
+        r = read(fd, &elf_header->e32, sizeof(elf_header->e32)); /*R32 ELF hdr*/
+        if (r != sizeof(elf_header->e32) ||
+						!is_elf_file((char *)&elf_header->e32))
+		{
+            fprintf(stderr, "%s: %s: File format not recognized for " \
+			"32-bit ELF\n", argv[0], file_name);
             close(fd);
             return -1; /* return failure if 32-bit ELF header is not valid */
         }
@@ -97,15 +104,19 @@ int open_and_validate_elf(char *file_name, elf_t *elf_header, char **argv) {
  * @file_name: Name of the file to process
  * return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
  */
-int process_and_print_symbols(elf_t *elf_header, int fd, char **argv, char *file_name) {
+int process_and_print_symbols(elf_t *elf_header, int fd, char **argv,
+							char *file_name)
+{
     int exit_status = EXIT_SUCCESS;
     size_t num_printed = 0;
 
     swap_all_endian(elf_header); /* Swap endianness of ELF header */
 
-    exit_status = print_all_symbol_tables(elf_header, fd, &num_printed); /* Print symbol tables */
+    exit_status = print_all_symbol_tables(elf_header, fd, &num_printed);
+	/*Print symbol tables */
     if (exit_status != EXIT_SUCCESS) {
-        fprintf(stderr, "%s: %s: failed to print symbol tables\n", argv[0], file_name);
+        fprintf(stderr, "%s: %s: failed to print symbol tables\n",
+				argv[0], file_name);
     } else if (num_printed == 0) {
         fprintf(stderr, "%s: %s: no symbols\n", argv[0], file_name);
     }
