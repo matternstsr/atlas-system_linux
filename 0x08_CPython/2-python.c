76e0f0b2729e5390e1ac8bbbd018d47ab4687dad
matternstsr <matternstsr@gmail.com>
2024-07-22 10:46:50 -0500

    doing #2 blind local fails on all

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -59,13 +59,21 @@ void print_python_bytes(PyObject *p)
     /* Print the string representation of the bytes object */
     str = PyBytes_AsString(p);
     printf("  trying string: %s\n", str);  /* Use PyBytes_AsString directly */
-
     /* Print the first 10 bytes of the bytes object in hexadecimal format */
-    printf("  first %zd bytes: ", size < 10 ? size +1 : 10);  /*Adjusted to print up to 10 bytes */
+    printf("  first %zd bytes: ", size < 10 ? size : 10); 
+    /* Adjusted to print up to 10 bytes */
     for (i = 0; i < (size < 10 ? size : 10); i++)
     {
         printf("%02x", (unsigned char)str[i]);
-        if (i < size - 1) /* Changed to iterate up to size */
+        if (i < size - 1)  /* Changed to iterate up to size */
             printf(" ");
     }
+
+    /* Add a terminating 00 if the actual size is less than 10 */
+    if (size < 10)
+    {
+        printf(" 00");
+    }
+
+    printf("\n");
 }
