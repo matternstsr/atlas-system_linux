9c9127501439c54991a6fbef2c77d83e38f81591
matternstsr <matternstsr@gmail.com>
2024-07-22 12:49:55 -0500

    ha ha ha

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -9,8 +9,8 @@ void print_python_string(PyObject *p)
 
     if (!PyUnicode_Check(p))
     {
-        wprintf(stderr, L"[.] string object info\n");
-        wprintf(stderr, L"  [ERROR] Invalid String Object\n");
+        wprintf(L"[.] string object info\n");
+        wprintf(L"  [ERROR] Invalid String Object\n");
     }
 
     length = PyUnicode_GET_LENGTH(p);
@@ -18,20 +18,20 @@ void print_python_string(PyObject *p)
     if (PyUnicode_AS_UNICODE(p))
     {
         ascii = PyUnicode_AsUTF8(p);
-        wprintf(stdout, L"[.] string object info\n");
-        wprintf(stdout, L"  type: compact ascii\n");
-        wprintf(stdout, L"  length: %ld\n", length);
-        wprintf(stdout, L"  value: %s\n", ascii);
+        wprintf(L"[.] string object info\n");
+        wprintf(L"  type: compact ascii\n");
+        wprintf(L"  length: %ld\n", length);
+        wprintf(L"  value: %s\n", ascii);
     }
     else
     {
         unicode_str = PyUnicode_AsUnicode(p);
-        wprintf(stdout, L"[.] string object info\n");
-        wprintf(stdout, L"  type: compact unicode object\n");
-        wprintf(stdout, L"  length: %ld\n", length);
-        wprintf(stdout, L"  value: L");
+        wprintf(L"[.] string object info\n");
+        wprintf(L"  type: compact unicode object\n");
+        wprintf(L"  length: %ld\n", length);
+        wprintf(L"  value: L");
         for (Py_ssize_t i = 0; i < length; ++i)
-            wprintf(stdout, L"%c", unicode_str[i]);
-        wprintf(stdout, L"\n");
+            wprintf(L"%c", unicode_str[i]);
+        wprintf(L"\n");
     }
 }
