91f95aa6dc31de5dc8dea9063781e34dada8d8db
matternstsr <matternstsr@gmail.com>
2024-07-22 11:19:11 -0500

    oops

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -75,10 +75,8 @@ void print_python_bytes(PyObject *p)
 
     /* Add a terminating 00 if the actual size is less than 10 */
     if (size < 10)
-    {
         printf(" 00");
     else if
         printf("00");
-    }
     printf("\n");
 }
