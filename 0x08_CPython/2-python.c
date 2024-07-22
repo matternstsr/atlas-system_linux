b453fc0db5e8a2a6982614b3d7cc861b823f3364
matternstsr <matternstsr@gmail.com>
2024-07-22 10:37:36 -0500

    doing #2 blind local fails on all

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -6,33 +6,39 @@
  *    p: PyObject pointer representing the Python list object.
  */
 
-void print_python_list(PyObject *p)
+void print_python_bytes(PyObject *p)
 {
-    Py_ssize_t size, allocated, i;
-    PyObject *elem;
+    Py_ssize_t size, i;
+    const char *str;
 
-    /* Check if p is a valid PyListObject */
-    if (!PyList_Check(p)) {
-        fprintf(stderr, "[ERROR] Invalid List Object\n");
+    /* Check if p is a valid PyBytesObject */
+    if (!PyBytes_Check(p)) {
+        fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
         return;
     }
 
-    /* Retrieve size (number of elements) & allocated memory of the list */
-    size = PyList_Size(p);
-    allocated = ((PyListObject *)p)->allocated;
+    /* Retrieve the size (length) of the bytes object */
+    size = PyBytes_Size(p);
 
-    /* Print Python list information */
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %zd\n", size);
-    printf("[*] Allocated = %zd\n", allocated);
+    /* Print bytes object information */
+    printf("[.] bytes object info\n");
+    printf("  size: %zd\n", size);
 
-    /* Iterate through each element of the list and print its type */
-    for (i = 0; i < size; i++)
+    /* Print the string representation of the bytes object */
+    str = PyBytes_AsString(p);
+    printf("  trying string: %s\n", str);  /* Use PyBytes_AsString directly */
+
+    /* Print the first 10 bytes of the bytes object in hexadecimal format */
+    printf("  first %zd bytes: ", size < 10 ? size : 10);  /*Adjusted to print up to 10 bytes */
+    for (i = 0; i < (size < 10 ? size : 10); i++)
     {
-        elem = PyList_GetItem(p, i);
-        printf("Element %zd: %s\n", i, Py_TYPE(elem)->tp_name);
+        printf("%02x", (unsigned char)str[i]);
+        if (i < size - 1) /* Changed to iterate up to size */
+            printf(" ");
     }
+    printf(" 00\n");  /* Add the extra '00' byte at the end */
 }
+
 
 /** Print basic information about a Python bytes object.
  *  Arguments:
@@ -60,7 +66,7 @@ void print_python_bytes(PyObject *p)
     PyObject *repr = PyObject_Repr(p);
     const char *repr_str = PyUnicode_AsUTF8(repr);
     printf("  trying string: %s\n", repr_str);
-    Py_XDECREF(repr);  // Release the reference to the repr object
+    Py_XDECREF(repr);  /* Release the reference to the repr object */
 
     /* Print the first 10 bytes of the bytes object in hexadecimal format */
     printf("  first %zd bytes: ", size < 9 ? size + 1:10);
