94df52934b2549a5ba59b87e863ffd8fed2344cd
matternstsr <matternstsr@gmail.com>
2024-07-22 10:27:18 -0500

    doing #2 blind local fails on all

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -5,15 +5,14 @@
  *  Arguments:
  *    p: PyObject pointer representing the Python list object.
  */
-void print_python_list(PyObject *p)
-{
+
+void print_python_list(PyObject *p) {
     Py_ssize_t size, allocated, i;
     PyObject *elem;
 
     /* Check if p is a valid PyListObject */
-    if (!PyList_Check(p))
-    {
-        fprintf(stderr, "Invalid list object\n");
+    if (!PyList_Check(p)) {
+        fprintf(stderr, "[ERROR] Invalid List Object\n");
         return;
     }
 
@@ -27,62 +26,46 @@ void print_python_list(PyObject *p)
     printf("[*] Allocated = %zd\n", allocated);
 
     /* Iterate through each element of the list and print its type */
-    for (i = 0; i < size; i++)
-    {
+    for (i = 0; i < size; i++) {
         elem = PyList_GetItem(p, i);
         printf("Element %zd: %s\n", i, Py_TYPE(elem)->tp_name);
     }
 }
 
-
-#include <Python.h>
-#include <stdio.h>
-
 /** Print basic information about a Python bytes object.
  *  Arguments:
  *    p: PyObject pointer representing the Python bytes object.
  */
-void print_python_bytes(PyObject *p)
-{
+void print_python_bytes(PyObject *p) {
     Py_ssize_t size, i;
-    char *str;
-    PyObject *bytes_obj;
+    const char *str;
 
     /* Check if p is a valid PyBytesObject */
-    if (!PyBytes_Check(p))
-    {
+    if (!PyBytes_Check(p)) {
         fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
         return;
     }
 
     /* Retrieve the size (length) of the bytes object */
     size = PyBytes_Size(p);
-    bytes_obj = PyBytes_FromObject(p);/* save p as bytes object */
 
     /* Print bytes object information */
     printf("[.] bytes object info\n");
     printf("  size: %zd\n", size);
-    printf("  trying string: %s\n", PyUnicode_AsUTF8(bytes_obj));
 
-    /* Print the first 10 bytes of the bytes object */
-    printf("  first %zd bytes: ", size + 1);
+    /* Try to print the string representation of the bytes object */
+    PyObject *repr = PyObject_Repr(p);
+    const char *repr_str = PyUnicode_AsUTF8(repr);
+    printf("  trying string: %s\n", repr_str);
+    Py_XDECREF(repr);  // Release the reference to the repr object
+
+    /* Print the first 10 bytes of the bytes object in hexadecimal format */
+    printf("  first %zd bytes: ", size < 10 ? size : 10);
     str = PyBytes_AsString(p);
-    if (size < 10)
-    {
-        for (i = 0; i < size; i++)
-        {
-            printf("%02hhx", str[i]);
-            if (i < size - 1)
-                printf(" ");
-        }
-    } else {
-        for (i = 0; i < 10; i++)
-        {
-            printf("%02hhx", str[i]);
-            if (i < 9)
-                printf(" ");
-        }
+    for (i = 0; i < (size < 10 ? size : 10); i++) {
+        printf("%02x", (unsigned char)str[i]);
+        if (i < (size < 10 ? size : 10) - 1)
+            printf(" ");
     }
     printf("\n");
-    Py_DECREF(bytes_obj);  /* Release the reference to the bytes object */
 }
