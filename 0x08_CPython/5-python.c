068d7874816332486b7947b32af1c7a537f0a738
matternstsr <matternstsr@gmail.com>
2024-07-22 13:14:57 -0500

    test

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <Python.h>
 
-void print_python_int(PyObject *p)
-{
-
-    longnum = (PyLongObject *)p;
-    PyLongObject *longnum;
-
+void print_python_int(PyObject *p) {
     /* Check if p is a Python integer */
-    if (!p || !PyLong_Check(p))
+    if (!p || !PyLong_Check(p)) {
         printf("Invalid Int Object\n");
+        return;
+    }
 
     /* Extract the value of the Python integer */
     unsigned long x = PyLong_AsUnsignedLong(p);
-    if (x == (unsigned long)-1 && PyErr_Occurred())
-    {
-        /* PyErr_Occurred() returns true if prob in PyLong_AsUnsignedLong */
+    if (x == (unsigned long)-1 && PyErr_Occurred()) {
+        /* PyErr_Occurred() returns true if a prob PyLong_AsUnsignedLong */
+        PyErr_Clear(); /* Clear the error indicator */
         printf("C unsigned long int overflow\n");
-    }
-    else
+    } else {
         /* Print the value of x */
         printf("%lu\n", x);
+    }
 }
+
