0603bd322dedfa20c291499fb299d413a6979d92
matternstsr <matternstsr@gmail.com>
2024-07-22 13:31:34 -0500

    test

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -1,26 +1,41 @@
-#include <stdio.h>
 #include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void print_python_int(PyObject *p)
-{
+void print_python_int(PyObject *p) {
     /* Check if p is a Python integer */
-    if (!p || !PyLong_Check(p))
-    {
+    if (!p || !PyLong_Check(p)) {
         printf("Invalid Int Object\n");
         return;
     }
 
-    /* Convert Python integer to C long long */
-    unsigned long x = PyLong_AsUnsignedLongLong(p);
+    /* Cast p to PyLongObject pointer */
+    PyLongObject *num = (PyLongObject *) p;
 
-    /* Check for overflow or underflow */
-    if (x == (unsigned long)-1 && PyErr_Occurred())
-    {
-        PyErr_Clear();
-        printf("C long overflow\n");
+    /* Extract the size of the Python integer */
+    ssize_t size = num->ob_size;
+
+    /* Check if the Python integer is negative */
+    int neg = 0;
+    if (size < 0) {
+        neg = 1;
+        size = -size;
+    }
+
+    /* Check for overflow (size > 3 is considered overflow) */
+    if (size > 3) {
+        printf("C unsigned long int overflow\n");
         return;
     }
 
-    /* Print the value */
-    printf("%ld\n", x);
+    /* Calculate the value of the Python integer manually */
+    unsigned long long total = 0;
+    for (ssize_t i = size - 1; i >= 0; i--) {
+        total = (total << PyLong_SHIFT) + num->ob_digit[i];
+    }
+
+    /* Print the value with appropriate sign */
+    if (neg)
+        printf("-");
+    printf("%llu\n", total);
 }
