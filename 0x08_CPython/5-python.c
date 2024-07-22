7fc0fe2c99f61d80415d8b99e7ef0fabf992a3dc
matternstsr <matternstsr@gmail.com>
2024-07-22 13:49:25 -0500

    test

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 void print_python_int(PyObject *p) {
+    // Check if p is NULL or not a Python long integer
     if (!p || !PyLong_Check(p)) {
         printf("Invalid Int Object\n");
         return;
@@ -10,24 +11,16 @@ void print_python_int(PyObject *p) {
     // Cast p to PyLongObject pointer
     PyLongObject *num = (PyLongObject *) p;
 
-    // Check if the number is negative
-    int neg = (num->ob_size < 0);
+    // Determine if the number is negative
+    int neg = (num->ob_digit[num->ob_size - 1] >> (PyLong_SHIFT - 1));
 
     // Prepare variables for calculation
     unsigned long int total = 0;
     const unsigned long int base = (1UL << PyLong_SHIFT);
 
-    // Determine the start and end of the digit array to iterate
-    ssize_t start = 0;
-    ssize_t end = num->ob_size - 1;
-
-    // Adjust start and end indices based on negative sign
-    if (neg) {
-        start = 1;
-    }
-
     // Iterate through the digits of the number
-    for (ssize_t i = start; i <= end; ++i) {
+    ssize_t start = neg ? 1 : 0;
+    for (ssize_t i = start; i < num->ob_size; ++i) {
         // Check for overflow
         if (total > (ULONG_MAX - ((unsigned long int)num->ob_digit[i] * base)) / base) {
             printf("C unsigned long int overflow\n");
