a67f84ceada1e8cabf1133fa0e9d7e155e893877
matternstsr <matternstsr@gmail.com>
2024-07-22 11:01:15 -0500

    oops

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -67,7 +67,7 @@ void print_python_bytes(PyObject *p)
         printf("%02x", (unsigned char)str[i]);
         if (i + 1 < size)  /* Changed to iterate up to size */
             printf(" ");
-        else if (i =10)
+        else if (i = 10)
             printf(" ");
     }
 
