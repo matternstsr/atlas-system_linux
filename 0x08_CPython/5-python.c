84bba9b05e31efbcf52950abbf1534efa9a2d11e
matternstsr <matternstsr@gmail.com>
2024-07-22 13:41:35 -0500

    test

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -1,31 +1,37 @@
 #include <Python.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 void print_python_int(PyObject *p)
 {
     /* Check if p is a Python integer */
     if (!p || !PyLong_Check(p))
+    {
         printf("Invalid Int Object\n");
+        return;
+    }
 
-    /* Cast p to PyVarObject pointer */
-    PyVarObject *num = (PyVarObject *) p;
+    /* Cast p to PyLongObject pointer */
+    PyLongObject *num = (PyLongObject *) p;
 
     /* Initialize variables */
     int neg = 0;
     unsigned long int total = 0;
+    unsigned long int base = 1;
 
     /* Determine if the number is negative */
     if (num->ob_digit[num->ob_size - 1] >> (PyLong_SHIFT - 1))
         neg = 1;
 
+    /* Calculate the total value of the Python integer */
+    for (ssize_t i = 0; i < num->ob_size; i++)
+    {
+        total += num->ob_digit[i] * base;
+        base <<= PyLong_SHIFT;
+    }
+
     /* Print the value with appropriate sign */
     if (neg)
         printf("-");
-
-    /* Calculate the total value of the Python integer */
-    for (ssize_t i = num->ob_size - 1; i >= 0; i--)
-        total = (total << PyLong_SHIFT) + num->ob_digit[i];
 
     /* Print the calculated value */
     printf("%lu\n", total);
