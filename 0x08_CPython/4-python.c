a9a6669ba0ea8fc8589d1a89aded633601f95a74
matternstsr <matternstsr@gmail.com>
2024-07-22 13:03:52 -0500

    ha ha ha

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -4,6 +4,8 @@
 void print_python_string(PyObject *p)
 {
     Py_ssize_t length;
+    Py_UNICODE *unicode_str;
+    PyASCIIObject *str;
     char *ascii;
     
     str = (PyASCIIObject *)p;
@@ -18,21 +20,16 @@ void print_python_string(PyObject *p)
 
     if (str->state.ascii)
     {
-        ascii = PyUnicode_AsUTF8(p);
         wprintf(L"[.] string object info\n");
         wprintf(L"  type: compact ascii\n");
         wprintf(L"  length: %ld\n", length);
-        wprintf(L"  value: %s\n", ascii);
+	    wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
     }
     else
     {
-        unicode_str = PyUnicode_AsUnicode(p);
         wprintf(L"[.] string object info\n");
         wprintf(L"  type: compact unicode object\n");
         wprintf(L"  length: %ld\n", length);
-        wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
-        for (Py_ssize_t i = 0; i < length; ++i)
-            wprintf(L"%c", unicode_str[i]);
-        wprintf(L"\n");
+	    wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
     }
 }
