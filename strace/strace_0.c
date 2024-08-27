949394c539828e594c991b87798ef5fcfb1b64a2
matternstsr <matternstsr@gmail.com>
2024-08-27 17:25:16 -0500

    just to see

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ptrace.h>
-#include <sys/user.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/user.h>
 
 /**
  * main - The entry point of the program
@@ -19,7 +20,7 @@
  * 
  * Return: Returns 0 on successful execution, or exits with status 1 if an error occurs.
  */
-int main(int argc, char **argv, char **envp)
+int main(int argc, const char *argv[], char *const envp[])
 {
     pid_t child;
     int status, print_check = 0;
@@ -39,7 +40,7 @@ int main(int argc, char **argv, char **envp)
     if (child == 0) {
         // In the child process
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execve(argv[1], argv + 1, envp);
+        execve(argv[1], (char *const *)(argv + 1), (char *const *)envp);
         perror("execve"); // Handle execve error
         return 1; // Return 1 to indicate an error in execve
     } else {
@@ -50,36 +51,13 @@ int main(int argc, char **argv, char **envp)
             if (WIFEXITED(status))
                 break;
 
-            // Use get_regs to retrieve the system call numbers
-            if (get_regs(child, &regs) == 0 && should_print(print_check)) {
+            // Retrieve and print system call numbers
+            ptrace(PTRACE_GETREGS, child, NULL, &regs);
+            if (print_check == 0 || print_check % 2 != 0) {
                 fprintf(stderr, "%lu\n", (unsigned long)regs.orig_rax);
-                fflush(stderr); // Flush stderr to ensure output is written
             }
             print_check++;
         }
     }
     return 0;
-}
-
-/**
- * get_regs - Retrieve the registers of the child process using ptrace
- * @child: The PID of the child process to retrieve registers from.
- * @regs: A pointer to a user_regs_struct to store the register values.
- * 
- * Return: 0 on success, or -1 on failure.
- */
-static inline int get_regs(pid_t child, user_regs *regs)
-{
-    return ptrace(PTRACE_GETREGS, child, NULL, regs);
-}
-
-/**
- * should_print - Determine if the system call number should be printed
- * @check: The current check value to decide printing logic.
- * 
- * Return: 1 if the system call number should be printed, 0 otherwise.
- */
-static inline int should_print(int check)
-{
-    return !check || (check & 1);
 }
