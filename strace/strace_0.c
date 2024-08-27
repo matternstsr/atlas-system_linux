1eeeefc085d6af5fb018bf41839c2fadc5249abb
matternstsr <matternstsr@gmail.com>
2024-08-27 16:27:59 -0500

    hmmmmmm

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -1,4 +1,10 @@
 #include "syscalls.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /**
  * main - The entry point of the program
@@ -16,47 +22,63 @@
 int main(int argc, char **argv, char **envp)
 {
     pid_t child;
-	int status, check = 0;
-	user_regs regs;
+    int status, check = 0;
+    user_regs regs;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
-		return (-1);
-	}
+        return 1; // Return 1 to indicate an error in usage
+    }
 
-	child = fork();
-	if (child == -1)
-		perror(argv[1]), exit(1);
-	else if (child == 0)
-	{
-		ptrace(PTRACE_TRACEME, child, NULL, NULL);
-		execve(argv[1], (char * const *)(argv + 1), (char * const *)envp);
-	}
-	else
-	{
-		while (1)
-        {
+    child = fork();
+    if (child == -1) {
+        perror("fork");
+        return 1; // Return 1 to indicate an error in fork
+    }
+
+    if (child == 0) {
+        // In the child process
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execve(argv[1], (char * const *)(argv + 1), (char * const *)envp);
+        perror("execve"); // Handle execve error
+        return 1; // Return 1 to indicate an error in execve
+    } else {
+        // In the parent process
+        while (1) {
             ptrace(PTRACE_SYSCALL, child, NULL, NULL);
             wait(&status);
             if (WIFEXITED(status))
                 break;
             ptrace(PTRACE_GETREGS, child, NULL, &regs);
-			if (check == 0 || check % 2 != 0)
-				fprintf(stderr, "%lu\n", (size_t)regs.orig_rax);
-                fflush(stdout);
+
+            if (check == 0 || check % 2 != 0) {
+                fprintf(stderr, "%lu\n", (unsigned long)regs.orig_rax);
+                fflush(stderr); // Flush stderr to ensure output is written
+            }
             check++;
         }
-        
     }
     return 0;
 }
 
-
+/**
+ * get_regs - Retrieve the registers of the child process using ptrace
+ * @child: The PID of the child process to retrieve registers from.
+ * @regs: A pointer to a user_regs_struct to store the register values.
+ * 
+ * Return: 0 on success, or -1 on failure.
+ */
 static inline int get_regs(pid_t child, user_regs *regs)
 {
     return ptrace(PTRACE_GETREGS, child, NULL, regs);
 }
 
+/**
+ * should_print - Determine if the system call number should be printed
+ * @check: The current check value to decide printing logic.
+ * 
+ * Return: 1 if the system call number should be printed, 0 otherwise.
+ */
 static inline int should_print(int check)
 {
     return !check || (check & 1);
