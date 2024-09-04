01a8b080ec963ab4d488f6724fe4f976149a103a
matternstsr <matternstsr@gmail.com>
2024-09-04 16:18:12 -0500

    going back I DONT GET THIS:

--- multithreading/list.h
+++ multithreading/list.h
@@ -12,32 +12,32 @@
  */
 typedef struct node_s
 {
-    void        *content;
-    struct node_s    *prev;
-    struct node_s    *next;
+	void		*content;
+	struct node_s	*prev;
+	struct node_s	*next;
 } node_t;
 
 /**
  * struct list_s - List structure
  *
- * @head: Pointer to the front node
- * @tail: Pointer to the back node
+ * @head: Ponter to the front node
+ * @tail: Ponter to the back node
  * @size: Number of nodes in the list
  */
 typedef struct list_s
 {
-    node_t  *head;
-    node_t  *tail;
-    size_t  size;
+	node_t	*head;
+	node_t	*tail;
+	size_t	size;
 } list_t;
 
 typedef void (*node_func_t)(void *);
 
 /* list.c */
-node_t  *node_create(void *content);
-node_t  *list_add(list_t *list, void *content);
-list_t *list_init(void);
-void    list_destroy(list_t *list, node_func_t free_func);
-void    list_each(list_t *list, node_func_t func);
+node_t	*node_create(void *content);
+node_t	*list_add(list_t *list, void *content);
+list_t	*list_init(list_t *list);
+void	list_destroy(list_t *list, node_func_t free_func);
+void	list_each(list_t *list, node_func_t func);
 
 #endif /* LIST_H */
