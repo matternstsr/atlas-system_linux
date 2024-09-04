59d098733a3781e919b5abbfbc2378a9041dab10
matternstsr <matternstsr@gmail.com>
2024-09-04 15:13:58 -0500

    going back I DONT GET THIS:

--- multithreading/list.h
+++ multithreading/list.h
@@ -14,5 +14,6 @@ typedef struct list_s
 list_t *list_create(size_t initial_capacity);
 void list_add(list_t *list, unsigned long value);
 void list_free(list_t *list);
+void list_destroy(list_t *list, void (*free_func)(void *));
 
 #endif /* LIST_H */
