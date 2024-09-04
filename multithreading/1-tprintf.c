23cedb5aca552c2d8631a51eb9a01648d7a4c109
matternstsr <matternstsr@gmail.com>
2024-09-03 19:52:23 -0500

    1-tprintf.c

---
+++ multithreading/1-tprintf.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <pthread.h>
+#include "multithreading.h"
+
+/**
+* tprintf - Print a formatted string with the thread ID.
+* @format: Format string for output.
+*
+* Prints the formatted string preceded by the thread ID.
+*
+* Return: Number of characters printed (like `printf`).
+*/
+int tprintf(const char *format, ...)
+{
+	va_list args;
+	pthread_t thread_id;
+	int ret;
+
+	/* Get the current thread ID */
+	thread_id = pthread_self();
+
+	/* Print the thread ID */
+	printf("[%lu] ", (unsigned long)thread_id);
+
+	/* Initialize variable argument list */
+	va_start(args, format);
+
+	/* Print the formatted string */
+	ret = vprintf(format, args);
+
+	/* End variable argument list */
+	va_end(args);
+
+	/* Return the number of characters printed */
+	return ret;
+}
