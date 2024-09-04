167647af390ded12309d05fecf544259388ee184
matternstsr <matternstsr@gmail.com>
2024-09-03 19:09:28 -0500

    0-thread_entry.c

--- multithreading/0-thread_entry.c
+++ multithreading/0-thread_entry.c
@@ -3,22 +3,22 @@
 #include "multithreading.h"
 
 /**
- * thread_entry - Thread entry point function.
- * @arg: arg passed to the thread. (Usually a ptr to data or a str.)
- *
- * This func is called when the thread starts. It processes the arg 
- * and does the thread's tasks.
- *
- * Return: ALWAYS returns NULL.
- */
+* thread_entry - Thread entry point function.
+* @arg: arg passed to the thread. (Usually a ptr to data or a str.)
+*
+* This func is called when the thread starts. It processes the arg 
+* and does the thread's tasks.
+*
+* Return: ALWAYS returns NULL.
+*/
 void *thread_entry(void *arg)
 {
-    /* Cast argument to a string */
-    char *str = (char *)arg;
+	/* Cast argument to a string */
+	char *str = (char *)arg;
 
-    /* Print the string */
-    printf("%s\n", str);
+	/* Print the string */
+	printf("%s\n", str);
 
-    /* Return NULL */
-    pthread_exit (NULL);
+	/* Return NULL */
+	pthread_exit (NULL);
 }
