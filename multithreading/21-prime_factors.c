7f2f2130885df43be39cf249a12c969714b37346
matternstsr <matternstsr@gmail.com>
2024-09-04 18:06:37 -0500

    spaces

--- multithreading/21-prime_factors.c
+++ multithreading/21-prime_factors.c
@@ -4,53 +4,53 @@
 #include "list.h"
 
 /**
- * prime_factors - Factors a given number into a list containing prime factors
- *
- * @s: string of the number to factor
- * Return: list of prime factors
- **/
+* prime_factors - Factors a given number into a list containing prime factors
+*
+* @s: string of the number to factor
+* Return: list of prime factors
+**/
 list_t *prime_factors(char const *s)
 {
-    unsigned long num = strtoul(s, NULL, 10);
-    if (num == 0) {
-        fprintf(stderr, "Invalid number: %s\n", s);
-        return NULL;
-    }
+	unsigned long num = strtoul(s, NULL, 10);
+	if (num == 0) {
+		fprintf(stderr, "Invalid number: %s\n", s);
+		return NULL;
+	}
 
-    unsigned long *temp;
-    unsigned long primenum = 2;
-    list_t *prime_list = malloc(sizeof(list_t));
+	unsigned long *temp;
+	unsigned long primenum = 2;
+	list_t *prime_list = malloc(sizeof(list_t));
 	list_init(prime_list);
 
-    while (primenum * primenum <= num)
-    {
-        while (num % primenum == 0)
-        {
-            temp = malloc(sizeof(unsigned long));
-            if (!temp) {
-                perror("Failed to allocate memory for prime factor");
-                list_destroy(prime_list, free);
-                exit(EXIT_FAILURE);
-            }
-            *temp = primenum;
-            list_add(prime_list, temp);
-            num /= primenum;
-        }
+	while (primenum * primenum <= num)
+	{
+		while (num % primenum == 0)
+		{
+			temp = malloc(sizeof(unsigned long));
+			if (!temp) {
+				perror("Failed to allocate memory for prime factor");
+				list_destroy(prime_list, free);
+				exit(EXIT_FAILURE);
+			}
+			*temp = primenum;
+			list_add(prime_list, temp);
+			num /= primenum;
+		}
 
-        primenum += 1 + (primenum != 2);
-    }
+		primenum += 1 + (primenum != 2);
+	}
 
-    if (num >= 2)
-    {
-        temp = malloc(sizeof(unsigned long));
-        if (!temp) {
-            perror("Failed to allocate memory for prime factor");
-            list_destroy(prime_list, free);
-            exit(EXIT_FAILURE);
-        }
-        *temp = num;
-        list_add(prime_list, temp);
-    }
+	if (num >= 2)
+	{
+		temp = malloc(sizeof(unsigned long));
+		if (!temp) {
+			perror("Failed to allocate memory for prime factor");
+			list_destroy(prime_list, free);
+			exit(EXIT_FAILURE);
+		}
+		*temp = num;
+		list_add(prime_list, temp);
+	}
 
-    return prime_list;
+	return prime_list;
 }
