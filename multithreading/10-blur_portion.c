7cd4ccb2a8ef246f0535703acd917f167f1e0045
matternstsr <matternstsr@gmail.com>
2024-09-04 20:13:38 -0500

    descriptions

--- multithreading/10-blur_portion.c
+++ multithreading/10-blur_portion.c
@@ -18,50 +18,64 @@ static int is_valid_pixel(img_t const *img, size_t x, size_t y);
 *
 */
 static void apply_gaussian_blur(img_t const *img, img_t *img_blur,
-	kernel_t const *kernel, size_t x_start, size_t y_start, size_t width,
-	size_t height)
+    kernel_t const *kernel, size_t x_start, size_t y_start, size_t width,
+    size_t height)
 {
-	pixel_t *pixels, *pixels_blur;
-	size_t pixel_x, pixel_y, pixel_index, kernel_half_size, ki, kj;
-	float r, g, b, weight_sum, weight;
+    pixel_t *pixels, *pixels_blur;
+    size_t pixel_x, pixel_y, pixel_index, kernel_half_size, ki, kj;
+    float r, g, b, weight_sum, weight;
 
-	kernel_half_size = kernel->size / 2;
-	pixels = img->pixels;
-	pixels_blur = img_blur->pixels;
+    kernel_half_size = kernel->size / 2;
+    pixels = img->pixels;
+    pixels_blur = img_blur->pixels;
 
-	for (size_t y = y_start; y < y_start + height; y++)
-	{ /* Looping Over Pixels: */
-		for (size_t x = x_start; x < x_start + width; x++)
-		{
-			r = 0;
-			g = 0;
-			b = 0;
-			weight_sum = 0.0;
-			for (ki = 0; ki < kernel->size; ki++)
-			{
-				for (kj = 0; kj < kernel->size; kj++)
-				{ /* Applying the Kernel: */
-					pixel_x = x + ki - kernel_half_size;
-					pixel_y = y + kj - kernel_half_size;
-
-					if (is_valid_pixel(img, pixel_x, pixel_y))
-					{
-						pixel_index = pixel_y * img->w + pixel_x;
-						weight = kernel->matrix[ki][kj];
-						r += pixels[pixel_index].r * weight;
-						g += pixels[pixel_index].g * weight;
-						b += pixels[pixel_index].b * weight;
-						weight_sum += weight;
-					}
-				}
-			} /* Updating the Blurred Image */
-			pixel_index = y * img->w + x;
-			pixels_blur[pixel_index].r = (char)(r / weight_sum);
-			pixels_blur[pixel_index].g = (char)(g / weight_sum);
-			pixels_blur[pixel_index].b = (char)(b / weight_sum);
-		}
-	}
+    float kernel_weight_sum = 0.0;/* Ensure the kernel weights sum to 1 */
+    for (ki = 0; ki < kernel->size; ki++) {
+        for (kj = 0; kj < kernel->size; kj++) {
+            kernel_weight_sum += kernel->matrix[ki][kj];
+        }
+    }
+    if (kernel_weight_sum == 0) {
+        kernel_weight_sum = 1; /* Prevent division by zero */
+    }
+    for (size_t y = y_start; y < y_start + height; y++)
+    { 
+        for (size_t x = x_start; x < x_start + width; x++)
+        {
+            r = 0;
+            g = 0;
+            b = 0;
+            weight_sum = 0.0;
+            for (ki = 0; ki < kernel->size; ki++)
+            {
+                for (kj = 0; kj < kernel->size; kj++)
+                { 
+                    pixel_x = x + ki - kernel_half_size;
+                    pixel_y = y + kj - kernel_half_size;
+                    if (is_valid_pixel(img, pixel_x, pixel_y))
+                    {
+                        pixel_index = pixel_y * img->w + pixel_x;
+                        weight = kernel->matrix[ki][kj];
+                        r += pixels[pixel_index].r * weight;
+                        g += pixels[pixel_index].g * weight;
+                        b += pixels[pixel_index].b * weight;
+                        weight_sum += weight;
+                    }
+                }
+            } /* Normalize the result */
+            if (weight_sum > 0) {
+                r /= weight_sum;
+                g /= weight_sum;
+                b /= weight_sum;
+            }
+            pixel_index = y * img->w + x;
+            pixels_blur[pixel_index].r = (char)fmin(fmax(r, 0), 255);
+            pixels_blur[pixel_index].g = (char)fmin(fmax(g, 0), 255);
+            pixels_blur[pixel_index].b = (char)fmin(fmax(b, 0), 255);
+        }
+    }
 }
+
 
 /**
 * is_valid_pixel - checks if given pixel coordinates are within image bounds.
