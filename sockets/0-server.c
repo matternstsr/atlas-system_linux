d88f60f8c2559e59cac539856a533d5c13e2fa4d
matternstsr <matternstsr@gmail.com>
2024-10-10 22:56:27 -0500

    trying 0

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -12,38 +12,38 @@
 
 int main()
 {
-    int sockfd;
-    struct sockaddr_in server_addr;
+	int sockfd;
+	struct sockaddr_in server_addr;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); /* Create a socket */
-    if (sockfd < 0)
-    {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-    /* Prepare the sockaddr_in structure */
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET; /* IPv4 optimizes direction used*/
-    server_addr.sin_addr.s_addr = INADDR_ANY;  /* Any address */
-    server_addr.sin_port = htons(PORT);  /* Port number */
-    /* Bind the socket */
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("Bind failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-    if (listen(sockfd, 5) < 0) /* Listen on the socket */
-    {
-        perror("Listen failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-    printf("Server listening on port %d\n", PORT);
-    while (1) /* Hang indefinitely */
-    {
-        pause();  /* Suspend the process until a signal is received */
-    }
-    close(sockfd); /* Close the socket (not reachable) */
-    return 0;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); /* Create a socket */
+	if (sockfd < 0)
+	{
+		perror("Socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	/* Prepare the sockaddr_in structure */
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET; /* IPv4 optimizes direction used*/
+	server_addr.sin_addr.s_addr = INADDR_ANY;  /* Any address */
+	server_addr.sin_port = htons(PORT);  /* Port number */
+	/* Bind the socket */
+	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("Bind failed");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+	if (listen(sockfd, 5) < 0) /* Listen on the socket */
+	{
+		perror("Listen failed");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+	printf("Server listening on port %d\n", PORT);
+	while (1) /* Hang indefinitely */
+	{
+		pause();  /* Suspend the process until a signal is received */
+	}
+	close(sockfd); /* Close the socket (not reachable) */
+	return 0;
 }
