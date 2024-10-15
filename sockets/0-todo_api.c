39f1b2fbdfa4d79a70300f5d0db1bb3ebf661f2f
matternstsr <matternstsr@gmail.com>
2024-10-15 17:02:21 -0500

    hmm

--- sockets/0-todo_api.c
+++ sockets/0-todo_api.c
@@ -4,18 +4,34 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 12345
+#define PORT 8080
 #define BUFFER_SIZE 1024
+
+void handle_client(int client_sock)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_received;
+
+	bytes_received = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+	if (bytes_received < 0)
+	{
+		perror("Receive failed");
+		return;
+	}
+	buffer[bytes_received] = '\0';
+
+	printf("Raw request: %s\n", buffer);
+
+	const char *http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!";
+	send(client_sock, http_response, strlen(http_response), 0);
+}
 
 int main(void)
 {
 	int sockfd, newsockfd;
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t addr_len = sizeof(client_addr);
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_received;
 
-	/* Create a socket */
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 	{
@@ -23,13 +39,11 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	/* Prepare the sockaddr_in structure */
 	memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family = AF_INET; /* IPv4 */
-	server_addr.sin_addr.s_addr = INADDR_ANY; /* Any address */
-	server_addr.sin_port = htons(PORT); /* Port number */
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(PORT);
 
-	/* Bind the socket */
 	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
 	{
 		perror("Bind failed");
@@ -37,36 +51,29 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	/* Listen for incoming connections */
 	if (listen(sockfd, 5) < 0)
 	{
 		perror("Listen failed");
 		close(sockfd);
 		exit(EXIT_FAILURE);
 	}
+
 	printf("Server listening on port %d\n", PORT);
 
-	/* Accept an incoming connection */
-	newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_len);
-	if (newsockfd < 0)
+	while (1)
 	{
-		perror("Accept failed");
-		close(sockfd);
-		exit(EXIT_FAILURE);
+		newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_len);
+		if (newsockfd < 0)
+		{
+			perror("Accept failed");
+			continue;
+		}
+
+		printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
+		handle_client(newsockfd);
+		close(newsockfd);
 	}
 
-	/* Receive a message from the client */
-	bytes_received = recv(newsockfd, buffer, sizeof(buffer) - 1, 0);
-	if (bytes_received < 0)
-		perror("Receive failed");
-	else
-	{
-		buffer[bytes_received] = '\0'; /* Null-terminate the received string */
-		printf("Message received: \"%s\"\n", buffer);
-	}
-
-	/* Close the connections */
-	close(newsockfd);
 	close(sockfd);
-	return 0;
+	return (0);
 }
