20737c4fa9a0f4d32ddcdfc389f6992b5bed7587
matternstsr <matternstsr@gmail.com>
2024-10-15 18:52:16 -0500

    hmm

--- sockets/1-todo_api.c
+++ sockets/1-todo_api.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define RESPONSE "HTTP/1.1 200 OK\r\n\r\n"
+#define MESSAGE "HTTP/1.1 200 OK\r\n\r\n"
 
-void parse_headers(char *request);
+/**
+ * query_parser - Parses an HTTP query
+ * @query: Query string to parse
+ */
+void query_parser(char *query);
 
 /**
  * main - Entry point of the server
@@ -16,16 +21,16 @@ void parse_headers(char *request);
  */
 int main(void)
 {
-    int server_fd, client_fd;
-    size_t received_bytes = 0;
-    char request_buffer[4096];
+    int socket_fd, client_fd;
+    size_t bytes = 0;
+    char buffer[4096], method[50], path[50], query[255], response[32] = MESSAGE;
     struct sockaddr_in address;
-    socklen_t addr_len = sizeof(address);
+    socklen_t addrlen = sizeof(address);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1)
     {
-        perror("socket creation failed");
+        perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
@@ -33,74 +38,74 @@ int main(void)
     address.sin_port = htons(8080);
     address.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    if (bind(socket_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
-        perror("binding failed");
+        perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port 8080\n");
     fflush(stdout);
 
-    if (listen(server_fd, 5) < 0)
+    if (listen(socket_fd, 5) < 0)
     {
-        perror("listening failed");
+        perror("listen failed");
         exit(EXIT_FAILURE);
     }
 
     while (1)
     {
-        client_fd = accept(server_fd, (struct sockaddr *)&address, &addr_len);
+        client_fd = accept(socket_fd, (struct sockaddr *)&address, &addrlen);
         if (client_fd < 0)
         {
-            perror("connection acceptance failed");
+            perror("accept failed");
             exit(EXIT_FAILURE);
         }
 
         printf("Client connected: %s\n", inet_ntoa(address.sin_addr));
-        fflush(stdout);
-
-        received_bytes = recv(client_fd, request_buffer, sizeof(request_buffer) - 1, 0);
-        if (received_bytes > 0)
+        bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+        if (bytes > 0)
         {
-            request_buffer[received_bytes] = '\0'; /* Null-terminate the buffer */
-            printf("Raw request: \"%s\"\n", request_buffer);
+            buffer[bytes] = '\0'; /* Null-terminate the buffer */
+            printf("Raw request: \"%s\"\n", buffer);
             fflush(stdout);
-            parse_headers(request_buffer); /* Use the new header parser */
+            sscanf(buffer, "%s %[^?]?%s", method, path, query);
+            printf("Path: %s\n", path);
+            fflush(stdout);
+            query_parser(query);
         }
 
-        send(client_fd, RESPONSE, sizeof(RESPONSE) - 1, 0);
+        send(client_fd, response, sizeof(response) - 1, 0);
         close(client_fd);
     }
 
-    close(server_fd);
-    return (0);
+    close(socket_fd);
+    return 0;
 }
 
 /**
- * parse_headers - Parses an HTTP request headers
- * @request: The request string to parse
+ * query_parser - Parses an HTTP query
+ * @query: Query string to parse
  */
-void parse_headers(char *request)
+void query_parser(char *query)
 {
-    int index = 0;
-    char *line_token;
-    char *header_lines[16] = {0}; /* Array to store header lines */
-    char header_key[50], header_value[50];
+    int i = 0, flag = 0;
+    char *token = NULL, *key_vals[16] = {0}, key[50], val[50];
 
-    line_token = strtok(request, "\r\n");
-    while (line_token)
+    do
     {
-        header_lines[index++] = line_token; /* Store each line */
-        line_token = strtok(NULL, "\r\n");
-    }
-
-    /* Process the headers starting from index 1 (skip request line) */
-    for (index = 1; header_lines[index]; index++)
-    {
-        if (sscanf(header_lines[index], "%[^:]: %s", header_key, header_value) == 2)
+        token = strsep(&query, "&");
+        if (token && token[0])
         {
-            printf("Header: \"%s\" -> \"%s\"\n", header_key, header_value);
+            key_vals[i++] = token;
+            flag = 1;
         }
+    } while (token && flag--);
+
+    for (i = 0; key_vals[i]; i++)
+    {
+        sscanf(key_vals[i], "%[^=]=%s", key, val);
+        printf("Query: \"%s\" -> \"%s\"\n", key, val);
+        fflush(stdout);
     }
 }
