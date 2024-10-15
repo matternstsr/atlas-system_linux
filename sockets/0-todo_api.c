16851f24f1d8d3ed4a4422a36a374211e339bd0e
matternstsr <matternstsr@gmail.com>
2024-10-15 17:12:37 -0500

    hmm

--- sockets/0-todo_api.c
+++ sockets/0-todo_api.c
@@ -1,92 +1,86 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#define RESPONSE "HTTP/1.1 200 OK\r\n\r\n"
 
-void handle_client(int client_sock)
-{
-	char buffer[BUFFER_SIZE], method[10], path[100], version[10];
-	ssize_t bytes_received;
-	const char *http_response;
-
-	bytes_received = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
-	if (bytes_received < 0)
-	{
-		perror("Receive failed");
-		return;
-	}
-	buffer[bytes_received] = '\0'; /* Null-terminate the string */
-
-	/* Print the raw request */
-	printf("Raw request: %s\n", buffer);
-
-	/* Parse the request line */
-	sscanf(buffer, "%s %s %s", method, path, version);
-	
-	/* Print the parsed method, path, and version */
-	printf("Method: %s\n", method);
-	printf("Path: %s\n", path);
-	printf("Version: %s\n", version);
-	
-	/* Prepare HTTP response */
-	http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!";
-	
-	/* Send response */
-	send(client_sock, http_response, strlen(http_response), 0);
-}
-
+/* 
+* main - Entry point of the HTTP server
+* Return: 0 on success 
+*/
 int main(void)
 {
-	int sockfd, newsockfd;
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t addr_len = sizeof(client_addr);
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
+	int server_socket, client_socket;
+	size_t received_bytes = 0;
+	char buffer[4096];
+	char method[50], path[50], version[50];
+	struct sockaddr_in server_addr;
+	socklen_t addr_len = sizeof(server_addr);
+	
+	/* Create socket */
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket < 0)
 	{
 		perror("Socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-
-	memset(&server_addr, 0, sizeof(server_addr));
+	
+	/* Configure server address */
 	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(8080);
 	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(PORT);
-
-	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	
+	/* Bind the socket */
+	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
 	{
 		perror("Bind failed");
-		close(sockfd);
+		close(server_socket);
 		exit(EXIT_FAILURE);
 	}
-
-	if (listen(sockfd, 5) < 0)
+	
+	printf("Server listening on port 8080\n");
+	
+	/* Start listening for connections */
+	if (listen(server_socket, 5) < 0)
 	{
 		perror("Listen failed");
-		close(sockfd);
+		close(server_socket);
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Server listening on port %d\n", PORT);
-
+	/* Main loop to accept and handle clients */
 	while (1)
 	{
-		newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_len);
-		if (newsockfd < 0)
+		client_socket = accept(server_socket, (struct sockaddr *)&server_addr, &addr_len);
+		if (client_socket < 0)
 		{
 			perror("Accept failed");
-			continue;
+			continue; /* Continue to the next iteration on error */
+		}
+		
+		printf("Client connected: %s\n", inet_ntoa(server_addr.sin_addr));
+		
+		/* Receive request */
+		received_bytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+		if (received_bytes > 0)
+		{
+			buffer[received_bytes] = '\0'; /* Null-terminate the received string */
+			printf("Raw request: \"%s\"\n", buffer);
+			
+			/* Parse the request */
+			sscanf(buffer, "%s %s %s", method, path, version);
+			printf("Method: %s\nPath: %s\nVersion: %s\n", method, path, version);
 		}
 
-		printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
-		handle_client(newsockfd);
-		close(newsockfd);
+		/* Send response */
+		send(client_socket, RESPONSE, sizeof(RESPONSE) - 1, 0);
+		close(client_socket); /* Close the client connection */
 	}
 
-	close(sockfd);
+	close(server_socket); /* Close the server socket */
 	return (0);
 }
