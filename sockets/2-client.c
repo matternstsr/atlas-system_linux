d44ed3f6b1402ddae5f36fcbc5b3d732232c6354
matternstsr <matternstsr@gmail.com>
2024-10-15 16:00:58 -0500

    betty

--- sockets/2-client.c
+++ sockets/2-client.c
@@ -3,87 +3,65 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
-#define BUFFER_SIZE 1024
+#define EXIT_S 0
+#define EXIT_F 1
 
-/**
-* main - Connects to a specified server.
-* This function creates a TCP socket, connects to the server at the
-* provided host and port, and allows the user to send messages to the server.
-* The program expects two command-line arguments: the host and port number.
-* If the arguments are insufficient, it prints usage information and exits.
-* 
-* Return: 0 upon successful execution.
+/*
+* main - Connects to a listening server.
+*
+* @argc: arg count.
+* @argv: arg array.
+* Return: EXIT_S on success | EXIT_F on failure.
 */
 int main(int argc, char *argv[])
 {
-	int sockfd;
-	struct sockaddr_in server_addr;
-	char buffer[BUFFER_SIZE];
+	struct addrinfo hints, *res, *p;
+	int sockfd, status;
 
-	/* Check if the correct number of arguments is provided */
-	if (argc != 3)
+	if (argc != 3) /* Check for the correct number of arguments */
 	{
 		fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
-		exit(EXIT_FAILURE);
+		return EXIT_F;
 	}
 
-	const char *host = argv[1]; /* Convert port to unsigned short */
-	unsigned short port = (unsigned short)atoi(argv[2]);
+	memset(&hints, 0, sizeof(hints)); /* Initialize hints */
+	hints.ai_family = AF_INET; /* Use IPv4 */
+	hints.ai_socktype = SOCK_STREAM; /* TCP socket */
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); /* Create a socket */
-	if (sockfd < 0)
-	{
-		perror("Socket creation failed");
-		exit(EXIT_FAILURE);
+	if ((status = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0)
+	{ /* Get address info */
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		return EXIT_F;
 	}
 
-	memset(&server_addr, 0, sizeof(server_addr)); /* Prepare sockaddr_in */
-	server_addr.sin_family = AF_INET; /* IPv4 */
-	server_addr.sin_port = htons(port); /* Port number */
-
-	/* Convert IP address from text to binary form */
-	if (inet_pton(AF_INET, host, &server_addr.sin_addr) <= 0)
-	{
-		/* perror("Invalid address/ Address not supported"); */
-		close(sockfd);
-		exit(EXIT_FAILURE);
+	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sockfd == -1)
+	{ /* Create a socket */
+		perror("socket");
+		freeaddrinfo(res);
+		return EXIT_F;
 	}
 
-	/* Connect to the server */
-	if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-	{
-		perror("Connection failed");
-		close(sockfd);
-		exit(EXIT_FAILURE);
-	}
-
-	printf("Connected to server at %s:%d\n", host, port);
-
-	while (1) /* Communicate with the server */
-	{
-		printf("Enter message (or 'exit' to quit): ");
-		fgets(buffer, sizeof(buffer), stdin);
-
-		buffer[strcspn(buffer, "\n")] = 0; /* Remove newline char from input */
-
-		if (strcmp(buffer, "exit") == 0) /* Exit loop if user types "exit" */
+	for (p = res; p != NULL; p = p->ai_next)
+	{ /* Attempt to connect */
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == 0)
 		{
-			break;
+			printf("Connected to %s:%s\n", argv[1], argv[2]);
+			break; /* Connection successful */
 		}
-
-		send(sockfd, buffer, strlen(buffer), 0);/* Send message to the server */
-
-		ssize_t bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-		if (bytes_received < 0)  /* Receive the server's response */
-		{
-			perror("Receive failed");
-			break;
-		}
-		buffer[bytes_received] = '\0'; /* Null-terminate the received string */
-		printf("Server response: %s\n", buffer);
 	}
 
-	close(sockfd); /* Close the socket */
-	return 0;
+	if (p == NULL) /* Check if we were able to connect */
+	{
+		fprintf(stderr, "Connection failed\n");
+		close(sockfd);
+		freeaddrinfo(res);
+		return EXIT_F;
+	}
+
+	freeaddrinfo(res); /* Clean up */
+	close(sockfd);
+	return EXIT_S;
 }
