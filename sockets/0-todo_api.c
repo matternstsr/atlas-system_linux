7f4f1861b69d87ebd0ace7bce3be2833898fc2cd
matternstsr <matternstsr@gmail.com>
2024-10-15 17:22:09 -0500

    hmm

--- sockets/0-todo_api.c
+++ sockets/0-todo_api.c
@@ -9,77 +9,78 @@
 #define RESPONSE_MSG "HTTP/1.1 200 OK\r\n\r\n"
 
 /* 
-* main - Entry point of the HTTP server 
-* Return: 0 on success 
-*/
+ * main - Entry point of the HTTP server 
+ * Return: 0 on success 
+ */
 int main(void)
 {
-	int server_sock, client_sock;
-	size_t received_bytes = 0;
-	char req_buffer[4096], request_method[50], request_path[50], http_version[50];
-	struct sockaddr_in client_addr;
-	socklen_t addr_len = sizeof(client_addr);
+    int server_sock, client_sock;
+    size_t received_bytes = 0;
+    char req_buffer[4096], request_method[50], request_path[50], http_version[50];
+    char sent[32] = RESPONSE_MSG;
+    struct sockaddr_in client_addr;
+    socklen_t addr_len = sizeof(client_addr);
 
-	/* Create socket */
-	server_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_sock < 0)
-	{
-		perror("Socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+    /* Create socket */
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-	/* Configure server address */
-	client_addr.sin_family = AF_INET;
-	client_addr.sin_port = htons(8080);
-	client_addr.sin_addr.s_addr = INADDR_ANY;
+    /* Configure server address */
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(8080);
+    client_addr.sin_addr.s_addr = INADDR_ANY;
 
-	/* Bind the socket */
-	if (bind(server_sock, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0)
-	{
-		perror("Binding failed");
-		close(server_sock);
-		exit(EXIT_FAILURE);
-	}
+    /* Bind the socket */
+    if (bind(server_sock, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0)
+    {
+        perror("Binding failed");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
 
-	printf("Server listening on port 8080\n");
+    printf("Server listening on port 8080\n");
 
-	/* Start listening for connections */
-	if (listen(server_sock, 5) < 0)
-	{
-		perror("Listening failed");
-		close(server_sock);
-		exit(EXIT_FAILURE);
-	}
+    /* Start listening for connections */
+    if (listen(server_sock, 5) < 0)
+    {
+        perror("Listening failed");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
 
-	/* Main loop to accept and handle clients */
-	while (1)
-	{
-		client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_len);
-		if (client_sock < 0)
-		{
-			perror("Accepting connection failed");
-			continue; /* Continue to the next iteration on error */
-		}
+    /* Main loop to accept and handle clients */
+    while (1)
+    {
+        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_sock < 0)
+        {
+            perror("Accepting connection failed");
+            continue; /* Continue to the next iteration on error */
+        }
 
-		printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
+        printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
 
-		/* Receive request */
-		received_bytes = recv(client_sock, req_buffer, sizeof(req_buffer) - 1, 0);
-		if (received_bytes > 0)
-		{
-			req_buffer[received_bytes] = '\0'; /* Null-terminate the received string */
-			printf("Raw request: \"%s\"\n", req_buffer);
-			
-			/* Parse the request */
-			sscanf(req_buffer, "%s %s %s", request_method, request_path, http_version);
-			printf("Method: %s\nPath: %s\nVersion: %s\n", request_method, request_path, http_version);
-		}
+        /* Receive request */
+        received_bytes = recv(client_sock, req_buffer, sizeof(req_buffer) - 1, 0);
+        if (received_bytes > 0)
+        {
+            req_buffer[received_bytes] = '\0'; /* Null-terminate the received string */
+            printf("Raw request: \"%s\"\n", req_buffer);
+            
+            /* Parse the request */
+            sscanf(req_buffer, "%s %s %s", request_method, request_path, http_version);
+            printf("Method: %s\nPath: %s\nVersion: %s\n", request_method, request_path, http_version);
+        }
 
-		/* Send response */
-		send(client_sock, RESPONSE_MSG, sizeof(RESPONSE_MSG) - 1, 0);
-		close(client_sock); /* Close the client connection */
-	}
+        /* Send response */
+        send(client_sock, sent, sizeof(sent) - 1, 0);
+        close(client_sock); /* Close the client connection */
+    }
 
-	close(server_sock); /* Close the server socket */
-	return (0);
+    close(server_sock); /* Close the server socket */
+    return (0);
 }
