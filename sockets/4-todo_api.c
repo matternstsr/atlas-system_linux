2ffdde239b584644845b423ddf5aa8e319ae6e23
matternstsr <matternstsr@gmail.com>
2024-10-16 15:12:52 -0500

    hmm

--- sockets/4-todo_api.c
+++ sockets/4-todo_api.c
@@ -22,10 +22,10 @@ void parse_headers(char *query, int fd);
 void parse_body(char *body, int fd);
 void add_todo_item(char *description, char *title, int fd);
 
-const char *STAT_404 = "HTTP/1.1 404 Not Found\r\n";
-const char *STAT_411 = "HTTP/1.1 411 Length Required\r\n";
-const char *STAT_422 = "HTTP/1.1 422 Unprocessable Entity\r\n";
-const char *STAT_201 = "HTTP/1.1 201 Created\r\n";
+const char *STAT_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+const char *STAT_411 = "HTTP/1.1 411 Length Required\r\n\r\n";
+const char *STAT_422 = "HTTP/1.1 422 Unprocessable Entity\r\n\r\n";
+const char *STAT_201 = "HTTP/1.1 201 Created\r\n\r\n";
 
 int main(void)
 {
@@ -108,7 +108,7 @@ void parse_headers(char *query, int fd)
 	char *line = NULL, *header_lines[16] = {0}, *body = NULL, key[50], value[50];
 
 	printf("Parsing headers\n");
-	while ((line = strsep(&query, "\r\n")) != NULL && i < 16)
+	while ((line = strsep(&query, "\r\n\r\n")) != NULL && i < 16)
 		header_lines[i++] = line;
 	body = header_lines[i - 1];
 
@@ -192,8 +192,8 @@ void add_todo_item(char *description, char *title, int fd)
 	fflush(stdout);
 
 	dprintf(fd, "%s", STAT_201);
-	dprintf(fd, "Content-Length: %d\r\n", response_length);
+	dprintf(fd, "Content-Length: %d\r\n\r\n", response_length);
 	dprintf(fd, "Content-Type: application/json\r\n\r\n");
-	dprintf(fd, "%s\r\n", response_buffer); /* added new line..nowork.*/
+	dprintf(fd, "%s\r\n\r\n", response_buffer); /* added new line..nowork.*/
 }
 
