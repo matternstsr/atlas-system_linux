d20393bf7a4794c4d58df175d1eccaed28532085
matternstsr <matternstsr@gmail.com>
2024-10-16 14:48:33 -0500

    hmm

--- sockets/4-todo_api.c
+++ sockets/4-todo_api.c
@@ -17,6 +17,16 @@ typedef struct todo
 
 todo_t *todo_list = NULL;
 
+void handle_request(char *request, int fd);
+void parse_headers(char *query, int fd);
+void parse_body(char *body, int fd);
+void add_todo_item(char *description, char *title, int fd);
+
+const char *STAT_404 = "HTTP/1.1 404 Not Found\r\n";
+const char *STAT_411 = "HTTP/1.1 411 Length Required\r\n";
+const char *STAT_422 = "HTTP/1.1 422 Unprocessable Entity\r\n";
+const char *STAT_201 = "HTTP/1.1 201 Created\r\n";
+
 int main(void)
 {
 	int server_socket, client_socket;
