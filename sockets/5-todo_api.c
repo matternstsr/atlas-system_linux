eadf11b59e8899d2e7e58304d29218479679afe6
matternstsr <matternstsr@gmail.com>
2024-10-16 16:47:28 -0500

    hmm

--- sockets/5-todo_api.c
+++ sockets/5-todo_api.c
@@ -32,40 +32,47 @@ int main(void)
 	socklen_t addrlen = sizeof(s_address);
 
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_fd == -1)
-		perror("socket failed"), exit(EXIT_FAILURE);
+	if (socket_fd == -1) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
 	
 	s_address.sin_family = AF_INET;
 	s_address.sin_port = htons(8080);
 	s_address.sin_addr.s_addr = INADDR_ANY;
 
-	if (bind(socket_fd, (struct sockaddr *)&s_address, sizeof(s_address)) < 0)
-		perror("bind failed"), exit(EXIT_FAILURE);
-	
+	if (bind(socket_fd, (struct sockaddr *)&s_address, sizeof(s_address)) < 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
 	printf("Server listening on port 8080\n");
 	
-	if (listen(socket_fd, 5) < 0)
-		perror("listen failed"), exit(EXIT_FAILURE);
+	if (listen(socket_fd, 5) < 0) {
+		perror("listen failed");
+		exit(EXIT_FAILURE);
+	}
 	
-	while (1)
-	{
+	while (1) {
 		connect = accept(socket_fd, (struct sockaddr *)&s_address, &addrlen);
-		if (connect < 0)
-			perror("accept failed"), exit(EXIT_FAILURE);
-		
+		if (connect < 0) {
+			perror("accept failed");
+			exit(EXIT_FAILURE);
+		}
+
 		printf("Client connected: %s\n", inet_ntoa(s_address.sin_addr));
 		bytes = recv(connect, buffer, sizeof(buffer) - 1, 0);
-		if (bytes > 0)
-		{
+		if (bytes > 0) {
 			buffer[bytes] = '\0';
-			printf("Raw request: \"%s\"\n", buffer), fflush(stdout);
+			printf("Raw request: \"%s\"\n", buffer);
 			sscanf(buffer, "%s %s", method, path);
+			printf("Method: %s, Path: %s\n", method, path);
 			body_parser(buffer, connect, method);
 		}
 		close(connect);
 	}
 	
-	return (0);
+	return 0;
 }
 
 void body_parser(char *query, int connect, const char *method)
@@ -75,24 +82,23 @@ void body_parser(char *query, int connect, const char *method)
 
 	while (line != NULL) {
 		if (strncmp(line, "Content-Length:", 15) == 0) {
-			int length = atoi(line + 16);
 			body = line + 16 + strlen(line + 16);
 			break;
 		}
 		line = strtok(NULL, "\r\n");
 	}
-
 	query_parser(body, connect, method);
 }
 
 void query_parser(char *query, int connect, const char *method)
 {
 	char response_body[1024];
+	int i;
 
 	if (strcmp(method, "GET") == 0) {
 		if (strcmp(query, "/todos") == 0) {
 			snprintf(response_body, sizeof(response_body), "[");
-			for (int i = 0; i < todo_count; i++) {
+			for (i = 0; i < todo_count; i++) {
 				char todo_item[256];
 				snprintf(todo_item, sizeof(todo_item), "{\"id\":%d,\"title\":\"%s\",\"description\":\"%s\"}%s",
 						todos[i].id, todos[i].title, todos[i].description, (i < todo_count - 1) ? "," : "");
@@ -123,9 +129,7 @@ void send_response(int connect, int status_code, const char *body)
 	int body_length = strlen(body);
 	
 	snprintf(header, sizeof(header), "HTTP/1.1 %d %s\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s",
-			status_code, status_code == 200 ? "OK" : 
-			status_code == 201 ? "Created" : 
-			status_code == 404 ? "Not Found" : "Error", body_length, body);
+			status_code, status_code == 200 ? "OK" : "Not Found", body_length, body);
 	
 	send(connect, header, strlen(header), 0);
 }
