cced84866d303c0ace4d34b68e13aecee9be1693
matternstsr <matternstsr@gmail.com>
2024-10-16 14:01:38 -0500

    hmm

--- sockets/3-todo_api.c
+++ sockets/3-todo_api.c
@@ -7,107 +7,93 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#define PORT 8080
 #define RESPONSE "HTTP/1.1 200 OK\r\n\r\n"
-#define BUFFERSZ 4096
-#define PATHSZ 50
 
-void parse_http_parameters(char *query);
-void parse_http_body(char *query);
+void parse_http_parameters(char *request);
+char *make_response(char *address, char *request);
 
 int main(void)
 {
-	int socket_fd, connect;
-	size_t bytes = 0;
-	char buffer[BUFFERSZ];
-	char path[PATHSZ];
-	struct sockaddr_in s_address;
-	socklen_t addrlen = sizeof(s_address);
+    int socket_fd, client_socket;
+    size_t bytes = 0;
+    char buffer[4096];
+    struct sockaddr_in server_address;
+    socklen_t addrlen = sizeof(server_address);
 
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_fd == -1)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
 
-	s_address.sin_family = AF_INET;
-	s_address.sin_port = htons(8080);
-	s_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
-	if (bind(socket_fd, (struct sockaddr *)&s_address, sizeof(s_address)) < 0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
+    if (bind(socket_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
 
-	printf("Server listening on port 8080\n");
-	if (listen(socket_fd, 5) < 0)
-	{
-		perror("listen failed");
-		exit(EXIT_FAILURE);
-	}
+    printf("Server listening on port %d\n", PORT);
+    if (listen(socket_fd, 5) < 0)
+    {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
 
-	while (1)
-	{
-		connect = accept(socket_fd, (struct sockaddr *)&s_address, &addrlen);
-		if (connect < 0)
-		{
-			perror("accept failed");
-			exit(EXIT_FAILURE);
-		}
+    while (1)
+    {
+        client_socket = accept(socket_fd, (struct sockaddr *)&server_address, &addrlen);
+        if (client_socket < 0)
+        {
+            perror("accept failed");
+            exit(EXIT_FAILURE);
+        }
 
-		printf("Client connected: %s\n", inet_ntoa(s_address.sin_addr));
-		bytes = recv(connect, buffer, sizeof(buffer) - 1, 0);
-		if (bytes > 0)
-		{
-			buffer[bytes] = '\0';
-			printf("Raw request: \"%s\"\n", buffer);
-			sscanf(buffer, "%*s %s", path);
-			printf("Path: %s\n", path);
-			parse_http_body(buffer);
-		}
+        char client_address[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &server_address.sin_addr, client_address, sizeof(client_address));
+        bytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+        if (bytes > 0)
+        {
+            buffer[bytes] = '\0';
+            make_response(client_address, buffer);
+        }
 
-		send(connect, RESPONSE, strlen(RESPONSE), 0);
-		close(connect);
-	}
+        send(client_socket, RESPONSE, strlen(RESPONSE), 0);
+        close(client_socket);
+    }
 
-	close(socket_fd);
-	return 0;
+    close(socket_fd);
+    return 0;
 }
 
-void parse_http_body(char *query)
+char *make_response(char *address, char *request)
 {
-	int i = 0;
-	char *token = NULL, *lines[16] = {0}, *body = NULL;
-
-	while ((token = strsep(&query, "\r\n")) != NULL)
-	{
-		if (i < 16)
-		{
-			lines[i++] = token;
-		}
-	}
-
-	body = lines[i - 1];
-	parse_http_parameters(body);
+    printf("Client connected: %s\n", address);
+    printf("Raw request: \"%s\"\n", request);
+    parse_http_parameters(request);
+    fflush(stdout);
+    return strdup(RESPONSE);
 }
 
-void parse_http_parameters(char *query)
+void parse_http_parameters(char *request)
 {
-	int i = 0;
-	char *token = NULL, *key_vals[16] = {0}, key[50], val[50];
+    char *body_params = strstr(request, "\r\n\r\n") + 4;
+    char *path = strtok(strtok(strchr(request, ' ') + 1, " "), "?");
+    char *key, *value;
 
-	while ((token = strsep(&query, "&")) != NULL)
-	{
-		if (i < 16 && token[0])
-		{
-			key_vals[i++] = token;
-		}
-	}
+    printf("Path: %s\n", path);
 
-	for (i = 0; key_vals[i]; i++)
-	{
-		sscanf(key_vals[i], "%[^=]=%s", key, val);
-		printf("Body param: \"%s\" -> \"%s\"\n", key, val);
-	}
+    for (
+        key = strtok(body_params, "="), value = strtok(NULL, "&");
+        key && value;
+        key = strtok(NULL, "="), value = strtok(NULL, "&")
+    )
+    {
+        printf("Body param: \"%s\" -> \"%s\"\n", key, value);
+    }
 }
