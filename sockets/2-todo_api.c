81f3b398f4012ad1d228bb39408c8177d2b3c483
matternstsr <matternstsr@gmail.com>
2024-10-16 13:05:32 -0500

    hmm

--- sockets/2-todo_api.c
+++ sockets/2-todo_api.c
@@ -75,9 +75,10 @@ int main(void)
 
 void parse_headers(char *request)
 {
-	char *line_token,*key_start = header_key,*value_start = header_value;
+	char *line_token,*key_start,*value_start = header_value;
 	char header_key[256], header_value[256];
 
+	line_token,*key_start = header_key;
 	line_token = strtok(request, "\r\n");
 	while (line_token)
 	{
