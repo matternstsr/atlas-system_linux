abfc00591dd49272ce795247625e8f35f889fe1a
matternstsr <matternstsr@gmail.com>
2024-10-16 14:39:43 -0500

    hmm

--- sockets/4-todo_api.c
+++ sockets/4-todo_api.c
@@ -1,147 +1,173 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include "sockets.h"
 
-#define GET_URIS {"/todos"}
-#define HEAD_URIS {NULL}
-#define POST_URIS {"/todos"}
-#define PUT_URIS {NULL}
-#define DELETE_URIS {"/todos"}
-#define CONNECT_URIS {NULL}
-#define OPTIONS_URIS {NULL}
-#define TRACE_URIS {NULL}
+todo_t *todo_list = NULL;
 
-bool is_known_uri(http_request_t *request) {
-    const char *uris_by_method[][NUM_HTTP_METHODS] = {
-        GET_URIS,
-        HEAD_URIS,
-        POST_URIS,
-        PUT_URIS,
-        DELETE_URIS,
-        CONNECT_URIS,
-        OPTIONS_URIS,
-        TRACE_URIS
-    };
-    
-    char **uris = uris_by_method[request->method];
+int main(void)
+{
+    int server_socket, client_socket;
+    size_t received_bytes = 0;
+    char request_buffer[4096];
+    struct sockaddr_in server_address;
+    socklen_t address_length = sizeof(server_address);
 
-    for (size_t i = 0; uris[i]; ++i) {
-        if (strcmp(request->uri, uris[i]) == 0) {
-            return true;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
+	{
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+	{
+        perror("Binding failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is now listening on port 8080\n");
+
+    if (listen(server_socket, 5) < 0)
+	{
+        perror("Listening failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+	{
+        client_socket = accept(server_socket, (struct sockaddr *)&server_address, &address_length);
+        if (client_socket < 0)
+		{
+            perror("Connection acceptance failed");
+            exit(EXIT_FAILURE);
         }
-    }
-    return false;
-}
 
-size_t handle_get_request(http_request_t *request, char **body, todo_t *todos, int sum_repr_lens, int id) {
-    char *param_id = get_param(request->query_params, "id");
-    
-    if (param_id) {
-        int i = atoi(param_id);
-        if (i >= id || !todos[i].repr) return 0;
-
-        *body = strdup(todos[i].repr);
-        return todos[i].repr_len;
-    } 
-
-    *body = malloc(sum_repr_lens + 3);
-    (*body)[0] = '[';
-    
-    size_t length = 1;
-    char *delimiter = "";
-
-    for (int i = 0; i < id; ++i) {
-        if (todos[i].repr) {
-            length += sprintf(*body + length, "%s%s", delimiter, todos[i].repr);
-            delimiter = ",";
+        printf("Client connected: %s\n", inet_ntoa(server_address.sin_addr));
+        received_bytes = recv(client_socket, request_buffer, sizeof(request_buffer), 0);
+        if (received_bytes > 0)
+		{
+            printf("Received request: \"%s\"\n", request_buffer);
+            fflush(stdout);
+            handle_request(request_buffer, client_socket);
         }
+        close(client_socket);
     }
-    
-    (*body)[length++] = ']';
-    (*body)[length] = '\0';
-    
-    return length;
+    return 0;
 }
 
-char *handle_delete_request(http_request_t *request, todo_t *todos, int id, int *sum_repr_lens) {
-    char *param_id = get_param(request->query_params, "id");
-    
-    if (!param_id) return NULL;
+void handle_request(char *request, int fd)
+{
+    char method[50], path[50];
 
-    int i = atoi(param_id);
-    if (i >= id || !todos[i].repr) return NULL;
+    printf("Processing request\n");
+    sscanf(request, "%s %s", method, path);
 
-    *sum_repr_lens -= todos[i].repr_len;
-    memset(&todos[i], 0, sizeof(todo_t));
-    return strdup("\r\n");
+    if (strcmp(method, "POST") != 0 && strcmp(method, "GET") != 0)
+	{
+        send(fd, STAT_404, sizeof(STAT_404), 0);
+        return;
+    }
+    if (strcmp(path, "/todos") != 0)
+	{
+        send(fd, STAT_404, strlen(STAT_404), 0);
+        return;
+    }
+    parse_headers(request, fd);
 }
 
-char *process_request(http_request_t *request) {
-    char *body = NULL;
-    static todo_t todos[100];
-    static int current_id = 0, total_repr_length = 0;
-    size_t response_length = 0;
+void parse_headers(char *query, int fd)
+{
+    int i = 0, has_content_length = 0;
+    char *line = NULL, *header_lines[16] = {0}, *body = NULL, key[50], value[50];
 
-    if (request->method == GET) {
-        response_length = handle_get_request(request, &body, todos, total_repr_length, current_id);
-    } else {
-        char *title = get_param(request->body_params, "title");
-        char *description = get_param(request->body_params, "description");
+    printf("Parsing headers\n");
+    while ((line = strsep(&query, "\r\n")) != NULL && i < 16)
+        header_lines[i++] = line;
+    body = header_lines[i - 1];
 
-        if (!title || !description) return NULL;
-
-        add_todo(todos, current_id, title, description);
-        total_repr_length += todos[current_id].repr_len;
-        body = strdup(todos[current_id].repr);
-        response_length = todos[current_id].repr_len;
-        current_id++;
+    for (i = 1; header_lines[i]; i++)
+	{
+        sscanf(header_lines[i], "%[^:]: %s", key, value);
+        if (strcmp(key, "Content-Length") == 0)
+            has_content_length = 1;
     }
-
-    if (request->method == DELETE) {
-        return handle_delete_request(request, todos, current_id, &total_repr_length);
+    if (!has_content_length)
+	{
+        send(fd, STAT_411, strlen(STAT_411), 0);
+        return;
     }
-
-    if (!body) return NULL;
-
-    #define RESPONSE_FORMAT "Content-Length: %lu\r\nContent-Type: application/json\r\n\r\n%s"
-    
-    char *response = calloc(response_length + sizeof(RESPONSE_FORMAT) + 10, sizeof(char));
-    sprintf(response, RESPONSE_FORMAT, response_length, body);
-    free(body);
-    return response;
+    printf("Body: %s\n", body);
+    parse_body(body, fd);
 }
 
-char *make_response(char *client_address, char *buffer) {
-    struct http_request_s *request;
-    char *status = NULL, *response = NULL;
+void parse_body(char *body, int fd)
+{
+    int i = 0, has_title = 0, has_description = 0;
+    char *pair = NULL, *key_value_pairs[16] = {0}, key[50], value[50], *title = NULL, *description = NULL;
 
-    (void)client_address;
+    printf("Parsing body\n");
+    while ((pair = strsep(&body, "&")) != NULL && i < 16)
+        key_value_pairs[i++] = pair;
 
-    if (!(request = http_request_init(buffer))) {
-        status = "400 Bad Request";
-    } else if (!is_known_uri(request)) {
-        status = "404 Not Found";
-    } else if (request->method == POST && !get_header(request->headers, "Content-Length")) {
-        status = "411 Length Required";
-    } else {
-        response = process_request(request);
-        if (!response) {
-            status = (request->method == POST) ? "422 Unprocessable Entity" : "404 Not Found";
-        } else {
-            status = (request->method == POST) ? "201 Created" : (request->method == DELETE) ? "204 No Content" : "200 OK";
+    for (i = 0; key_value_pairs[i]; i++)
+	{
+        sscanf(key_value_pairs[i], "%[^=]=%s", key, value);
+        if (strcmp(key, "title") == 0)
+		{
+            title = strdup(value);
+            has_title = 1;
+        }
+		else if (strcmp(key, "description") == 0)
+		{
+            description = strdup(value);
+            has_description = 1;
         }
     }
 
-    char *method = request ? request->method_str : strtok(buffer, " ");
-    char *uri = request ? request->uri : strtok(NULL, "? ");
-    printf("%s %s -> %s\n", method, uri, status);
-
-    char *res = calloc(1024, sizeof(char));
-    sprintf(res, "HTTP/1.1 %s\r\n%s", status, response ? response : "\r\n");
+    if (!has_title || !has_description)
+	{
+        send(fd, STAT_422, strlen(STAT_422), 0);
+        return;
+    }
     
-    free_http_request(request);
-    free(response);
-    return res;
+    printf("Title: %s\nDescription: %s\n", title, description);
+    add_todo_item(description, title, fd);
+}
+
+void add_todo_item(char *description, char *title, int fd)
+{
+    static size_t todo_id = 0;
+    char response_buffer[1024];
+    todo_t *new_todo = calloc(1, sizeof(todo_t));
+    if (!new_todo)
+        return;
+
+    new_todo->id = todo_id++;
+    new_todo->title = strdup(title);
+    new_todo->description = strdup(description);
+    new_todo->next = NULL;
+
+    if (!todo_list)
+        todo_list = new_todo;
+	else
+	{
+        todo_t *current = todo_list;
+        while (current->next)
+            current = current->next;
+        current->next = new_todo;
+    }
+
+    snprintf(response_buffer, sizeof(response_buffer), "{\"id\":%lu,\"title\":\"%s\",\"description\":\"%s\"}",
+             new_todo->id, title, description);
+
+    int response_length = strlen(response_buffer);
+    printf("%s\n", response_buffer);
+    fflush(stdout);
+    
+    dprintf(fd, "%s", STAT_201);
+    dprintf(fd, "Content-Length: %d\r\n", response_length);
+    dprintf(fd, "Content-Type: application/json\r\n\r\n");
+    dprintf(fd, "%s", response_buffer);
 }
