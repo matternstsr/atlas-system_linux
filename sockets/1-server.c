b7bf9aefdecf7ad21c331b0f8ba0f1b1657fd802
matternstsr <matternstsr@gmail.com>
2024-10-16 17:24:15 -0500

    betty

--- sockets/1-server.c
+++ sockets/1-server.c
@@ -135,12 +135,14 @@ void handle_client(int newsockfd)
 	close(newsockfd); /* Close the connection to the client */
 }
 
+
 /**
-* handle_client - Manages communication with a connected client.
-*
-* @newsockfd: the socket file descriptor for the connected client.
-* Return: void.
-*/
+ * cleanup - Closes the server and client sockets.
+ *
+ * @sockfd: the server socket file descriptor.
+ * @newsockfd: the client socket file descriptor (can be -1 if no client).
+ * Return: void.
+ */
 void cleanup(int sockfd, int newsockfd)
 {
 	if (newsockfd != -1)
