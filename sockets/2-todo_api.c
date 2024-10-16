e8a0617cc35a6319b0fee775b9a96a5316b60376
matternstsr <matternstsr@gmail.com>
2024-10-16 13:19:38 -0500

    hmm

--- sockets/2-todo_api.c
+++ sockets/2-todo_api.c
@@ -13,88 +13,90 @@ void parse_headers(char *request);
 
 int main(void)
 {
-    int server_fd, client_fd;
-    size_t received_bytes = 0;
-    char request_buffer[4096];
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_len = sizeof(client_addr);
+	int server_fd, client_fd;
+	size_t received_bytes = 0;
+	char request_buffer[4096];
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t addr_len;
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-    {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+	addr_len = sizeof(client_addr);
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+	{
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(8080);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("binding failed");
-        exit(EXIT_FAILURE);
-    }
+	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("binding failed");
+		exit(EXIT_FAILURE);
+	}
 
-    printf("Server listening on port 8080\n");
-    fflush(stdout);
+	printf("Server listening on port 8080\n");
+	fflush(stdout);
 
-    if (listen(server_fd, 5) < 0)
-    {
-        perror("listening failed");
-        exit(EXIT_FAILURE);
-    }
+	if (listen(server_fd, 5) < 0)
+	{
+		perror("listening failed");
+		exit(EXIT_FAILURE);
+	}
 
-    while (1)
-    {
-        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
-        if (client_fd < 0)
-        {
-            perror("connection acceptance failed");
-            exit(EXIT_FAILURE);
-        }
+	while (1)
+	{
+		client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+		if (client_fd < 0)
+		{
+			perror("connection acceptance failed");
+			exit(EXIT_FAILURE);
+		}
 
-        printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
-        fflush(stdout);
+		printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
+		fflush(stdout);
 
-        received_bytes = recv(client_fd, request_buffer, sizeof(request_buffer) - 1, 0);
-        if (received_bytes > 0)
-        {
-            request_buffer[received_bytes] = '\0';
-            printf("Raw request: \"%s\"\n", request_buffer);
-            fflush(stdout);
-            parse_headers(request_buffer);
-        }
+		received_bytes = recv(client_fd, request_buffer, sizeof(request_buffer) - 1, 0);
+		if (received_bytes > 0)
+		{
+			request_buffer[received_bytes] = '\0';
+			printf("Raw request: \"%s\"\n", request_buffer);
+			fflush(stdout);
+			parse_headers(request_buffer);
+		}
 
-        send(client_fd, RESPONSE, strlen(RESPONSE), 0);
-        close(client_fd);
-    }
+		send(client_fd, RESPONSE, strlen(RESPONSE), 0);
+		close(client_fd);
+	}
 
-    close(server_fd);
-    return 0;
+	close(server_fd);
+	return 0;
 }
 
 void parse_headers(char *request)
 {
-    char *line_token, *key_start, *value_start;
+	char *line_token;
 	char header_key[256], header_value[256];
+	char *key_start, *value_start;
 
-    line_token = strtok(request, "\r\n");
-    while (line_token)
-    {
-        if (line_token[0] != '\0' && line_token != request) 
-        {
-            if (sscanf(line_token, "%255[^:]: %255[^\r\n]", header_key, header_value) == 2)
-            {
-                key_start = header_key;
-                while (isspace(*key_start)) key_start++;
+	line_token = strtok(request, "\r\n");
+	while (line_token)
+	{
+		if (line_token[0] == '\0')
+			break;
 
-                value_start = header_value;
-                while (isspace(*value_start)) value_start++;
+		if (sscanf(line_token, "%255[^:]: %255[^\r\n]", header_key, header_value) == 2)
+		{
+			key_start = header_key;
+			while (isspace(*key_start)) key_start++;
 
-                printf("Header: \"%s\" -> \"%s\"\n", key_start, value_start);
-            }
-        }
-        line_token = strtok(NULL, "\r\n");
-    }
+			value_start = header_value;
+			while (isspace(*value_start)) value_start++;
+
+			printf("Header: \"%s\" -> \"%s\"\n", key_start, value_start);
+		}
+		line_token = strtok(NULL, "\r\n");
+	}
 }
