88e22432610186628d8bc89770c10be588f8e7b6
matternstsr <matternstsr@gmail.com>
2024-10-16 13:25:17 -0500

    hmm

--- sockets/2-todo_api.c
+++ sockets/2-todo_api.c
@@ -2,102 +2,76 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <ctype.h>
 
-#define RESPONSE "HTTP/1.1 200 OK\r\n\r\n"
+#define HTTP_RESPONSE "HTTP/1.1 200 OK\r\n\r\n"
 
-void parse_headers(char *request);
+void parse_http_headers(char *request);
 
 int main(void)
 {
-	int server_fd, client_fd;
+	int server_socket, client_socket;
 	size_t received_bytes = 0;
-	char request_buffer[4096];
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t addr_len;
+	char request_buffer[4096], response_message[] = HTTP_RESPONSE;
+	struct sockaddr_in server_address;
+	socklen_t address_length = sizeof(server_address);
 
-	addr_len = sizeof(client_addr);
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1)
-	{
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket == -1)
+		perror("socket failed"), exit(EXIT_FAILURE);
+	
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(8080);
+	server_address.sin_addr.s_addr = INADDR_ANY;
 
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(8080);
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-
-	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-	{
-		perror("binding failed");
-		exit(EXIT_FAILURE);
-	}
+	if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+		perror("bind failed"), exit(EXIT_FAILURE);
 
 	printf("Server listening on port 8080\n");
-	fflush(stdout);
-
-	if (listen(server_fd, 5) < 0)
-	{
-		perror("listening failed");
-		exit(EXIT_FAILURE);
-	}
+	if (listen(server_socket, 5) < 0)
+		perror("listen failed"), exit(EXIT_FAILURE);
 
 	while (1)
 	{
-		client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
-		if (client_fd < 0)
-		{
-			perror("connection acceptance failed");
-			exit(EXIT_FAILURE);
-		}
-
-		printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
-		fflush(stdout);
-
-		received_bytes = recv(client_fd, request_buffer, sizeof(request_buffer) - 1, 0);
+		client_socket = accept(server_socket, (struct sockaddr *)&server_address, &address_length);
+		if (client_socket < 0)
+			perror("accept failed"), exit(EXIT_FAILURE);
+		
+		printf("Client connected: %s\n", inet_ntoa(server_address.sin_addr));
+		
+		received_bytes = recv(client_socket, request_buffer, sizeof(request_buffer) - 1, 0);
 		if (received_bytes > 0)
 		{
 			request_buffer[received_bytes] = '\0';
-			printf("Raw request: \"%s\"\n", request_buffer);
-			fflush(stdout);
-			parse_headers(request_buffer);
+			printf("Raw request: \"%s\"\n", request_buffer), fflush(stdout);
+			parse_http_headers(request_buffer);
 		}
-
-		send(client_fd, RESPONSE, strlen(RESPONSE), 0);
-		close(client_fd);
+		
+		send(client_socket, response_message, strlen(response_message), 0);
+		close(client_socket);
 	}
-
-	close(server_fd);
-	return 0;
+	return (0);
 }
 
-void parse_headers(char *request)
+void parse_http_headers(char *request)
 {
-	char *line_token;
-	char header_key[256], header_value[256];
-	char *key_start, *value_start;
+	char *header_line;
+	char *header_array[16] = {0};
+	char header_key[50], header_value[50];
+	int header_count = 0;
 
-	line_token = strtok(request, "\r\n");
-	while (line_token)
+	while ((header_line = strsep(&request, "\r\n")) != NULL)
 	{
-		if (line_token[0] == '\0')
-			break;
+		if (header_count < 16)
+			header_array[header_count++] = header_line;
+	}
 
-		if (sscanf(line_token, "%255[^:]: %255[^\r\n]", header_key, header_value) == 2)
-		{
-			key_start = header_key;
-			while (isspace(*key_start)) key_start++;
-
-			value_start = header_value;
-			while (isspace(*value_start)) value_start++;
-
-			printf("Header: \"%s\" -> \"%s\"\n", key_start, value_start);
-		}
-
-		line_token = strtok(NULL, "\r\n");
+	for (int i = 1; i < header_count; i++)
+	{
+		if (sscanf(header_array[i], "%[^:]: %[^\r\n]", header_key, header_value) == 2)
+			printf("Header: \"%s\" -> \"%s\"\n", header_key, header_value), fflush(stdout);
 	}
 }
